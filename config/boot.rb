693e20e092fd89529bbc1ff2e1de3f041431522c
Willy Mene <wmene@stanford.edu>
2012-03-07 16:02:41 -0800

    Loads Dor::Item and can initialize workspace

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
-ENV["RACK_ENV"] ||= "development"
+environment = ENV["RACK_ENV"] ||= "development"
 
 require 'rubygems'
 require 'bundler/setup'
@@ -7,3 +7,8 @@ Bundler.require(:default, ENV["RACK_ENV"].to_sym)
 
 $:.unshift File.expand_path(File.join(File.dirname(__FILE__), "..", "lib"))
 Dir["./lib/**/*.rb"].each { |f| require f }
+
+require 'dor-services'
+
+env_file = File.expand_path(File.dirname(__FILE__) + "/environments/#{environment}")
+require env_file
--- lib/dor_services_app.rb
+++ lib/dor_services_app.rb
@@ -1,13 +1,22 @@
 module Dor
   
   class DorServicesApi < Grape::API
-    
-    resource :dor do
+    version 'v1'
+
+    resource :dor do        # TODO might have to assume that passenger will route /dor to this app
+      resource 'objects/:id' do
       
-      get :hola do
-        "what up"
+        # params source can be passed as apended parameter to url:
+        #  http://lyberservices-dev/v1/dor/objects/{druid}/initialize_workspace?source=/path/to/content/dir
+        # or
+        # It can be passed in the body of the request as application/x-www-form-urlencoded parameters, as if submitted from a form
+        # TODO: We could get away with loading a simple object that mixes in Dor::Assembleable.  It just needs to implement #pid
+        post :initialize_workspace do
+          item = Dor::Item.load_instance(params[:id])
+          item.initialize_workspace(params[:source])
+        end
+      
       end
-      
     end
     
   end
