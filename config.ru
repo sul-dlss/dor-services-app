bec1d0c783987fd862081a61c6df5aa341fdda37
Willy Mene <wmene@stanford.edu>
2012-03-20 12:03:06 -0700

    Redirect stdout and stderr to logfile

--- config.ru
+++ config.ru
@@ -1,4 +1,8 @@
 require File.dirname(__FILE__) + '/config/boot.rb'
 
+$stdout.reopen(LyberCore::Log.logfile)
+$stderr.reopen(LyberCore::Log.logfile)
+
 use Rack::CommonLogger, LyberCore::Log.log
+use Rack::ShowExceptions
 run Dor::DorServicesApi
--- config/boot.rb
+++ config/boot.rb
@@ -14,6 +14,7 @@ require 'dor_services_app'
 require 'registration_response'
 require 'registration_params'
 
+# Override from lyber-core gem so that we can access the log object in the config.ru
 module LyberCore
   
   class LyberCore::Log
@@ -24,6 +25,7 @@ module LyberCore
   
 end
 
+# Alias Logger#write to #<< in order for Rack::CommonLogger to use in config.ru
 class ::Logger; alias_method :write, :<<; end
 
 LyberCore::Log.set_logfile(File.join(File.dirname(__FILE__), "..", "log", "dor-services-app.log"))
