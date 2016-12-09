8194d3975426de0f110d9709a8d23cfc727cf981
Chris Beer <chris@cbeer.info>
2016-12-09 09:40:37 -0800

    Use the Stanford-modified MARC2MODS 3.4 XSLT pending upstream fixes to 3.6

--- app/models/marcxml_resource.rb
+++ app/models/marcxml_resource.rb
@@ -1,6 +1,6 @@
 # MARC resource model for retrieving and transforming MARC records
 class MarcxmlResource
-  MARC_TO_MODS_XSLT = Nokogiri::XSLT(File.read(File.join(Rails.root, 'app', 'xslt', 'MARC21slim2MODS3-6.xsl')))
+  MARC_TO_MODS_XSLT = Nokogiri::XSLT(File.read(File.join(Rails.root, 'app', 'xslt', 'MARC21slim2MODS3-4.xsl')))
 
   def self.find_by(catkey: nil, barcode: nil)
     if catkey
