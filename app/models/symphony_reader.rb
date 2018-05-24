bf32829d860419c69361a4106475509a09b61f3d
Aaron Collier <amcollie@stanford.edu>
2018-05-24 10:38:27 -0700

    Inject SIRSI into 003

--- app/models/symphony_reader.rb
+++ app/models/symphony_reader.rb
@@ -16,15 +16,15 @@ class SymphonyReader
     record.leader = leader if leader
 
     fields.uniq.each do |field|
-      record << marc_field(field) unless field['tag'] == '001' # explicitly remove all 001 fields from the record
+      record << marc_field(field) unless %w[001 003].include? field['tag'] # explicitly remove all 001 and 003 fields from the record
     end
 
     # explicitly inject the catkey into the 001 field
-    record << marc_field('tag' => '001',
-                         'subfields' => [
-                           { 'code' => '_',
-                             'data' => "a#{catkey}" }
-                         ])
+    record << marc_field('tag' => '001', 'subfields' => [{ 'code' => '_', 'data' => "a#{catkey}" }])
+
+    # explicitly inject the SIRSI into the 003 field
+    record << marc_field('tag' => '003', 'subfields' => [{ 'code' => '_', 'data' => 'SIRSI' }])
+
     record
   end
 
