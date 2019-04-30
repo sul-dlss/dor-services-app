2a105f50cc26ac700e70751e4d7c7d1658171cda
Justin Coyne <jcoyne@justincoyne.com>
2019-04-30 15:10:08 -0500

    Expose the descriptive metadata as an endpoint
    This will be consumed by common-accessioning and argo.
    We can then move that class to dor-services from dor-services-app

--- spec/requests/metadata_spec.rb
+++ spec/requests/metadata_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Refresh metadata' do
+RSpec.describe 'Display metadata' do
   let(:user) { Settings.DOR.SERVICE_USER }
   let(:password) { Settings.DOR.SERVICE_PASSWORD }
   let(:basic_auth) { ActionController::HttpAuthentication::Basic.encode_credentials(user, password) }
@@ -13,10 +13,27 @@ RSpec.describe 'Refresh metadata' do
     allow(Dor).to receive(:find).and_return(object)
   end
 
-  it 'returns the DC xml' do
-    get '/v1/objects/druid:mk420bs7601/metadata/dublin_core',
-        headers: { 'Authorization' => basic_auth }
-    expect(response).to be_successful
-    expect(response.body).to include '<dc:title>Hello</dc:title>'
+  describe 'dublin core' do
+    it 'returns the DC xml' do
+      get '/v1/objects/druid:mk420bs7601/metadata/dublin_core',
+          headers: { 'Authorization' => basic_auth }
+      expect(response).to be_successful
+      expect(response.body).to include '<dc:title>Hello</dc:title>'
+    end
+  end
+
+  describe 'descriptive' do
+    it 'returns the DC xml' do
+      get '/v1/objects/druid:mk420bs7601/metadata/descriptive',
+          headers: { 'Authorization' => basic_auth }
+      expect(response).to be_successful
+      expect(response.body).to be_equivalent_to <<~XML
+        <mods xmlns="http://www.loc.gov/mods/v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="3.6" xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <titleInfo>
+            <title>Hello</title>
+          </titleInfo>
+        </mods>
+      XML
+    end
   end
 end
