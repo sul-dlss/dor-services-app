f3edbee401d04dfbda4b0a21b2462b6cb5229e21
Justin Coyne <jcoyne@justincoyne.com>
2019-05-13 10:58:46 -0500

    Deprecate the workflows controller
    The workflow service handles this now

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -2,6 +2,7 @@
 
 class WorkflowsController < ApplicationController
   def initial
+    Honeybadger.notify("Call to deprecated API #{request.path}. The workflow server now knows about initial_workflow")
     render content_type: 'application/xml', body: Dor::WorkflowObject.initial_workflow(params[:wf_name])
   end
 end
