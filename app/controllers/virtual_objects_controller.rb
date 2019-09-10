de2bbd87376433ce29d8ffe11f4af53ac42b5438
Michael J. Giarlo <mjgiarlo@stanford.edu>
2019-09-10 10:23:41 -0700

    Enhance parameter validation in `VirtualObjectsController` by using dry-schema

--- app/controllers/virtual_objects_controller.rb
+++ app/controllers/virtual_objects_controller.rb
@@ -2,27 +2,62 @@
 
 # Create virtual objects
 class VirtualObjectsController < ApplicationController
-  # Create one or more virtual objects represented by JSON:
-  # {
-  #   virtual_objects: [
-  #     { parent_id: '', child_ids: [] }
-  #   ]
-  # }
-  def create
-    # validate that the virtual_objects parameter is an present, raises ActionController::ParameterMissing
-    params.require(:virtual_objects)
-    filtered_params = params.permit(virtual_objects: [:parent_id, child_ids: []])
-    raise ActionController::ParameterMissing, 'virtual_objects must be an array' unless filtered_params[:virtual_objects]
+  before_action :validate_params!, only: :create
 
+  # Create one or more virtual objects represented by JSON (see `#schema` below):
+  def create
     errors = []
 
-    filtered_params[:virtual_objects].each do |virtual_object|
+    create_params[:virtual_objects].each do |virtual_object|
+      parent_id, child_ids = virtual_object.values_at(:parent_id, :child_ids)
       # Update the constituent relationship between the parent and child druids
-      errors << ConstituentService.new(parent_druid: virtual_object[:parent_id]).add(child_druids: virtual_object[:child_ids])
+      errors << ConstituentService.new(parent_druid: parent_id).add(child_druids: child_ids)
+    rescue ActiveFedora::ObjectNotFoundError, Rubydora::FedoraInvalidRequest => e
+      errors << { parent_id => [e.message] }
     end
 
-    return render json: { errors: errors }, status: :unprocessable_entity if errors.any?
+    return render_error(errors: errors, status: :unprocessable_entity) if errors.any?
 
     head :no_content
+  end
+
+  private
+
+  def create_params
+    params.to_unsafe_h
+  end
+
+  # Use dry-validation to validate params instead of Rails strong parameters.
+  # This gives us finer-grained validation.
+  def validate_params!
+    errors = schema.call(create_params).errors(full: true)
+    return render_error(errors: errors, status: :bad_request) if errors.any?
+  end
+
+  def render_error(errors:, status:)
+    render json: { errors: errors }, status: status
+  end
+
+  # {
+  #   virtual_objects: [
+  #     {
+  #       parent_id: 'a-non-empty-string',
+  #       child_ids: [
+  #         'another-non-empty-string',
+  #         ...
+  #       ]
+  #     },
+  #     ...
+  #   ]
+  # }
+  def schema
+    Dry::Schema.JSON do
+      required(:virtual_objects).value(:array, min_size?: 1).each do
+        hash do
+          required(:parent_id).filled(:string)
+          required(:child_ids).value(:array, min_size?: 1).each(:filled?)
+        end
+      end
+    end
   end
 end
--- spec/requests/batch_create_virtual_objects_spec.rb
+++ spec/requests/batch_create_virtual_objects_spec.rb
@@ -27,30 +27,6 @@ RSpec.describe 'Batch creation of virtual objects' do
     end
   end
 
-  context 'when virtual_objects param is not provided' do
-    it 'renders an error' do
-      post '/v1/virtual_objects',
-           params: { title: 'New name' },
-           headers: { 'X-Auth' => "Bearer #{jwt}" }
-      expect(service).not_to have_received(:add)
-      expect(response).to be_bad_request
-      json = JSON.parse(response.body)
-      expect(json['errors'][0]['detail']).to eq 'param is missing or the value is empty: virtual_objects'
-    end
-  end
-
-  context 'when virtual_objects is not an array' do
-    it 'renders an error' do
-      post '/v1/virtual_objects',
-           params: { virtual_objects: child1_id },
-           headers: { 'X-Auth' => "Bearer #{jwt}" }
-      expect(service).not_to have_received(:add)
-      expect(response).to be_bad_request
-      json = JSON.parse(response.body)
-      expect(json['errors'][0]['detail']).to eq 'param is missing or the value is empty: virtual_objects must be an array'
-    end
-  end
-
   context 'when virtual_objects contain objects that are not combinable' do
     before do
       allow(service).to receive(:add).and_return(parent_id => ["Item #{child2_id} is not open for modification"])
@@ -63,6 +39,114 @@ RSpec.describe 'Batch creation of virtual objects' do
       expect(service).to have_received(:add).with(child_druids: [child1_id, child2_id])
       expect(response).to be_unprocessable
       expect(response.body).to eq '{"errors":[{"druid:mk420bs7601":["Item druid:child2 is not open for modification"]}]}'
+    end
+  end
+
+  context 'when virtual_objects param is not provided' do
+    it 'renders an error' do
+      post '/v1/virtual_objects',
+           params: { title: 'New name' },
+           headers: { 'X-Auth' => "Bearer #{jwt}" }
+      expect(service).not_to have_received(:add)
+      expect(response).to be_bad_request
+      json = JSON.parse(response.body)
+      expect(json['errors'][0]['text']).to eq 'virtual_objects is missing'
+    end
+  end
+
+  context 'when virtual_objects is not an array' do
+    it 'renders an error' do
+      post '/v1/virtual_objects',
+           params: { virtual_objects: child1_id },
+           headers: { 'X-Auth' => "Bearer #{jwt}" }
+      expect(service).not_to have_received(:add)
+      expect(response).to be_bad_request
+      json = JSON.parse(response.body)
+      expect(json['errors'][0]['text']).to eq 'virtual_objects must be an array'
+    end
+  end
+
+  context 'when virtual_objects is empty array' do
+    it 'renders an error' do
+      post '/v1/virtual_objects',
+           params: { virtual_objects: [] },
+           headers: { 'X-Auth' => "Bearer #{jwt}" }
+      expect(service).not_to have_received(:add)
+      expect(response).to be_bad_request
+      json = JSON.parse(response.body)
+      expect(json['errors'][0]['text']).to eq 'parent_id is missing'
+    end
+  end
+
+  context 'when virtual_objects array lacks hashes defining parent_id' do
+    it 'renders an error' do
+      post '/v1/virtual_objects',
+           params: { virtual_objects: [{ child_ids: ['foo'] }] },
+           headers: { 'X-Auth' => "Bearer #{jwt}" }
+      expect(service).not_to have_received(:add)
+      expect(response).to be_bad_request
+      json = JSON.parse(response.body)
+      expect(json['errors'][0]['text']).to eq 'parent_id is missing'
+    end
+  end
+
+  context 'when virtual_objects array has a hash w/ an empty parent_id' do
+    it 'renders an error' do
+      post '/v1/virtual_objects',
+           params: { virtual_objects: [{ parent_id: '' }] },
+           headers: { 'X-Auth' => "Bearer #{jwt}" }
+      expect(service).not_to have_received(:add)
+      expect(response).to be_bad_request
+      json = JSON.parse(response.body)
+      expect(json['errors'][0]['text']).to eq 'parent_id must be filled'
+    end
+  end
+
+  context 'when virtual_objects array lacks hashes defining child_ids' do
+    it 'renders an error' do
+      post '/v1/virtual_objects',
+           params: { virtual_objects: [{ parent_id: 'foo' }] },
+           headers: { 'X-Auth' => "Bearer #{jwt}" }
+      expect(service).not_to have_received(:add)
+      expect(response).to be_bad_request
+      json = JSON.parse(response.body)
+      expect(json['errors'][0]['text']).to eq 'child_ids is missing'
+    end
+  end
+
+  context 'when virtual_objects array has a hash w/ child_ids not an array' do
+    it 'renders an error' do
+      post '/v1/virtual_objects',
+           params: { virtual_objects: [{ parent_id: 'foo', child_ids: '' }] },
+           headers: { 'X-Auth' => "Bearer #{jwt}" }
+      expect(service).not_to have_received(:add)
+      expect(response).to be_bad_request
+      json = JSON.parse(response.body)
+      expect(json['errors'][0]['text']).to eq 'child_ids must be an array'
+    end
+  end
+
+  context 'when virtual_objects array has a hash w/ child_ids empty' do
+    it 'renders an error' do
+      post '/v1/virtual_objects',
+           params: { virtual_objects: [{ parent_id: 'foo', child_ids: [] }] },
+           headers: { 'X-Auth' => "Bearer #{jwt}" }
+      expect(service).not_to have_received(:add)
+      expect(response).to be_bad_request
+      json = JSON.parse(response.body)
+      expect(json['errors'][0]['text']).to eq '0 must be filled'
+    end
+  end
+
+  context 'when virtual_objects array has a hash w/ child_ids containing empties' do
+    it 'renders an error' do
+      post '/v1/virtual_objects',
+           params: { virtual_objects: [{ parent_id: 'foo', child_ids: ['foo', 'bar', ''] }] },
+           headers: { 'X-Auth' => "Bearer #{jwt}" }
+      expect(service).not_to have_received(:add)
+      expect(response).to be_bad_request
+      json = JSON.parse(response.body)
+      expect(json['errors'][0]['text']).to eq '2 must be filled'
     end
   end
 end
