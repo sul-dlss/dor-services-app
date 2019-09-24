247bc504778d2f9ad5c62853577ab00b2bfd2de5
Michael J. Giarlo <mjgiarlo@stanford.edu>
2019-09-24 08:29:48 -0700

    Revert "Create a model and a table for background job results"
    This reverts commit ebec9df2b1b4940dcd576e76a8f4743397401fdd.

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,5 @@ module DorServices
     config.action_dispatch.rescue_responses.merge!(
       "ActiveFedora::ObjectNotFoundError" => :not_found
     )
-
-    # This makes sure our Postgres enums function are persisted to the schema
-    config.active_record.schema_format = :sql
   end
 end
