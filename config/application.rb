4515368a72e6a7a3fa6b31f2930d07b49224240d
Michael J. Giarlo <mjgiarlo@stanford.edu>
2019-09-24 08:29:59 -0700

    Revert "Add ActiveRecord and Postgres as dependencies"
    This reverts commit 54b4d406d6941e905e491230ed2b16ff7982f2d7.

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require "rails"
 # Pick the frameworks you want:
 require "action_controller/railtie"
 require 'active_job/railtie'
-require 'active_record/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
