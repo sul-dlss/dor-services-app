6c2aea82502bcce5da044b9cddd88e081eb82f9e
Michael J. Giarlo <mjgiarlo@stanford.edu>
2019-10-04 09:13:53 -0700

    Add database config to capistrano linked_files
    This is managed by Puppet, so use the system provided configuration instead of the one from the application

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :log_level, :info
 # set :keep_releases, 5
 
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle config/certs config/settings)
-set :linked_files, %w(bin/write_marc_record config/secrets.yml config/honeybadger.yml config/newrelic.yml)
+set :linked_files, %w(bin/write_marc_record config/secrets.yml config/honeybadger.yml config/newrelic.yml config/database.yml)
 
 # Sidekiq configuration (run one process)
 # see sidekiq.yml for concurrency and queue settings
