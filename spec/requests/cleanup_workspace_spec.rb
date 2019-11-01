85aeb9a483124b3bcab66e1048709e26713caa51
Justin Coyne <jcoyne@justincoyne.com>
2019-11-01 10:15:29 -0500

    Provide error handling for when the directory can't be deleted

--- spec/requests/cleanup_workspace_spec.rb
+++ spec/requests/cleanup_workspace_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'Cleanup workspace' do
     end
   end
 
-  context 'when an error is raised' do
+  context "when the directory doesn't exist" do
     before do
       allow(CleanupService).to receive(:cleanup_by_druid)
         .and_raise(Errno::ENOENT, 'dir_s_rmdir - /dor/workspace/aa/222')
@@ -31,6 +31,23 @@ RSpec.describe 'Cleanup workspace' do
       expect(response.body).to eq(
         '{"errors":[{"status":"422","title":"Unable to remove directory",' \
         '"detail":"No such file or directory - dir_s_rmdir - /dor/workspace/aa/222"}]}'
+      )
+    end
+  end
+
+  context 'when the directory is not empty' do
+    before do
+      allow(CleanupService).to receive(:cleanup_by_druid)
+        .and_raise(Errno::ENOTEMPTY, 'dir_s_rmdir - /dor/assembly/pw/569/pw/4290')
+    end
+
+    it 'returns JSON-API error' do
+      delete "/v1/objects/#{object_id}/workspace",
+             headers: { 'Authorization' => "Bearer #{jwt}" }
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response.body).to eq(
+        '{"errors":[{"status":"422","title":"Unable to remove directory",' \
+        '"detail":"Directory not empty - dir_s_rmdir - /dor/assembly/pw/569/pw/4290"}]}'
       )
     end
   end
