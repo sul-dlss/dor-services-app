f855e23092abd99adc24545e8774518f9b7691ab
Michael J. Giarlo <mjgiarlo@stanford.edu>
2019-12-06 11:33:05 -0800

    Stop adding successful results to errors array when creating virtual objects
    Refs sul-dlss/argo#1728

--- spec/jobs/create_virtual_objects_job_spec.rb
+++ spec/jobs/create_virtual_objects_job_spec.rb
@@ -62,4 +62,40 @@ RSpec.describe CreateVirtualObjectsJob, type: :job do
       expect(result.output[:errors].first[parent_id]).to match_array(['One thing was not combinable', 'And another'])
     end
   end
+
+  context 'with a mix of errors and successful results' do
+    let(:other_child1_id) { 'druid:bar' }
+    let(:other_child2_id) { 'druid:baz' }
+    let(:other_parent_id) { 'druid:foo' }
+    let(:virtual_objects) do
+      [
+        { parent_id: parent_id, child_ids: [child1_id, child2_id] },
+        { parent_id: other_parent_id, child_ids: [other_child1_id, other_child2_id] }
+      ]
+    end
+
+    before do
+      allow(ConstituentService).to receive(:new).with(parent_druid: other_parent_id).and_return(service)
+      allow(service).to receive(:add).and_return(nil, parent_id => ['One thing was not combinable', 'And another'])
+      described_class.perform_now(virtual_objects: virtual_objects,
+                                  background_job_result: result)
+    end
+
+    it 'marks the job as processing' do
+      expect(result).to have_received(:processing!).once
+    end
+
+    it 'invokes the constituent service to do the virtual object creation' do
+      expect(service).to have_received(:add).with(child_druids: [child1_id, child2_id]).once
+      expect(service).to have_received(:add).with(child_druids: [other_child1_id, other_child2_id]).once
+    end
+
+    it 'marks the job as complete' do
+      expect(result).to be_complete
+    end
+
+    it 'has output with errors' do
+      expect(result.output[:errors].first[parent_id]).to match_array(['One thing was not combinable', 'And another'])
+    end
+  end
 end
