e505514995dd0b4f3f4fc4f57f436fe26dfb31c9
Justin Coyne <jcoyne@justincoyne.com>
2020-01-31 20:11:13 -0600

    Don't send invoked_by when there is no context

--- app/services/event_factory.rb
+++ app/services/event_factory.rb
@@ -3,7 +3,7 @@
 # Creates Event records and adds the host name and invoking system identifier
 class EventFactory
   def self.create(druid:, event_type:, data:)
-    invoked_by = Honeybadger.get_context[:invoked_by]
+    invoked_by = Honeybadger.get_context && Honeybadger.get_context[:invoked_by]
     event_data = data.merge(host: Socket.gethostname, invoked_by: invoked_by)
     Event.create!(druid: druid, event_type: event_type, data: event_data)
   end
