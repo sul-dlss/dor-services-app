c371c212c1883bcdcaa71af5de9de7915dfeff5c
Michael J. Giarlo <mjgiarlo@stanford.edu>
2020-02-06 12:15:03 -0800

    Fix forbidden errors when browsing sidekiq routes
    This, and the replaced code, are both incantations listed in the Sidekiq wiki: https://github.com/mperham/sidekiq/wiki/Monitoring#forbidden
    
    I confirmed this was the right incantation by SSHing to a deployed instance and running:
    
    ```
    2.5.0 :001 > Rails.configuration.secret_token
     => nil
    2.5.0 :002 >  Rails.application.credentials[:secret_key_base]
     => nil
    2.5.0 :003 > Rails.application.secrets[:secret_key_base]
     => "a non-nil value which is hidden here to protect the instance"
    ```
    
    I did this because I noticed when troubleshooting sul-dlss/argo#1800 that I could not use the Sidekiq UI to delete or kill individual jobs. I briefly entertained the idea that this was related to committee/OpenAPI but this is not related to OpenAPI.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   require 'sidekiq/web'
-  Sidekiq::Web.set :session_secret, Rails.application.credentials[:secret_key_base]
+  Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
   mount Sidekiq::Web => '/queues'
 
   scope '/v1' do
