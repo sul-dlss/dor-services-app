20418f341de9e97c9da8d5b56e2bd9a1a2a89062
Justin Coyne <jcoyne@justincoyne.com>
2020-02-11 16:09:59 -0600

    Allow creating an object with a Cocina model

---
+++ app/services/cocina/object_creator.rb
@@ -0,0 +1,124 @@
+# frozen_string_literal: true
+
+module Cocina
+  # Given a Cocina model, create an ActiveFedora model.
+  class ObjectCreator
+    # TODO: We shouldn't need to provide a bogus externalIdentifier
+    def self.create(params)
+      new.create(params)
+    end
+
+    def create(params)
+      obj = case params[:type]
+            when *Cocina::Models::DRO::TYPES
+              build_dro(params)
+            when *Cocina::Models::Collection::TYPES
+              build_collection(params)
+            when *Cocina::Models::AdminPolicy::TYPES
+              build_apo(params)
+            else
+              raise "Unknown type #{params[:type]}"
+            end
+      if validate(obj)
+        af_model = create_from_model(obj)
+
+        # This will rebuild the cocina model from fedora, which shows we are only returning persisted data
+        return Mapper.build(af_model)
+      end
+
+      raise 'not valid'
+    end
+
+    private
+
+    def build_dro(params)
+      Cocina::Models::DRO.from_dynamic(params)
+    end
+
+    def build_collection(params)
+      Cocina::Models::Collection.from_dynamic(params)
+    end
+
+    def build_apo(params)
+      Cocina::Models::AdminPolicy.from_dynamic(params)
+    end
+
+    def validate(obj)
+      # TODO: Check for sourceId conflict
+      # check_source_id obj.identification.sourceId
+
+      # Validate APO exists (this raises an error if it doesn't)
+      Dor.find(obj.administrative.hasAdminPolicy)
+    end
+
+    def create_from_model(obj)
+      af_object = if obj.admin_policy?
+                    create_apo(obj)
+                  elsif obj.dro?
+                    create_dro(obj)
+                  elsif obj.collection?
+                    create_collection(obj)
+                  else
+                    raise "unsupported type #{obj.type}"
+                  end
+
+      # TODO: Synch from symphony if a catkey is present
+      # RefreshMetadataAction.run(identifiers: identifiers, datastream: af_object.descMetadata)
+
+      af_object.save!
+      af_object
+    end
+
+    def create_apo(obj)
+      Dor::AdminPolicyObject.new(pid: Dor::SuriService.mint_id,
+                                 admin_policy_object_id: obj.administrative.hasAdminPolicy,
+                                 # source_id: obj.identification.sourceId,
+                                 label: obj.label).tap do |item|
+        item.descMetadata.mods_title = obj.description.title.first.titleFull
+
+        admin_node = item.administrativeMetadata.ng_xml.xpath('//administrativeMetadata').first
+        admin_node.add_child "<dissemination><workflow id=\"#{obj.administrative.registration_workflow}\"></dissemination>"
+        item.administrativeMetadata.ng_xml_will_change!
+      end
+    end
+
+    def create_dro(obj)
+      Dor::Item.new(pid: Dor::SuriService.mint_id,
+                    admin_policy_object_id: obj.administrative.hasAdminPolicy,
+                    # source_id: obj.identification.sourceId,
+                    label: obj.label).tap do |item|
+        item.descMetadata.mods_title = obj.description.title.first.titleFull
+        item.identityMetadata.tag = content_type_tag(obj.type)
+      end
+    end
+
+    def create_collection(obj)
+      Dor::Collection.new(pid: Dor::SuriService.mint_id,
+                          admin_policy_object_id: obj.administrative.hasAdminPolicy,
+                          # source_id: obj.identification.sourceId,
+                          label: obj.label).tap do |item|
+        item.descMetadata.mods_title = obj.description.title.first.titleFull
+      end
+    end
+
+    def content_type_tag(type)
+      tag = case type
+            when Cocina::Models::Vocab.image
+              'Image'
+            when Cocina::Models::Vocab.three_dimensional
+              '3D'
+            when Cocina::Models::Vocab.map
+              'Map'
+            when Cocina::Models::Vocab.media
+              'Media'
+            when Cocina::Models::Vocab.manuscript
+              'Manuscript'
+            when Cocina::Models::Vocab.book
+              'Book'
+            else
+              Cocina::Models::Vocab.object
+            end
+      "Process : Content Type : #{tag}"
+    end
+  end
+end
---
+++ spec/requests/create_object_spec.rb
@@ -0,0 +1,148 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Create object' do
+  let(:object) { Dor::AdminPolicyObject.new(pid: 'druid:dd999df4567') }
+  let(:data) { item.to_json }
+
+  before do
+    allow(Dor::SuriService).to receive(:mint_id).and_return('druid:gg777gg7777')
+    allow(Dor).to receive(:find).and_return(object)
+  end
+
+  context 'when an item is provided' do
+    let(:item) do
+      Cocina::Models::DRO.new(type: Cocina::Models::Vocab.image,
+                              label: 'This is my label',
+                              version: 1,
+                              description: {
+                                title: [{ titleFull: 'This is my title', primary: true }]
+                              },
+                              administrative: {
+                                hasAdminPolicy: 'druid:dd999df4567'
+                              },
+                              externalIdentifier: 'druid:bc123df4567') # TODO: can we get rid of this?
+    end
+
+    context 'when an object already exists', skip: 'until we support sourceId' do
+      before do
+        # allow(RegistrationService).to receive(:register_object).and_raise(Dor::DuplicateIdError.new('druid:existing123obj'))
+      end
+
+      it 'returns a 409 error' do
+        post '/v1/objects',
+             params: data,
+             headers: { 'Authorization' => "Bearer #{jwt}", 'Content-Type' => 'application/json' }
+        expect(response.status).to eq(409)
+      end
+    end
+
+    context 'when the request is successful' do
+      before do
+        allow_any_instance_of(Dor::Item).to receive(:save!)
+      end
+
+      it 'registers the object with the registration service' do
+        post '/v1/objects',
+             params: data,
+             headers: { 'Authorization' => "Bearer #{jwt}", 'Content-Type' => 'application/json' }
+        expect(response.body).to eq Cocina::Models::DRO.new(item.attributes.merge(externalIdentifier: 'druid:gg777gg7777')).to_json
+
+        expect(response.status).to eq(201)
+        expect(response.location).to eq '/v1/objects/druid:gg777gg7777'
+      end
+    end
+  end
+
+  context 'when a collection is provided' do
+    let(:item) do
+      Cocina::Models::Collection.new(type: Cocina::Models::Vocab.collection,
+                                     label: 'This is my label',
+                                     version: 1,
+                                     description: {
+                                       title: [{ titleFull: 'This is my title', primary: true }]
+                                     },
+                                     administrative: {
+                                       hasAdminPolicy: 'druid:dd999df4567'
+                                     },
+                                     externalIdentifier: 'druid:bc123df4567') # TODO: can we get rid of this?
+    end
+
+    context 'when an object already exists', skip: 'until we support sourceId' do
+      before do
+        # allow(RegistrationService).to receive(:register_object).and_raise(Dor::DuplicateIdError.new('druid:existing123obj'))
+      end
+
+      it 'returns a 409 error' do
+        post '/v1/objects',
+             params: data,
+             headers: { 'Authorization' => "Bearer #{jwt}", 'Content-Type' => 'application/json' }
+        expect(response.status).to eq(409)
+      end
+    end
+
+    context 'when the request is successful' do
+      before do
+        allow_any_instance_of(Dor::Collection).to receive(:save!)
+      end
+
+      it 'registers the object with the registration service' do
+        post '/v1/objects',
+             params: data,
+             headers: { 'Authorization' => "Bearer #{jwt}", 'Content-Type' => 'application/json' }
+        expect(response.body).to eq Cocina::Models::Collection.new(item.attributes.merge(externalIdentifier: 'druid:gg777gg7777')).to_json
+
+        expect(response.status).to eq(201)
+        expect(response.location).to eq '/v1/objects/druid:gg777gg7777'
+      end
+    end
+  end
+
+  context 'when an APO is provided' do
+    let(:item) do
+      Cocina::Models::AdminPolicy.new(type: Cocina::Models::Vocab.admin_policy,
+                                      label: 'This is my label',
+                                      version: 1,
+                                      description: {
+                                        title: [{ titleFull: 'This is my title', primary: true }]
+                                      },
+                                      administrative: {
+                                        hasAdminPolicy: 'druid:dd999df4567',
+                                        registration_workflow: 'assemblyWF'
+                                      },
+                                      externalIdentifier: 'druid:bc123df4567') # TODO: can we get rid of this?
+    end
+
+    context 'when an object already exists', skip: 'until we support sourceId' do
+      before do
+        # allow(RegistrationService).to receive(:register_object).and_raise(Dor::DuplicateIdError.new('druid:existing123obj'))
+      end
+
+      it 'returns a 409 error' do
+        post '/v1/objects',
+             params: data,
+             headers: { 'Authorization' => "Bearer #{jwt}", 'Content-Type' => 'application/json' }
+        expect(response.status).to eq(409)
+      end
+    end
+
+    context 'when the request is successful' do
+      before do
+        allow_any_instance_of(Dor::AdminPolicyObject).to receive(:save!)
+        # This stubs out Solr:
+        allow_any_instance_of(Dor::AdminPolicyObject).to receive(:admin_policy_object_id).and_return('druid:dd999df4567')
+      end
+
+      it 'registers the object with the registration service' do
+        post '/v1/objects',
+             params: data,
+             headers: { 'Authorization' => "Bearer #{jwt}", 'Content-Type' => 'application/json' }
+        expect(response.body).to eq Cocina::Models::AdminPolicy.new(item.attributes.merge(externalIdentifier: 'druid:gg777gg7777')).to_json
+
+        expect(response.status).to eq(201)
+        expect(response.location).to eq '/v1/objects/druid:gg777gg7777'
+      end
+    end
+  end
+end
