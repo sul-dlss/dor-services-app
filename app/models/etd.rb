ba12bb95305dbd1d257891a88104d42f00545b56
Justin Coyne <jcoyne@justincoyne.com>
2020-03-07 09:15:14 -0600

    Add contentMetadata to ETDs
    So that the legacy metadata update endpoint can be used

--- app/models/etd.rb
+++ app/models/etd.rb
@@ -5,4 +5,12 @@
 # and return a Dor::Item.
 # This is necessary for the Cocina::Mapper because Etds do not use descMetadata
 # like ever other object.
-class Etd < Dor::Etd; end
+class Etd < Dor::Etd
+  # This is required so that LegacyMetadataService can write contentMetadata.
+  # We need it because Dor::Etd's parent is Dor::Abstract rather than Dor::Item
+  # The other-metadata robot in the etdSubmitWF calls the legacy metadata update.
+  has_metadata name: 'contentMetadata',
+               type: Dor::ContentMetadataDS,
+               label: 'Content Metadata',
+               control_group: 'M'
+end
