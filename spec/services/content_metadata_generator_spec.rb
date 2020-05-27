4ca61885c9be80354869df4905117f3546a6084c
Justin Coyne <jcoyne@justincoyne.com>
2020-05-27 11:20:35 -0500

    Add the correct resource types for geo objects

--- spec/services/content_metadata_generator_spec.rb
+++ spec/services/content_metadata_generator_spec.rb
@@ -247,21 +247,95 @@ RSpec.describe ContentMetadataGenerator do
     # https://argo.stanford.edu/view/druid:bb033gt0615
     let(:object_type) { Cocina::Models::Vocab.geo }
 
+    let(:file1) do
+      {
+        'version' => 1,
+        'type' => 'http://cocina.sul.stanford.edu/models/file.jsonld',
+        'filename' => '00001.zip',
+        'label' => '00001.zip',
+        'hasMimeType' => 'application/zip',
+        'size' => 997,
+        'administrative' => {
+          'sdrPreserve' => true,
+          'shelve' => false
+        },
+        'access' => {
+          'access' => 'dark'
+        },
+        'hasMessageDigests' => [
+          {
+            'type' => 'sha1',
+            'digest' => 'cb19c405f8242d1f9a0a6180122dfb69e1d6e4c7'
+          },
+          {
+            'type' => 'md5',
+            'digest' => 'e6d52da47a5ade91ae31227b978fb023'
+          }
+
+        ]
+      }
+    end
+
+    let(:file3) do
+      {
+        'version' => 1,
+        'type' => 'http://cocina.sul.stanford.edu/models/file.jsonld',
+        'filename' => '00002.xml',
+        'label' => '00002.xml',
+        'hasMimeType' => 'text/xml',
+        'size' => 1914,
+        'administrative' => {
+          'sdrPreserve' => true,
+          'shelve' => false
+        },
+        'access' => {
+          'access' => 'world'
+        },
+        'hasMessageDigests' => []
+      }
+    end
+
+    let(:filesets) do
+      [
+        {
+          'version' => 1,
+          'type' => 'http://cocina.sul.stanford.edu/models/fileset.jsonld',
+          'label' => 'Data',
+          'structural' => { 'contains' => [file1] }
+        },
+        {
+          'version' => 1,
+          'type' => 'http://cocina.sul.stanford.edu/models/fileset.jsonld',
+          'label' => 'Preview',
+          'structural' => { 'contains' => [file2, file4] }
+        },
+        {
+          'version' => 1,
+          'type' => 'http://cocina.sul.stanford.edu/models/fileset.jsonld',
+          'label' => 'Attachment',
+          'structural' => { 'contains' => [file3] }
+        }
+      ]
+    end
+
     it 'generates contentMetadata.xml' do
       expect(generate).to be_equivalent_to '<?xml version="1.0"?>
          <contentMetadata objectId="druid:bc123de5678" type="geo">
-           <resource id="bc123de5678_1" sequence="1" type="file">
-             <label>Page 1</label>
-             <file id="00001.html" mimetype="text/html" size="997" preserve="yes" publish="no" shelve="no" role="transcription">
+           <resource id="bc123de5678_1" sequence="1" type="object">
+             <label>Data</label>
+             <file id="00001.zip" mimetype="application/zip" size="997" publish="no" shelve="no" preserve="yes">
                <checksum type="sha1">cb19c405f8242d1f9a0a6180122dfb69e1d6e4c7</checksum>
                <checksum type="md5">e6d52da47a5ade91ae31227b978fb023</checksum>
              </file>
-             <file id="00001.jp2" mimetype="image/jp2"  size="149570" preserve="yes" publish="yes" shelve="yes"/>
            </resource>
-           <resource id="bc123de5678_2" sequence="2" type="file">
-             <label>Page 2</label>
-             <file id="00002.html" mimetype="text/html" size="1914" preserve="yes" publish="yes" shelve="no"/>
-             <file id="00002.jp2" mimetype="image/jp2" size="111467" preserve="yes" publish="yes" shelve="yes"/>
+           <resource id="bc123de5678_2" sequence="2" type="preview">
+             <label>Preview</label>
+             <file id="00001.jp2" mimetype="image/jp2" size="149570" publish="yes" shelve="yes" preserve="yes"/>
+             <file id="00002.jp2" mimetype="image/jp2" size="111467" publish="yes" shelve="yes" preserve="yes"/>
+           </resource>
+           <resource id="bc123de5678_3" sequence="3" type="attachment">
+             <label>Attachment</label>
+             <file id="00002.xml" mimetype="text/xml" size="1914" publish="yes" shelve="no" preserve="yes"/>
            </resource>
          </contentMetadata>'
     end
