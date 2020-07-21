d38e54ec8f66f4f58117d5cd51038c284f25fc73
Justin Coyne <jcoyne@justincoyne.com>
2020-07-21 15:06:17 -0500

    Add pointer to consul docs on rightsMetadata

--- app/services/cocina/to_fedora/access.rb
+++ app/services/cocina/to_fedora/access.rb
@@ -5,6 +5,7 @@ module Cocina
     # This transforms the DRO.access schema to the
     # Fedora 3 data model rightsMetadata
     class Access
+      # TODO: this should be expanded to support file level rights: https://consul.stanford.edu/pages/viewpage.action?spaceKey=chimera&title=Rights+metadata+--+the+rightsMetadata+datastream
       def self.apply(item, access)
         rights_type = case access.access
                       when 'location-based'
