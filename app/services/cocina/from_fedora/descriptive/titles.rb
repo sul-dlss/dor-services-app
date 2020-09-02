50dd2ca742dab45f428cdd39750d1a28b95f00dc
Justin Coyne <jcoyne@justincoyne.com>
2020-09-02 00:01:29 -0500

    Add support for titles with parts as described by the metadata department
    https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_titleInfo.txt#L16-L56

--- app/services/cocina/from_fedora/descriptive/titles.rb
+++ app/services/cocina/from_fedora/descriptive/titles.rb
@@ -5,6 +5,14 @@ module Cocina
     class Descriptive
       # Maps titles
       class Titles
+        TYPES = {
+          'nonSort' => 'nonsorting characters',
+          'title' => 'main title',
+          'subTitle' => 'subtitle',
+          'partNumber' => 'part number',
+          'partName' => 'part name'
+        }.freeze
+
         # @param [Nokogiri::XML::Document] ng_xml the descriptive metadata XML
         # @return [Hash] a hash that can be mapped to a cocina model
         def self.build(ng_xml)
@@ -21,12 +29,40 @@ module Cocina
           child_nodes = ng_xml.xpath('//mods:mods/mods:titleInfo[not(@type)]/child::node()[not(self::text())][child::node()[self::text()]]', mods: DESC_METADATA_NS)
           return [{ value: child_nodes.first.text }] if child_nodes.map(&:name) == ['title']
 
+          # Title with parts: multiple subelements in titleInfo
+          # Filtering out text node children of titleInfo and the children that themselves have no text.
+          child_nodes = ng_xml.xpath('//mods:mods/mods:titleInfo[not(@type)]/child::node()[not(self::text())][child::node()[self::text()]]', mods: DESC_METADATA_NS)
+          return [{ structuredValue: structured_value(child_nodes) }] if child_nodes.any?
+
           raise Mapper::MissingTitle
         end
 
         private
 
         attr_reader :ng_xml
+
+        def structured_value(child_nodes)
+          new_nodes = child_nodes.map do |node|
+            { value: node.text, type: TYPES[node.name] }
+          end
+
+          unsortable = child_nodes.select { |node| node.name == 'nonSort' }
+          if unsortable.any?
+            new_nodes << {
+              "note": [
+                {
+                  "value": unsortable.sum do |node|
+                    add = node.text.end_with?('-') || node.text.end_with?("'") ? 0 : 1
+                    node.text.size + add
+                  end,
+                  "type": 'nonsorting character count'
+                }
+              ]
+            }
+          end
+
+          new_nodes
+        end
       end
     end
   end
--- spec/services/cocina/from_fedora/descriptive/titles_spec.rb
+++ spec/services/cocina/from_fedora/descriptive/titles_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Cocina::FromFedora::Descriptive::Titles do
+  let(:object) { Dor::Item.new }
+
   describe '.build' do
     subject(:build) { described_class.build(ng_xml) }
 
@@ -11,6 +13,33 @@ RSpec.describe Cocina::FromFedora::Descriptive::Titles do
 
       it 'raises and error' do
         expect { build }.to raise_error Cocina::Mapper::MissingTitle
+      end
+    end
+
+    context 'when the title has parts' do
+      let(:ng_xml) do
+        Nokogiri::XML <<~XML
+          <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xmlns="http://www.loc.gov/mods/v3" version="3.6"
+            xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+            <titleInfo>
+              <nonSort>The</nonSort>
+              <title>journal of stuff</title>
+              <partNumber>volume 5</partNumber>
+              <partName>special issue</partName>
+            </titleInfo>
+          </mods>
+        XML
+      end
+
+      it 'is a structured value' do
+        expect(build).to eq [
+          { structuredValue: [{ type: 'nonsorting characters', value: 'The' },
+                              { type: 'main title', value: 'journal of stuff' },
+                              { type: 'part number', value: 'volume 5' },
+                              { type: 'part name', value: 'special issue' },
+                              { note: [{ type: 'nonsorting character count', value: 4 }] }] }
+        ]
       end
     end
   end
