849d6339215ca3395087d4a61681fe1609386764
Justin Coyne <jcoyne@justincoyne.com>
2020-09-02 00:01:29 -0500

    Add support for titles with parts as described by the metadata department
    https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_titleInfo.txt#L16-L56

--- spec/services/cocina/from_fedora/descriptive/titles_spec.rb
+++ spec/services/cocina/from_fedora/descriptive/titles_spec.rb
@@ -3,14 +3,41 @@
 require 'rails_helper'
 
 RSpec.describe Cocina::FromFedora::Descriptive::Titles do
+  let(:object) { Dor::Item.new }
+
   describe '.build' do
     subject(:build) { described_class.build(object) }
 
     context 'when the object has no title' do
-      let(:object) { Dor::Item.new }
-
       it 'raises and error' do
         expect { build }.to raise_error Cocina::Mapper::MissingTitle
+      end
+    end
+
+    context 'when the title has parts' do
+      before do
+        object.descMetadata.content = <<~XML
+          <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xmlns="http://www.loc.gov/mods/v3" version="3.6"
+            xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+            <titleInfo>
+              <nonSort>The</nonSort>
+              <title>journal of stuff</title>
+              <partNumber>volume 5</partNumber>
+              <partName>special issue</partName>
+            </titleInfo>
+          </mods>
+        XML
+      end
+
+      it 'is a structured value' do
+        expect(build).to eq [
+          { structuredValue: [{ type: 'nonsorting characters', value: 'The' },
+                              { type: 'main title', value: 'journal of stuff' },
+                              { type: 'part', value: 'volume 5' },
+                              { type: 'part', value: 'special issue' },
+                              { note: [{ type: 'nonsorting character count', value: 4 }] }] }
+        ]
       end
     end
   end
