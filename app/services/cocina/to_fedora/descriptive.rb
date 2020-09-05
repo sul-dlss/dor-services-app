751a97bf4eb4dddfd23e1e5fbb7d8d5674d1b2aa
Justin Coyne <jcoyne@justincoyne.com>
2020-09-04 22:38:25 -0500

    Map Cocina::Models::Description to descMetadata XML

---
+++ app/services/cocina/to_fedora/descriptive.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Cocina
+  module ToFedora
+    # This transforms the DRO.descriptive schema to the
+    # Fedora 3 data model descMetadata
+    class Descriptive
+      TAG_NAME = {
+        'nonsorting characters' => :nonSort,
+        'main title' => :title,
+        'part name' => 'partName',
+        'part number' => 'partNumber'
+      }.freeze
+      # @param [Cocina::Models::Descriptive] descriptive
+      # @return [Nokogiri::XML::Document]
+      def self.transform(descriptive)
+        new(descriptive).transform
+      end
+
+      def initialize(descriptive)
+        @descriptive = descriptive
+      end
+
+      def transform
+        Nokogiri::XML::Builder.new do |xml|
+          xml.mods('xmlns' => 'http://www.loc.gov/mods/v3',
+                   'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+                   'version' => '3.6',
+                   'xsi:schemaLocation' => 'http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd') do
+            descriptive.title.each do |title|
+              xml.titleInfo do
+                title.structuredValue.each do |component|
+                  xml.public_send(TAG_NAME.fetch(component.type), component.value) if component.type
+                end
+              end
+            end
+          end
+        end
+      end
+
+      private
+
+      attr_reader :descriptive
+    end
+  end
+end
---
+++ spec/services/cocina/to_fedora/descriptive_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Cocina::ToFedora::Descriptive do
+  subject(:xml) { described_class.transform(descriptive).to_xml }
+
+  context 'when the title has a structured value' do
+    let(:descriptive) do
+      Cocina::Models::Description.new(
+        title: [
+          { structuredValue: [{ type: 'nonsorting characters', value: 'The' },
+                              { type: 'main title', value: 'journal of stuff' },
+                              { type: 'part number', value: 'volume 5' },
+                              { type: 'part name', value: 'special issue' },
+                              { note: [{ type: 'nonsorting character count', value: '4' }] }] }
+        ]
+      )
+    end
+
+    it 'builds the xml' do
+      expect(xml).to be_equivalent_to <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <titleInfo>
+            <nonSort>The</nonSort>
+            <title>journal of stuff</title>
+            <partNumber>volume 5</partNumber>
+            <partName>special issue</partName>
+          </titleInfo>
+        </mods>
+      XML
+    end
+  end
+end
