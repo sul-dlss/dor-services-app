cafbbee2d95ee1e527d90a40f21b6f86d41620db
Justin Coyne <jcoyne@justincoyne.com>
2020-09-08 22:21:35 -0500

    Add translated

--- spec/services/cocina/to_fedora/descriptive_spec.rb
+++ spec/services/cocina/to_fedora/descriptive_spec.rb
@@ -87,4 +87,88 @@ RSpec.describe Cocina::ToFedora::Descriptive do
       XML
     end
   end
+
+  context 'when the title is translated' do
+    let(:descriptive) do
+      Cocina::Models::Description.new(
+        title: [
+          {
+            "parallelValue": [
+              {
+                "structuredValue": [
+                  {
+                    "value": 'Les',
+                    "type": 'nonsorting characters'
+                  },
+                  {
+                    "value": 'misérables',
+                    "type": 'main title'
+                  },
+                  {
+                    "note": [
+                      {
+                        "value": '4',
+                        "type": 'nonsorting character count'
+                      }
+                    ]
+                  }
+                ],
+                "status": 'primary',
+                "valueLanguage": {
+                  "code": 'fre',
+                  "source": {
+                    "code": 'iso639-2b'
+                  }
+                }
+              },
+              {
+                "structuredValue": [
+                  {
+                    "value": 'The',
+                    "type": 'nonsorting characters'
+                  },
+                  {
+                    "value": 'wretched',
+                    "type": 'main title'
+                  },
+                  {
+                    "note": [
+                      {
+                        "value": '4',
+                        "type": 'nonsorting character count'
+                      }
+                    ]
+                  }
+                ],
+                "type": 'translated',
+                "valueLanguage": {
+                  "code": 'eng',
+                  "source": {
+                    "code": 'iso639-2b'
+                  }
+                }
+              }
+            ]
+          }
+        ]
+      )
+    end
+
+    it 'builds the xml' do
+      expect(xml).to be_equivalent_to <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <titleInfo usage="primary" lang="fre" altRepGroup="0">
+            <nonSort>Les</nonSort>
+            <title>mis&#xE9;rables</title>
+          </titleInfo>
+          <titleInfo type="translated" lang="eng" altRepGroup="0">
+            <nonSort>The</nonSort>
+            <title>wretched</title>
+          </titleInfo>
+        </mods>
+      XML
+    end
+  end
 end
