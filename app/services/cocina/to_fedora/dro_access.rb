5dca1849523907e90a697b0b8551349692ab556e
Justin Coyne <jcoyne@justincoyne.com>
2020-09-23 15:09:54 -0500

    Introduce ToFedora::DROAccess as a subclass

---
+++ app/services/cocina/to_fedora/dro_access.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Cocina
+  module ToFedora
+    # This transforms the DROAccess schema to the
+    # Fedora 3 data model rightsMetadata
+    class DROAccess < Access
+
+    end
+  end
+end
