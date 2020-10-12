6692b084b2ed113254d0ee3de9a1d975cf508064
Justin Coyne <jcoyne@justincoyne.com>
2020-10-12 16:10:16 -0500

    Map cocina name-title subject with additional terms including genre subdivision, authority for terms to MODS XML

--- spec/services/cocina/to_fedora/descriptive/subject_spec.rb
+++ spec/services/cocina/to_fedora/descriptive/subject_spec.rb
@@ -594,4 +594,60 @@ RSpec.describe Cocina::ToFedora::Descriptive::Subject do
       XML
     end
   end
+
+  context 'when it has a name-title subject with additional terms including genre subdivision, authority for terms' do
+    let(:subjects) do
+      [
+        Cocina::Models::DescriptiveValue.new(
+          structuredValue: [
+            {
+              "value": 'Shakespeare, William, 1564-1616',
+              "type": 'person',
+              "uri": 'http://id.loc.gov/authorities/names/n78095332',
+              "source": {
+                "code": 'naf',
+                "uri": 'http://id.loc.gov/authorities/names/'
+              }
+            },
+            {
+              "value": 'Hamlet',
+              "type": 'title',
+              "uri": 'http://id.loc.gov/authorities/names/n80008522',
+              "source": {
+                "code": 'naf',
+                "uri": 'http://id.loc.gov/authorities/names/'
+              }
+            },
+            {
+              "value": 'Bibliographies',
+              "type": 'genre',
+              "uri": 'http://id.loc.gov/authorities/subjects/sh99001362',
+              "source": {
+                "code": 'lcsh',
+                "uri": 'http://id.loc.gov/authorities/subjects/'
+              }
+            }
+          ]
+        )
+      ]
+    end
+
+    it 'builds the xml' do
+      expect(xml).to be_equivalent_to <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <subject authority="naf">
+            <name type="personal" authority="naf" authorityURI="http://id.loc.gov/authorities/names/" valueURI="http://id.loc.gov/authorities/names/n78095332">
+              <namePart>Shakespeare, William, 1564-1616</namePart>
+            </name>
+            <titleInfo authority="naf" authorityURI="http://id.loc.gov/authorities/names/" valueURI="http://id.loc.gov/authorities/names/n80008522">
+              <title>Hamlet</title>
+            </titleInfo>
+            <genre authority="lcsh" authorityURI="http://id.loc.gov/authorities/subjects/" valueURI="http://id.loc.gov/authorities/subjects/sh99001362">Bibliographies</genre>
+          </subject>
+        </mods>
+      XML
+    end
+  end
 end
