6b062ea4aff8952d0c64d3bd784dde7607a892ea
Justin Coyne <jcoyne@justincoyne.com>
2020-10-12 21:18:04 -0500

    Map cocina event date range with a qualifier to MODS XML

--- spec/services/cocina/to_fedora/descriptive/event_spec.rb
+++ spec/services/cocina/to_fedora/descriptive/event_spec.rb
@@ -240,4 +240,46 @@ RSpec.describe Cocina::ToFedora::Descriptive::Event do
       XML
     end
   end
+
+  context 'when it has an approximate date range' do
+    let(:events) do
+      [
+        Cocina::Models::Event.new(
+          {
+            type: 'creation',
+            date: [
+              {
+                structuredValue: [
+                  {
+                    value: '1940',
+                    type: 'start',
+                    status: 'primary',
+                    qualifier: 'approximate'
+                  },
+                  {
+                    value: '1945',
+                    type: 'end',
+                    qualifier: 'approximate'
+                  }
+                ]
+              }
+            ]
+          }
+        )
+      ]
+    end
+
+    it 'builds the xml' do
+      expect(xml).to be_equivalent_to <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <originInfo>
+          <dateCreated keyDate="yes" point="start" qualifier="approximate">1940</dateCreated>
+          <dateCreated point="end" qualifier="approximate">1945</dateCreated>
+          </originInfo>
+        </mods>
+      XML
+    end
+  end
 end
