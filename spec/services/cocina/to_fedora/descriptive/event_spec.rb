6e69a48ecb21c1c0f57aff8c5ac8b394e7dd2aca
Justin Coyne <jcoyne@justincoyne.com>
2020-10-13 09:52:11 -0500

    Map cocina event with multiple contributors to MODS XML

--- spec/services/cocina/to_fedora/descriptive/event_spec.rb
+++ spec/services/cocina/to_fedora/descriptive/event_spec.rb
@@ -755,4 +755,67 @@ RSpec.describe Cocina::ToFedora::Descriptive::Event do
       XML
     end
   end
+
+  context 'when it has multiple publishers' do
+    let(:events) do
+      [
+        Cocina::Models::Event.new(
+          "type": 'publication',
+          "contributor": [
+            {
+              "name": [
+                {
+                  "value": 'Ardis'
+                }
+              ],
+              "type": 'organization',
+              "role": [
+                {
+                  "value": 'publisher',
+                  "code": 'pbl',
+                  "uri": 'http://id.loc.gov/vocabulary/relators/pbl',
+                  "source": {
+                    "code": 'marcrelator',
+                    "uri": 'http://id.loc.gov/vocabulary/relators/'
+                  }
+                }
+              ]
+            },
+            {
+              "name": [
+                {
+                  "value": 'Commonplace Books'
+                }
+              ],
+              "type": 'organization',
+              "role": [
+                {
+                  "value": 'publisher',
+                  "code": 'pbl',
+                  "uri": 'http://id.loc.gov/vocabulary/relators/pbl',
+                  "source": {
+                    "code": 'marcrelator',
+                    "uri": 'http://id.loc.gov/vocabulary/relators/'
+                  }
+                }
+              ]
+            }
+          ]
+        )
+      ]
+    end
+
+    it 'builds the xml' do
+      expect(xml).to be_equivalent_to <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <originInfo>
+            <publisher>Ardis</publisher>
+            <publisher>Commonplace Books</publisher>
+          </originInfo>
+        </mods>
+      XML
+    end
+  end
 end
