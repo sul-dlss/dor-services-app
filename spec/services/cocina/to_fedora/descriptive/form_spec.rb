5bf85c48ae92aa94567459c8ecd53d9c7e996810
Justin Coyne <jcoyne@justincoyne.com>
2020-10-16 08:39:16 -0500

    Map form of type genere from cocina to MODS

--- spec/services/cocina/to_fedora/descriptive/form_spec.rb
+++ spec/services/cocina/to_fedora/descriptive/form_spec.rb
@@ -29,51 +29,179 @@ RSpec.describe Cocina::ToFedora::Descriptive::Form do
     end
   end
 
-  context 'with an object with one type' do
-    let(:forms) do
-      [
-        Cocina::Models::DescriptiveValue.new(
-          "value": 'text',
-          "type": 'resource type',
-          "source": {
-            "value": 'MODS resource type'
-          }
-        )
-      ]
+  describe 'typeOfResource' do
+    context 'with an object with one type' do
+      let(:forms) do
+        [
+          Cocina::Models::DescriptiveValue.new(
+            "value": 'text',
+            "type": 'resource type',
+            "source": {
+              "value": 'MODS resource type'
+            }
+          )
+        ]
+      end
+
+      it 'builds the xml' do
+        expect(xml).to be_equivalent_to <<~XML
+          <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xmlns="http://www.loc.gov/mods/v3" version="3.6"
+            xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+            <typeOfResource>text</typeOfResource>
+          </mods>
+        XML
+      end
     end
 
-    it 'builds the xml' do
-      expect(xml).to be_equivalent_to <<~XML
-        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-          xmlns="http://www.loc.gov/mods/v3" version="3.6"
-          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
-          <typeOfResource>text</typeOfResource>
-        </mods>
-      XML
+    context 'with an object with multiple types' do
+      xit 'https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_typeOfResource.txt#L17'
+    end
+
+    context 'with an object with multiple types and one predominant' do
+      xit 'https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_typeOfResource.txt#L39'
+    end
+
+    context 'with a manuscript' do
+      xit 'https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_typeOfResource.txt#L62'
+    end
+
+    context 'with an attribute without a value' do
+      xit 'https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_typeOfResource.txt#L79'
+    end
+
+    context 'with a collection' do
+      xit 'https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_typeOfResource.txt#L89'
+    end
+
+    context 'with display label' do
+      xit 'https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_typeOfResource.txt#L106'
     end
   end
 
-  context 'with an object with multiple types' do
-    xit 'https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_typeOfResource.txt#L17'
-  end
+  describe 'genre' do
+    context 'with a single value' do
+      let(:forms) do
+        [
+          Cocina::Models::DescriptiveValue.new(
+            "value": 'photographs',
+            "type": 'genre'
+          )
+        ]
+      end
 
-  context 'with an object with multiple types and one predominant' do
-    xit 'https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_typeOfResource.txt#L39'
-  end
+      it 'builds the xml' do
+        expect(xml).to be_equivalent_to <<~XML
+          <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xmlns="http://www.loc.gov/mods/v3" version="3.6"
+            xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+            <genre>photographs</genre>
+          </mods>
+        XML
+      end
+    end
 
-  context 'with a manuscript' do
-    xit 'https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_typeOfResource.txt#L62'
-  end
+    context 'with multiple values' do
+      let(:forms) do
+        [
+          Cocina::Models::DescriptiveValue.new(
+            "value": 'photographs',
+            "type": 'genre'
+          ),
+          Cocina::Models::DescriptiveValue.new(
+            "value": 'prints',
+            "type": 'genre'
+          )
+        ]
+      end
 
-  context 'with an sttribute without a value' do
-    xit 'https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_typeOfResource.txt#L79'
-  end
+      it 'builds the xml' do
+        expect(xml).to be_equivalent_to <<~XML
+          <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xmlns="http://www.loc.gov/mods/v3" version="3.6"
+            xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+            <genre>photographs</genre>
+            <genre>prints</genre>
+                      </mods>
+        XML
+      end
+    end
 
-  context 'with a collection' do
-    xit 'https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_typeOfResource.txt#L89'
-  end
+    context 'with type' do
+      let(:forms) do
+        [
+          Cocina::Models::DescriptiveValue.new(
+            "value": 'Art Deco',
+            "type": 'style'
+          )
+        ]
+      end
 
-  context 'with display label' do
-    xit 'https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_typeOfResource.txt#L106'
+      it 'builds the xml' do
+        expect(xml).to be_equivalent_to <<~XML
+          <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xmlns="http://www.loc.gov/mods/v3" version="3.6"
+            xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+            <genre type="style">Art Deco</genre>
+          </mods>
+        XML
+      end
+    end
+
+    context 'with authorities' do
+      let(:forms) do
+        [
+          Cocina::Models::DescriptiveValue.new(
+            "value": 'Photographs',
+            "type": 'genre',
+            "uri": 'http://id.loc.gov/authorities/genreForms/gf2017027249',
+            "source": {
+              "code": 'lcgft',
+              "uri": 'http://id.loc.gov/authorities/genreForms/'
+            }
+          )
+        ]
+      end
+
+      it 'builds the xml' do
+        expect(xml).to be_equivalent_to <<~XML
+          <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xmlns="http://www.loc.gov/mods/v3" version="3.6"
+            xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+            <genre authority="lcgft" authorityURI="http://id.loc.gov/authorities/genreForms/" valueURI="http://id.loc.gov/authorities/genreForms/gf2017027249">Photographs</genre>
+          </mods>
+        XML
+      end
+    end
+
+    context 'with usage' do
+      xit 'TODO https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_genre.txt#L57'
+    end
+
+    context 'with multilingual' do
+      xit 'TODO https://github.com/sul-dlss-labs/cocina-descriptive-metadata/blob/master/mods_cocina_mappings/mods_to_cocina_genre.txt#L74'
+    end
+
+    context 'with displayLabel' do
+      let(:forms) do
+        [
+          Cocina::Models::DescriptiveValue.new(
+            "value": 'Art deco',
+            "type": 'genre',
+            "displayLabel": 'Style'
+          )
+        ]
+      end
+
+      it 'builds the xml' do
+        expect(xml).to be_equivalent_to <<~XML
+          <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xmlns="http://www.loc.gov/mods/v3" version="3.6"
+            xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+            <genre displayLabel="Style">Art deco</genre>
+          </mods>
+        XML
+      end
+    end
   end
 end
