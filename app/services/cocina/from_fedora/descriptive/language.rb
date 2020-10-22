bfca90c1916ad87f7abb2f3e4cb2e882a866e855
Naomi Dushay <ndushay@stanford.edu>
2020-10-22 14:05:37 -0700

    implement language mappings to_ and from_fedora per Arcadia's examples

--- app/services/cocina/from_fedora/descriptive/language.rb
+++ app/services/cocina/from_fedora/descriptive/language.rb
@@ -6,11 +6,17 @@ module Cocina
       # Maps languages
       class Language
         LANG_XPATH = '//mods:language'
-        LANG_TEXT_XPATH = './mods:languageTerm[@type="text"]/text()'
-        LANG_TEXT_AUTHORITY_URI_XPATH = './mods:languageTerm[@type="text"]/@authorityURI'
-        LANG_TEXT_VALUE_URI_XPATH = './mods:languageTerm[@type="text"]/@valueURI'
-        LANG_CODE_XPATH = './mods:languageTerm[@type="code"]/text()'
-        LANG_CODE_AUTHORITY_XPATH = './mods:languageTerm[@type="code"]/@authority'
+        LANG_STATUS_XPATH = './@status'
+        OBJECT_PART_XPATH = './@objectPart'
+        DISPLAY_LABEL_XPATH = './@displayLabel'
+        LANGUAGE_TERM_XPATH = "#{LANG_XPATH}/mods:languageTerm"
+        SCRIPT_TERM_XPATH = "#{LANG_XPATH}/mods:scriptTerm"
+        LANG_TERM_TEXT_XPATH = './mods:languageTerm[@type="text"]/text()'
+        LANG_TERM_CODE_XPATH = './mods:languageTerm[@type="code"]/text()'
+        LANG_TERM_CODE_AUTHORITY_XPATH = './mods:languageTerm[@type="code"]/@authority'
+        LANG_TERM_TEXT_AUTHORITY_XPATH = './mods:languageTerm[@type="text"]/@authority'
+        TEXT_AUTHORITY_URI_XPATH = './*[@type="text"]/@authorityURI' # can be for languageTerm or scriptTerm
+        TEXT_VALUE_URI_XPATH = './*[@type="text"]/@valueURI' # can be for languageTerm or scriptTerm
 
         # @param [Nokogiri::XML::Document] ng_xml the descriptive metadata XML
         # @return [Hash] a hash that can be mapped to a cocina model
@@ -25,10 +31,12 @@ module Cocina
         def build
           [].tap do |langs|
             languages.each do |lang|
-              langs << { code: language_code_for(lang),
-                         value: language_text_for(lang),
-                         uri: language_uri_for(lang),
-                         source: language_source_for(lang) }.reject { |_k, v| v.blank? }
+              attribs = {}
+              attribs = lang_term_attributes_for(lang) if language_term?(lang)
+              attribs[:status] = language_status_for(lang) if language_status_for(lang).present?
+              attribs[:script] = script_term_attributes_for(script_term_nodes(lang)) if script_term?(lang)
+
+              langs << attribs
             end
           end
         end
@@ -37,27 +45,89 @@ module Cocina
 
         attr_reader :ng_xml
 
+        def lang_term_attributes_for(lang)
+          {
+            code: language_code_for(lang),
+            value: language_text_for(lang),
+            uri: language_value_uri_for(lang),
+            appliesTo: language_applies_to(lang),
+            displayLabel: language_display_label(lang),
+            source: language_source_for(lang)
+          }.reject { |_k, v| v.blank? }
+        end
+
+        def script_term_attributes_for(script_term_nodes)
+          return if script_term_nodes.blank?
+
+          code, value, authority = nil
+          script_term_nodes.each do |script_term_node|
+            code ||= script_term_node.content if script_term_node['type'] == 'code'
+            value ||= script_term_node.content if script_term_node['type'] == 'text'
+            authority ||= script_term_node['authority']
+          end
+          source = { code: authority } if authority
+          {
+            code: code,
+            value: value,
+            source: source
+          }.compact
+        end
+
         def languages
           @languages ||= ng_xml.xpath(LANG_XPATH, mods: DESC_METADATA_NS)
         end
 
         def language_code_for(lang)
-          lang.xpath(LANG_CODE_XPATH, mods: DESC_METADATA_NS).to_s
+          lang.xpath(LANG_TERM_CODE_XPATH, mods: DESC_METADATA_NS).to_s if language_term?(lang)
         end
 
         def language_text_for(lang)
-          lang.xpath(LANG_TEXT_XPATH, mods: DESC_METADATA_NS).to_s
+          lang.xpath(LANG_TERM_TEXT_XPATH, mods: DESC_METADATA_NS).to_s if language_term?(lang)
         end
 
-        def language_uri_for(lang)
-          lang.xpath(LANG_TEXT_VALUE_URI_XPATH, mods: DESC_METADATA_NS).to_s
+        # this can be present for type text and/or code, but we only want one.
+        def language_value_uri_for(lang)
+          result = lang.xpath(TEXT_VALUE_URI_XPATH, mods: DESC_METADATA_NS).to_s
+          result = lang.xpath('./*/@valueURI', mods: DESC_METADATA_NS).to_s if result.blank?
+          result
+        end
+
+        def language_applies_to(lang)
+          value = lang.xpath(OBJECT_PART_XPATH, mods: DESC_METADATA_NS).to_s
+          [value: value] if value.present?
+        end
+
+        def language_display_label(lang)
+          lang.xpath(DISPLAY_LABEL_XPATH, mods: DESC_METADATA_NS).to_s
+        end
+
+        def language_status_for(lang)
+          lang.xpath(LANG_STATUS_XPATH, mods: DESC_METADATA_NS).to_s
         end
 
         def language_source_for(lang)
+          code = lang.xpath(LANG_TERM_CODE_AUTHORITY_XPATH, mods: DESC_METADATA_NS).to_s if language_term?(lang)
+          # in case there is only a text node
+          code = lang.xpath(LANG_TERM_TEXT_AUTHORITY_XPATH, mods: DESC_METADATA_NS).to_s if code.blank? && language_term?(lang)
+          # this can be present for type text and/or code, but we only want one.
+          uri = lang.xpath(TEXT_AUTHORITY_URI_XPATH, mods: DESC_METADATA_NS).to_s
+          uri = lang.xpath('./*/@authorityURI', mods: DESC_METADATA_NS).to_s if uri.blank?
           {
-            code: lang.xpath(LANG_CODE_AUTHORITY_XPATH, mods: DESC_METADATA_NS).to_s,
-            uri: lang.xpath(LANG_TEXT_AUTHORITY_URI_XPATH, mods: DESC_METADATA_NS).to_s
+            code: code,
+            uri: uri
           }.reject { |_k, v| v.blank? }
+        end
+
+        def language_term?(lang)
+          lang.xpath(LANGUAGE_TERM_XPATH, mods: DESC_METADATA_NS).to_s.present?
+        end
+
+        def script_term?(lang)
+          script_term_nodes(lang).to_s.present?
+        end
+
+        def script_term_nodes(lang)
+          lang.xpath(SCRIPT_TERM_XPATH, mods: DESC_METADATA_NS)
         end
       end
     end
---
+++ spec/services/cocina/from_fedora/descriptive/language_spec.rb
@@ -0,0 +1,278 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Cocina::FromFedora::Descriptive::Language do
+  subject(:build) { described_class.build(ng_xml) }
+
+  let(:ng_xml) do
+    Nokogiri::XML <<~XML
+      <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xmlns="http://www.loc.gov/mods/v3" version="3.6"
+        xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+        #{xml}
+      </mods>
+    XML
+  end
+
+  context 'when single language with term, code, and authority uris' do
+    let(:xml) do
+      <<~XML
+        <language>
+          <languageTerm authority="iso639-2b" authorityURI="http://id.loc.gov/vocabulary/iso639-2" valueURI="http://id.loc.gov/vocabulary/iso639-2/ara" type="code">ara</languageTerm>
+          <languageTerm authority="iso639-2b" authorityURI="http://id.loc.gov/vocabulary/iso639-2" valueURI="http://id.loc.gov/vocabulary/iso639-2/ara" type="text">Arabic</languageTerm>
+        </language>
+      XML
+    end
+
+    it 'builds the cocina data structure' do
+      expect(build).to eq [
+        {
+          "code": 'ara',
+          "value": 'Arabic',
+          "uri": 'http://id.loc.gov/vocabulary/iso639-2/ara',
+          "source": {
+            "code": 'iso639-2b',
+            "uri": 'http://id.loc.gov/vocabulary/iso639-2'
+          }
+        }
+      ]
+    end
+  end
+
+  context 'with language term only' do
+    let(:xml) do
+      <<~XML
+        <language>
+          <languageTerm type="text">English</languageTerm>
+        </language>
+      XML
+    end
+
+    it 'builds the cocina data structure' do
+      expect(build).to eq [
+        {
+          "value": 'English'
+        }
+      ]
+    end
+  end
+
+  context 'with language code only' do
+    let(:xml) do
+      <<~XML
+        <language>
+          <languageTerm authority="iso639-2b" type="code">eng</languageTerm>
+        </language>
+      XML
+    end
+
+    it 'builds the cocina data structure' do
+      expect(build).to eq [
+        {
+          "code": 'eng',
+          "source": {
+            "code": 'iso639-2b'
+          }
+        }
+      ]
+    end
+  end
+
+  context 'with multiple languages' do
+    let(:xml) do
+      <<~XML
+        <language status="primary">
+          <languageTerm type="text" authority="iso639-2b" authorityURI="http://id.loc.gov/vocabulary/iso639-2/" valueURI="http://id.loc.gov/vocabulary/iso639-2/eng">English</languageTerm>
+          <languageTerm type="code" authority="iso639-2b" authorityURI="http://id.loc.gov/vocabulary/iso639-2/" valueURI="http://id.loc.gov/vocabulary/iso639-2/eng">eng</languageTerm>
+        </language>
+        <language>
+          <languageTerm type="text" authority="iso639-2b" authorityURI="http://id.loc.gov/vocabulary/iso639-2/" valueURI="http://id.loc.gov/vocabulary/iso639-2/fre">French</languageTerm>
+          <languageTerm type="code" authority="iso639-2b" authorityURI="http://id.loc.gov/vocabulary/iso639-2/" valueURI="http://id.loc.gov/vocabulary/iso639-2/fre">fre</languageTerm>
+        </language>
+      XML
+    end
+
+    it 'builds the cocina data structure' do
+      expect(build).to eq [
+        {
+          "value": 'English',
+          "code": 'eng',
+          "uri": 'http://id.loc.gov/vocabulary/iso639-2/eng',
+          "source": {
+            "code": 'iso639-2b',
+            "uri": 'http://id.loc.gov/vocabulary/iso639-2/'
+          },
+          "status": 'primary'
+        },
+        {
+          "value": 'French',
+          "code": 'fre',
+          "uri": 'http://id.loc.gov/vocabulary/iso639-2/fre',
+          "source": {
+            "code": 'iso639-2b',
+            "uri": 'http://id.loc.gov/vocabulary/iso639-2/'
+          }
+        }
+      ]
+    end
+  end
+
+  context 'with script and authority' do
+    let(:xml) do
+      <<~XML
+        <language>
+          <languageTerm type="text" authority="iso639-2b" authorityURI="http://id.loc.gov/vocabulary/iso639-2/" valueURI="http://id.loc.gov/vocabulary/iso639-2/rus">Russian</languageTerm>
+          <languageTerm type="code" authority="iso639-2b" authorityURI="http://id.loc.gov/vocabulary/iso639-2/" valueURI="http://id.loc.gov/vocabulary/iso639-2/rus">rus</languageTerm>
+          <scriptTerm type="text" authority="iso15924">Cyrillic</scriptTerm>
+          <scriptTerm type="code" authority="iso15924">Cyrl</scriptTerm>
+        </language>
+      XML
+    end
+
+    it 'builds the cocina data structure' do
+      expect(build).to eq [
+        {
+          "value": 'Russian',
+          "code": 'rus',
+          "uri": 'http://id.loc.gov/vocabulary/iso639-2/rus',
+          "source": {
+            "code": 'iso639-2b',
+            "uri": 'http://id.loc.gov/vocabulary/iso639-2/'
+          },
+          "script": {
+            "value": 'Cyrillic',
+            "code": 'Cyrl',
+            "source": {
+              "code": 'iso15924'
+            }
+          }
+        }
+      ]
+    end
+  end
+
+  context 'with script only' do
+    let(:xml) do
+      <<~XML
+        <language>
+          <scriptTerm type="text" authority="iso15924">Latin</scriptTerm>
+          <scriptTerm type="code" authority="iso15924">Latn</scriptTerm>
+        </language>
+      XML
+    end
+
+    it 'builds the cocina data structure' do
+      expect(build).to eq [
+        {
+          "script": {
+            "value": 'Latin',
+            "code": 'Latn',
+            "source": {
+              "code": 'iso15924'
+            }
+          }
+        }
+      ]
+    end
+  end
+
+  context 'with objectPart' do
+    let(:xml) do
+      <<~XML
+        <language objectPart="liner notes">
+          <languageTerm type="text">English</languageTerm>
+        </language>
+        <language objectPart="libretto">
+          <languageTerm type="text">German</languageTerm>
+        </language>
+      XML
+    end
+
+    it 'builds the cocina data structure' do
+      expect(build).to eq [
+        {
+          "value": 'English',
+          "appliesTo": [
+            {
+              "value": 'liner notes'
+            }
+          ]
+        },
+        {
+          "value": 'German',
+          "appliesTo": [
+            {
+              "value": 'libretto'
+            }
+          ]
+        }
+      ]
+    end
+  end
+
+  context 'with displayLabel' do
+    let(:xml) do
+      <<~XML
+        <language displayLabel="Translated to">
+          <languageTerm type="text">English</languageTerm>
+        </language>
+      XML
+    end
+
+    it 'builds the cocina data structure' do
+      expect(build).to eq [
+        {
+          "value": 'English',
+          "displayLabel": 'Translated to'
+        }
+      ]
+    end
+  end
+
+  context 'with authorityURI and valueURI for code' do
+    let(:xml) do
+      <<~XML
+        <language>
+          <languageTerm type="code" authority="iso639-2b" authorityURI="http://id.loc.gov/vocabulary/iso639-2" valueURI="http://id.loc.gov/vocabulary/iso639-2/ara">ara</languageTerm>
+        </language>
+      XML
+    end
+
+    it 'builds the cocina data structure' do
+      expect(build).to eq [
+        {
+          "code": 'ara',
+          "uri": 'http://id.loc.gov/vocabulary/iso639-2/ara',
+          "source": {
+            "code": 'iso639-2b',
+            "uri": 'http://id.loc.gov/vocabulary/iso639-2'
+          }
+        }
+      ]
+    end
+  end
+
+  context 'with authorityURI and valueURI for text' do
+    let(:xml) do
+      <<~XML
+        <language>
+          <languageTerm type="text" authority="iso639-2b" authorityURI="http://id.loc.gov/vocabulary/iso639-2" valueURI="http://id.loc.gov/vocabulary/iso639-2/ara">Arabic</languageTerm>
+        </language>
+      XML
+    end
+
+    it 'builds the cocina data structure' do
+      expect(build).to eq [
+        {
+          "value": 'Arabic',
+          "uri": 'http://id.loc.gov/vocabulary/iso639-2/ara',
+          "source": {
+            "code": 'iso639-2b',
+            "uri": 'http://id.loc.gov/vocabulary/iso639-2'
+          }
+        }
+      ]
+    end
+  end
+end
