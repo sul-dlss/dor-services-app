d8cda82ad8cd9768cd8bab819e5b2e19a13361aa
aaron-collier <aaron.collier@stanford.edu>
2020-10-26 13:18:37 -0700

    Add test for name subject with additional terms and authority for terms

--- spec/services/cocina/to_fedora/descriptive/subject_spec.rb
+++ spec/services/cocina/to_fedora/descriptive/subject_spec.rb
@@ -441,6 +441,52 @@ RSpec.describe Cocina::ToFedora::Descriptive::Subject do
     end
   end
 
+  context 'when it has a name subject with additional terms and authority for the terms' do
+    let(:subjects) do
+      [
+        Cocina::Models::DescriptiveValue.new(
+          {
+            structuredValue: [
+              {
+                "value": 'Shakespeare, William, 1564-1616',
+                "type": 'person',
+                "uri": 'http://id.loc.gov/authorities/names/n78095332',
+                "source": {
+                  "code": 'naf',
+                  "uri": 'http://id.loc.gov/authorities/names/'
+                }
+              },
+              {
+                "value": 'Homes and haunts',
+                "type": 'topic',
+                "uri": 'http://id.loc.gov/authorities/subjects/sh99005711',
+                "source": {
+                  "code": 'lcsh',
+                  "uri": 'http://id.loc.gov/authorities/subjects/'
+                }
+              }
+            ]
+          }
+        )
+      ]
+    end
+
+    it 'builds the xml' do
+      expect(xml).to be_equivalent_to <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <subject authority="naf">
+            <name authority="naf" authorityURI="http://id.loc.gov/authorities/names/" valueURI="http://id.loc.gov/authorities/names/n78095332" type="personal">
+              <namePart>Shakespeare, William, 1564-1616</namePart>
+            </name>
+            <topic authority="lcsh" authorityURI="http://id.loc.gov/authorities/subjects/" valueURI="http://id.loc.gov/authorities/subjects/sh99005711">Homes and haunts</topic>
+          </subject>
+        </mods>
+      XML
+    end
+  end
+
   context 'when it has a name subject with multiple namePart elements' do
     let(:subjects) do
       [
