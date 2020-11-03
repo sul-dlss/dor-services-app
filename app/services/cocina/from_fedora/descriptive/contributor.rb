455d857ed98e571e0e9b75db31559b270bfa3411
Naomi Dushay <ndushay@stanford.edu>
2020-11-03 15:50:18 -0800

    appease rubocop masters -- a different way

--- app/services/cocina/from_fedora/descriptive/contributor.rb
+++ app/services/cocina/from_fedora/descriptive/contributor.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/ClassLength
 module Cocina
   module FromFedora
     class Descriptive
@@ -167,3 +168,4 @@ module Cocina
     end
   end
 end
+# rubocop:enable Metrics/ClassLength
