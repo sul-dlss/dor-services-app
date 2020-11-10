b16b0df8f7674b5882e655a164a28f49f0234634
Justin Littman <justinlittman@stanford.edu>
2020-11-10 11:53:33 -0500

    Handle cartographics missing forms.
    closes #1343

--- spec/services/cocina/to_fedora/descriptive/subject_spec.rb
+++ spec/services/cocina/to_fedora/descriptive/subject_spec.rb
@@ -936,6 +936,45 @@ RSpec.describe Cocina::ToFedora::Descriptive::Subject do
     end
   end
 
+  context 'when it has a cartographic subject without forms' do
+    let(:writer) do
+      Nokogiri::XML::Builder.new do |xml|
+        xml.mods('xmlns' => 'http://www.loc.gov/mods/v3',
+                 'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+                 'version' => '3.6',
+                 'xsi:schemaLocation' => 'http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd') do
+          described_class.write(xml: xml, subjects: subjects, forms: [])
+        end
+      end
+    end
+
+    let(:subjects) do
+      [
+        Cocina::Models::DescriptiveValue.new(
+          "value": 'E 72°--E 148°/N 13°--N 18°',
+          "type": 'map coordinates',
+          "encoding": {
+            "value": 'DMS'
+          }
+        )
+      ]
+    end
+
+    it 'builds the xml' do
+      expect(xml).to be_equivalent_to <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <subject>
+            <cartographics>
+              <coordinates>E 72°--E 148°/N 13°--N 18°</coordinates>
+            </cartographics>
+          </subject>
+        </mods>
+      XML
+    end
+  end
+
   context 'when it has a geographic code subject' do
     let(:subjects) do
       [
