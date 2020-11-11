769608c5416d89a7ae2612b3950a542064e49a20
Justin Littman <justinlittman@stanford.edu>
2020-11-11 15:29:56 -0500

    Handles events that are missing source. Fixes attributes mistaking put on place element.
    closes #1364

--- spec/services/cocina/to_fedora/descriptive/event_spec.rb
+++ spec/services/cocina/to_fedora/descriptive/event_spec.rb
@@ -527,8 +527,8 @@ RSpec.describe Cocina::ToFedora::Descriptive::Event do
           xmlns="http://www.loc.gov/mods/v3" version="3.6"
           xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
           <originInfo>
-            <place authority="naf" authorityURI="http://id.loc.gov/authorities/names/" valueURI="http://id.loc.gov/authorities/names/n50046557">
-              <placeTerm type="text">Stanford (Calif.)</placeTerm>
+            <place>
+              <placeTerm type="text" authority="naf" authorityURI="http://id.loc.gov/authorities/names/" valueURI="http://id.loc.gov/authorities/names/n50046557">Stanford (Calif.)</placeTerm>
             </place>
           </originInfo>
         </mods>
@@ -1183,6 +1183,92 @@ RSpec.describe Cocina::ToFedora::Descriptive::Event do
               <placeTerm type="text">京都市</placeTerm>
             </place>
             <publisher>臨川書店</publisher>
+          </originInfo>
+        </mods>
+      XML
+    end
+  end
+
+  context 'when event location missing source' do
+    let(:events) do
+      [
+        Cocina::Models::Event.new(
+          "location": [
+            {
+              "code": 'xxu'
+            }
+          ]
+        )
+      ]
+    end
+
+    it 'builds the xml' do
+      expect(xml).to be_equivalent_to <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <originInfo>
+            <place>
+              <placeTerm type="code">xxu</placeTerm>
+            </place>
+          </originInfo>
+        </mods>
+      XML
+    end
+  end
+
+  context 'when event location with code missing source' do
+    let(:events) do
+      [
+        Cocina::Models::Event.new(
+          "location": [
+            {
+              "code": 'cau',
+              "uri": 'http://id.loc.gov/vocabulary/countries/cau'
+            }
+          ]
+        )
+      ]
+    end
+
+    it 'builds the xml' do
+      expect(xml).to be_equivalent_to <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <originInfo>
+            <place>
+              <placeTerm type="code" valueURI="http://id.loc.gov/vocabulary/countries/cau">cau</placeTerm>
+            </place>
+          </originInfo>
+        </mods>
+      XML
+    end
+  end
+
+  context 'when event location with value missing source' do
+    let(:events) do
+      [
+        Cocina::Models::Event.new(
+          "location": [
+            {
+              "value": 'California',
+              "uri": 'http://id.loc.gov/vocabulary/countries/cau'
+            }
+          ]
+        )
+      ]
+    end
+
+    it 'builds the xml' do
+      expect(xml).to be_equivalent_to <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <originInfo>
+            <place>
+              <placeTerm type="text" valueURI="http://id.loc.gov/vocabulary/countries/cau">California</placeTerm>
+            </place>
           </originInfo>
         </mods>
       XML
