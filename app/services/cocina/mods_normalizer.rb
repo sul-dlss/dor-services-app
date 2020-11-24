60b9c4df2c745bd7d44dd937c5ea0574f5fbdcae
Justin Littman <justinlittman@stanford.edu>
2020-11-24 15:08:18 -0500

    Adds MODS normalizer service.

---
+++ app/services/cocina/mods_normalizer.rb
@@ -0,0 +1,115 @@
+# frozen_string_literal: true
+
+module Cocina
+  # Normalizes a Fedora MODS document, accounting for differences between Fedora MODS and MODS generated from Cocina.
+  class ModsNormalizer
+    # @param [Nokogiri::Document] mods_ng_xml MODS to be normalized
+    # @param [Nokogiri::Document] mods_ng_xml2 MODS to be compared (actual)
+    # @return [Nokogiri::Document] normalized MODS
+    def self.normalize(mods_ng_xml)
+      ModsNormalizer.new(mods_ng_xml).normalize
+    end
+
+    def initialize(mods_ng_xml)
+      @ng_xml = mods_ng_xml.dup
+    end
+
+    def normalize
+      normalize_default_namespace
+      normalize_version
+      normalize_topics
+      normalize_subject_name
+      normalize_authority_uris
+      normalize_origin_info_event_types
+      normalize_subject_authority
+      ng_xml
+    end
+
+    private
+
+    attr_reader :ng_xml
+
+    def normalize_default_namespace
+      xml = ng_xml.to_s
+
+      unless xml.include?('xmlns="http://www.loc.gov/mods/v3"')
+        xml.sub!('mods:mods', 'mods:mods xmlns="http://www.loc.gov/mods/v3"')
+        xml.gsub!('mods:', '')
+      end
+      @ng_xml = Nokogiri::XML(xml) { |config| config.default_xml.noblanks }
+    end
+
+    def normalize_version
+      # Only normalize version when version isn't mapped.
+      unless /MODS version (\d\.\d)/.match(ng_xml.root.at('//mods:recordInfo/mods:recordOrigin',
+                                                          mods: Cocina::FromFedora::Descriptive::DESC_METADATA_NS)&.content)
+        ng_xml.root['version'] = '3.6'
+        ng_xml.root['xsi:schemaLocation'] = 'http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd'
+      end
+    end
+
+    def normalize_topics
+      ng_xml.root.xpath('//mods:subject[count(mods:topic) = 1]', mods: Cocina::FromFedora::Descriptive::DESC_METADATA_NS).each do |subject_node|
+        topic_node = subject_node.xpath('mods:topic', mods: Cocina::FromFedora::Descriptive::DESC_METADATA_NS).first
+        normalize_subject(subject_node, topic_node)
+      end
+    end
+
+    def normalize_authority_uris
+      ng_xml.root.xpath("//mods:*[@authorityURI='http://id.loc.gov/authorities/names']", mods: Cocina::FromFedora::Descriptive::DESC_METADATA_NS).each do |node|
+        node[:authorityURI] = 'http://id.loc.gov/authorities/names/'
+      end
+      ng_xml.root.xpath("//mods:*[@authorityURI='http://id.loc.gov/authorities/subjects']", mods: Cocina::FromFedora::Descriptive::DESC_METADATA_NS).each do |node|
+        node[:authorityURI] = 'http://id.loc.gov/authorities/subjects/'
+      end
+    end
+
+    def normalize_subject_name
+      ng_xml.root.xpath('//mods:subject[count(mods:name) = 1]', mods: Cocina::FromFedora::Descriptive::DESC_METADATA_NS).each do |subject_node|
+        name_node = subject_node.xpath('mods:name', mods: Cocina::FromFedora::Descriptive::DESC_METADATA_NS).first
+        normalize_subject(subject_node, name_node)
+      end
+    end
+
+    def normalize_subject(subject_node, child_node)
+      return unless subject_node[:authorityURI] || subject_node[:valueURI]
+
+      # If subject has authority and child doesn't, copy to child.
+      child_node[:authority] = subject_node[:authority] if subject_node[:authority] && !child_node[:authority]
+      # If subject has authorityURI and child doesn't, move to child.
+      child_node[:authorityURI] = subject_node[:authorityURI] if subject_node[:authorityURI] && !child_node[:authorityURI]
+      subject_node.delete('authorityURI')
+      # If subject has valueURI and child doesn't, move to child.
+      child_node[:valueURI] = subject_node[:valueURI] if subject_node[:valueURI] && !child_node[:valueURI]
+      subject_node.delete('valueURI')
+    end
+
+    def normalize_subject_authority
+      ng_xml.root.xpath("//mods:subject[@authority='naf']", mods: Cocina::FromFedora::Descriptive::DESC_METADATA_NS).each do |subject_node|
+        subject_node[:authority] = 'lcsh'
+      end
+    end
+
+    # change original xml to have the event type that will be output
+    def normalize_origin_info_event_types
+      # code
+      ng_xml.root.xpath('mods:originInfo', mods: Cocina::FromFedora::Descriptive::DESC_METADATA_NS).each do |origin_info_node|
+        date_issued_nodes = origin_info_node.xpath('mods:dateIssued', mods: Cocina::FromFedora::Descriptive::DESC_METADATA_NS)
+        add_event_type('publication', origin_info_node) && next if date_issued_nodes.present?
+
+        copyright_date_nodes = origin_info_node.xpath('mods:copyrightDate', mods: Cocina::FromFedora::Descriptive::DESC_METADATA_NS)
+        add_event_type('copyright notice', origin_info_node) && next if copyright_date_nodes.present?
+
+        date_created_nodes = origin_info_node.xpath('mods:dateCreated', mods: Cocina::FromFedora::Descriptive::DESC_METADATA_NS)
+        add_event_type('production', origin_info_node) && next if date_created_nodes.present?
+
+        date_captured_nodes = origin_info_node.xpath('mods:dateCaptured', mods: Cocina::FromFedora::Descriptive::DESC_METADATA_NS)
+        add_event_type('capture', origin_info_node) && next if date_captured_nodes.present?
+      end
+    end
+
+    def add_event_type(value, origin_info_node)
+      origin_info_node['eventType'] = value if origin_info_node[:eventType].blank?
+    end
+  end
+end
---
+++ spec/services/cocina/mods_normalizer_spec.rb
@@ -0,0 +1,201 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Cocina::ModsNormalizer do
+  let(:normalized_ng_xml) { described_class.normalize(mods_ng_xml) }
+
+  context 'when normalizing version' do
+    context 'when version in recordInfo' do
+      let(:mods_ng_xml) do
+        Nokogiri::XML <<~XML
+          <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xmlns="http://www.loc.gov/mods/v3" version="3.7"
+            xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-7.xsd">
+            <recordInfo>
+              <recordOrigin>Converted from MARCXML to MODS version 3.7 using MARC21slim2MODS3-6_SDR.xsl (SUL version 1 2018/06/13; LC Revision 1.118 2018/01/31)</recordOrigin>
+            </recordInfo>
+          </mods>
+        XML
+      end
+
+      it 'leaves version' do
+        expect(normalized_ng_xml.root['version']).to eq('3.7')
+        expect(normalized_ng_xml.root['xsi:schemaLocation']).to eq('http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-7.xsd')
+      end
+    end
+
+    context 'when version not in recordInfo' do
+      let(:mods_ng_xml) do
+        Nokogiri::XML <<~XML
+          <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xmlns="http://www.loc.gov/mods/v3" version="3.7"
+            xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-7.xsd">
+          </mods>
+        XML
+      end
+
+      it 'changes version to 3.6' do
+        expect(normalized_ng_xml.root['version']).to eq('3.6')
+        expect(normalized_ng_xml.root['xsi:schemaLocation']).to eq('http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd')
+      end
+    end
+  end
+
+  context 'when normalizing topic' do
+    let(:mods_ng_xml) do
+      Nokogiri::XML <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <subject authority="fast" authorityURI="http://id.worldcat.org/fast/" valueURI="http://id.worldcat.org/fast/1009447">
+            <topic>Marine biology</topic>
+          </subject>
+        </mods>
+      XML
+    end
+
+    it 'moves authority, authorityURI, valueURI to topic' do
+      expect(normalized_ng_xml).to be_equivalent_to <<~XML
+        <?xml version="1.0"?>
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <subject authority="fast">
+            <topic authority="fast" authorityURI="http://id.worldcat.org/fast/" valueURI="http://id.worldcat.org/fast/1009447">Marine biology</topic>
+          </subject>
+        </mods>
+      XML
+    end
+  end
+
+  context 'when normalizing normalized_ng_xml name' do
+    let(:mods_ng_xml) do
+      Nokogiri::XML <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <subject authority="fast" authorityURI="http://id.worldcat.org/fast/" valueURI="http://id.worldcat.org/fast/270223">
+            <name type="personal">
+              <namePart>Anning, Mary, 1799-1847</namePart>
+            </name>
+          </subject>
+        </mods>
+      XML
+    end
+
+    it 'moves authority, authorityURI, valueURI to topic' do
+      expect(normalized_ng_xml).to be_equivalent_to <<~XML
+        <?xml version="1.0"?>
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <subject authority="fast">
+            <name type="personal" authority="fast" authorityURI="http://id.worldcat.org/fast/" valueURI="http://id.worldcat.org/fast/270223">
+              <namePart>Anning, Mary, 1799-1847</namePart>
+            </name>
+          </subject>
+        </mods>
+      XML
+    end
+  end
+
+  context 'when normalizing authorityURIs' do
+    let(:mods_ng_xml) do
+      Nokogiri::XML <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <name authorityURI="http://id.loc.gov/authorities/names">
+            <namePart authorityURI="http://id.loc.gov/authorities/subjects">Anning, Mary, 1799-1847</namePart>
+          </name>
+        </mods>
+      XML
+    end
+
+    it 'adds trailing slash' do
+      expect(normalized_ng_xml).to be_equivalent_to <<~XML
+        <?xml version="1.0"?>
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <name authorityURI="http://id.loc.gov/authorities/names/">
+            <namePart authorityURI="http://id.loc.gov/authorities/subjects/">Anning, Mary, 1799-1847</namePart>
+          </name>
+        </mods>
+      XML
+    end
+  end
+
+  context 'when normalizing originInfo eventTypes' do
+    let(:mods_ng_xml) do
+      Nokogiri::XML <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <originInfo>
+            <dateIssued>1930</dateIssued>
+          </originInfo>
+          <originInfo>
+            <copyrightDate>1931</copyrightDate>
+          </originInfo>
+          <originInfo>
+            <dateCreated>1932</dateCreated>
+          </originInfo>
+          <originInfo>
+            <dateCaptured>1932</dateCaptured>
+          </originInfo>
+        </mods>
+      XML
+    end
+
+    it 'adds eventType if missing' do
+      expect(normalized_ng_xml).to be_equivalent_to <<~XML
+        <?xml version="1.0"?>
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <originInfo eventType="publication">
+            <dateIssued>1930</dateIssued>
+          </originInfo>
+          <originInfo eventType="copyright notice">
+            <copyrightDate>1931</copyrightDate>
+          </originInfo>
+          <originInfo eventType="production">
+            <dateCreated>1932</dateCreated>
+          </originInfo>
+          <originInfo eventType="capture">
+            <dateCaptured>1932</dateCaptured>
+          </originInfo>
+        </mods>
+      XML
+    end
+  end
+
+  context 'when normalizing normalized_ng_xml authority' do
+    let(:mods_ng_xml) do
+      Nokogiri::XML <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <subject authority="naf">
+            <topic>Marine biology</topic>
+          </subject>
+        </mods>
+      XML
+    end
+
+    it 'changes naf to lcsh' do
+      expect(normalized_ng_xml).to be_equivalent_to <<~XML
+        <?xml version="1.0"?>
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <subject authority="lcsh">
+            <topic>Marine biology</topic>
+          </subject>
+        </mods>
+      XML
+    end
+  end
+end
