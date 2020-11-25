f0288b7ec5cf34929a3ccbffaa0363ec513f880a
Justin Littman <justinlittman@stanford.edu>
2020-11-25 17:53:08 -0500

    Normalizes when relatedItem has otherType and type.

--- spec/services/cocina/mods_normalizer_spec.rb
+++ spec/services/cocina/mods_normalizer_spec.rb
@@ -326,4 +326,37 @@ RSpec.describe Cocina::ModsNormalizer do
       XML
     end
   end
+
+  context 'when normalizing relatedType with type and otherType' do
+    let(:mods_ng_xml) do
+      Nokogiri::XML <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <relatedItem type="otherFormat" otherType="Online version:" displayLabel="Online version:">
+            <titleInfo>
+              <title>Sitzungsberichte der Kaiserlichen Akademie der Wissenschaften. Mathematisch-Naturwissenschaftliche Classe. Abt. 2, Mathematik, Physik, Chemie, Physiologie, Meteorologie, physische Geographie und Astronomie</title>
+            </titleInfo>
+            <identifier type="local">(OCoLC)606338944</identifier>
+          </relatedItem>
+        </mods>
+      XML
+    end
+
+    it 'does not add otherType' do
+      expect(normalized_ng_xml).to be_equivalent_to <<~XML
+        <?xml version="1.0"?>
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <relatedItem type="otherFormat" displayLabel="Online version:">
+            <titleInfo>
+              <title>Sitzungsberichte der Kaiserlichen Akademie der Wissenschaften. Mathematisch-Naturwissenschaftliche Classe. Abt. 2, Mathematik, Physik, Chemie, Physiologie, Meteorologie, physische Geographie und Astronomie</title>
+            </titleInfo>
+            <identifier type="local">(OCoLC)606338944</identifier>
+          </relatedItem>
+        </mods>
+      XML
+    end
+  end
 end
