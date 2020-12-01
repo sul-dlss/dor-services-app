4a0ce3ad37e30392cc2a9d249c91f7d8efb64643
Justin Littman <justinlittman@stanford.edu>
2020-12-01 07:22:22 -0500

    Maps events without dates.
    closes #1551

--- spec/services/cocina/to_fedora/descriptive/event_spec.rb
+++ spec/services/cocina/to_fedora/descriptive/event_spec.rb
@@ -1308,4 +1308,36 @@ RSpec.describe Cocina::ToFedora::Descriptive::Event do
       XML
     end
   end
+
+  context 'when it has a displayLabel' do
+    let(:events) do
+      [
+        Cocina::Models::Event.new(
+          {
+            "type": 'creation',
+            "displayLabel": 'Origin',
+            "location": [
+              {
+                "value": 'Stanford (Calif.)'
+              }
+            ]
+          }
+        )
+      ]
+    end
+
+    it 'builds the xml' do
+      expect(xml).to be_equivalent_to <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <originInfo displayLabel="Origin" eventType="production">
+            <place>
+              <placeTerm type="text">Stanford (Calif.)</placeTerm>
+            </place>
+          </originInfo>
+        </mods>
+      XML
+    end
+  end
 end
