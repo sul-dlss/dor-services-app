3ffdf63b34c053fc4654786b8b260450fcf24186
Naomi Dushay <ndushay@stanford.edu>
2020-12-09 10:47:14 -0800

    get rid of xml instruction at in a spec

--- spec/services/cocina/to_fedora/descriptive/location_spec.rb
+++ spec/services/cocina/to_fedora/descriptive/location_spec.rb
@@ -360,7 +360,6 @@ RSpec.describe Cocina::ToFedora::Descriptive::Location do
 
     it 'builds the xml' do
       expect(xml).to be_equivalent_to <<~XML
-        <?xml version=\"1.0\"?>
         <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
           xmlns="http://www.loc.gov/mods/v3" version="3.6"
           xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
