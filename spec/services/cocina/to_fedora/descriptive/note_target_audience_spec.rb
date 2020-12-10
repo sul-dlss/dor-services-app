b86d5934daf073a65e93166380f90bf3c79e1428
Justin Littman <justinlittman@stanford.edu>
2020-12-09 21:28:21 -0500

    Maps target audience notes.
    closes #1608

---
+++ spec/services/cocina/to_fedora/descriptive/note_target_audience_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Cocina::ToFedora::Descriptive::Note do
+  subject(:xml) { writer.to_xml }
+
+  let(:writer) do
+    Nokogiri::XML::Builder.new do |xml|
+      xml.mods('xmlns' => 'http://www.loc.gov/mods/v3',
+               'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+               'version' => '3.6',
+               'xsi:schemaLocation' => 'http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd') do
+        described_class.write(xml: xml, notes: notes, id_generator: Cocina::ToFedora::Descriptive::IdGenerator.new)
+      end
+    end
+  end
+
+  context 'with authority' do
+    let(:notes) do
+      [
+        Cocina::Models::DescriptiveValue.new(
+          "value": 'juvenile',
+          "type": 'target audience',
+          "source": {
+            "code": 'marctarget'
+          }
+        )
+      ]
+    end
+
+    it 'builds the xml' do
+      expect(xml).to be_equivalent_to <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <targetAudience authority="marctarget">juvenile</targetAudience>
+        </mods>
+      XML
+    end
+  end
+
+  context 'without authority' do
+    let(:notes) do
+      [
+        Cocina::Models::DescriptiveValue.new(
+          "value": 'ages 3-6',
+          "type": 'target audience'
+        )
+      ]
+    end
+
+    it 'builds the xml' do
+      expect(xml).to be_equivalent_to <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <targetAudience>ages 3-6</targetAudience>
+        </mods>
+      XML
+    end
+  end
+end
