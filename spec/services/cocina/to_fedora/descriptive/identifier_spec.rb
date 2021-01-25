ad04292a47ed7721a7137c8c1ebe8dfc35644d94
Naomi Dushay <ndushay@stanford.edu>
2021-01-25 15:58:50 -0800

    mods identifier <--> cocina mapping specs reworked

--- spec/services/cocina/to_fedora/descriptive/identifier_spec.rb
+++ spec/services/cocina/to_fedora/descriptive/identifier_spec.rb
@@ -19,45 +19,11 @@ RSpec.describe Cocina::ToFedora::Descriptive::Identifier do
   context 'when identifiers is nil' do
     let(:identifiers) { nil }
 
-    it 'builds the xml' do
+    it 'builds empty MODS' do
       expect(xml).to be_equivalent_to <<~XML
         <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
           xmlns="http://www.loc.gov/mods/v3" version="3.6"
           xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
-        </mods>
-      XML
-    end
-  end
-
-  context 'when it has a single identifier' do
-    let(:identifiers) do
-      [
-        Cocina::Models::DescriptiveValue.new(
-          {
-            "value": '1234 5678 9203',
-            "type": 'ISBN',
-            "note": [
-              {
-                "type": 'type',
-                "value": 'isbn',
-                "uri": 'http://id.loc.gov/vocabulary/identifiers/isbn',
-                "source": {
-                  "value": 'Standard Identifier Schemes',
-                  "uri": 'http://id.loc.gov/vocabulary/identifiers/'
-                }
-              }
-            ]
-          }
-        )
-      ]
-    end
-
-    it 'builds the xml' do
-      expect(xml).to be_equivalent_to <<~XML
-        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-          xmlns="http://www.loc.gov/mods/v3" version="3.6"
-          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
-          <identifier type="isbn">1234 5678 9203</identifier>
         </mods>
       XML
     end
@@ -81,95 +47,6 @@ RSpec.describe Cocina::ToFedora::Descriptive::Identifier do
           xmlns="http://www.loc.gov/mods/v3" version="3.6"
           xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
           <identifier type="OCLC">123456789203</identifier>
-        </mods>
-      XML
-    end
-  end
-
-  context 'when it has a URI identifier' do
-    let(:identifiers) do
-      [
-        Cocina::Models::DescriptiveValue.new(
-          {
-            "uri": 'https://www.wikidata.org/wiki/Q146',
-            "note": [
-              {
-                "type": 'type',
-                "value": 'uri',
-                "uri": 'http://id.loc.gov/vocabulary/identifiers/uri',
-                "source": {
-                  "value": 'Standard Identifier Schemes',
-                  "uri": 'http://id.loc.gov/vocabulary/identifiers/'
-                }
-              }
-            ]
-          }
-        )
-      ]
-    end
-
-    it 'builds the xml' do
-      expect(xml).to be_equivalent_to <<~XML
-        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-          xmlns="http://www.loc.gov/mods/v3" version="3.6"
-          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
-          <identifier type="uri">https://www.wikidata.org/wiki/Q146</identifier>
-        </mods>
-      XML
-    end
-  end
-
-  context 'when it has a display label' do
-    let(:identifiers) do
-      [
-        Cocina::Models::DescriptiveValue.new(
-          {
-            "value": '1980-12345',
-            "displayLabel": 'Accession number'
-          }
-        )
-      ]
-    end
-
-    it 'builds the xml' do
-      expect(xml).to be_equivalent_to <<~XML
-        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-          xmlns="http://www.loc.gov/mods/v3" version="3.6"
-          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
-          <identifier displayLabel="Accession number">1980-12345</identifier>
-        </mods>
-      XML
-    end
-  end
-
-  context 'when it is invalid' do
-    let(:identifiers) do
-      [
-        Cocina::Models::DescriptiveValue.new(
-          "value": 'sn 87042262',
-          "status": 'invalid',
-          "type": 'LCCN',
-          "note": [
-            {
-              "type": 'type',
-              "value": 'lccn',
-              "uri": 'http://id.loc.gov/vocabulary/identifiers/lccn',
-              "source": {
-                "value": 'Standard Identifier Schemes',
-                "uri": 'http://id.loc.gov/vocabulary/identifiers/'
-              }
-            }
-          ]
-        )
-      ]
-    end
-
-    it 'builds the xml' do
-      expect(xml).to be_equivalent_to <<~XML
-        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-          xmlns="http://www.loc.gov/mods/v3" version="3.6"
-          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
-          <identifier type="lccn" invalid="yes">sn 87042262</identifier>
         </mods>
       XML
     end
