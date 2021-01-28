bbb0bad15c729d5d36b734af659a91bb2a5606d7
Justin Littman <justinlittman@stanford.edu>
2021-01-28 12:26:00 -0500

    Re-adds additional tests.

--- spec/services/cocina/mapping/descriptive/mods/origin_info_place_spec.rb
+++ spec/services/cocina/mapping/descriptive/mods/origin_info_place_spec.rb
@@ -48,111 +48,111 @@ RSpec.describe 'MODS originInfo place <--> cocina mappings' do
   end
 
   describe 'Place - code' do
-    let(:mods) do
-      <<~XML
-        <originInfo eventType="publication">
-          <place>
-            <placeTerm type="code" authority="marccountry" authorityURI="http://id.loc.gov/vocabulary/countries/" valueURI="http://id.loc.gov/vocabulary/countries/cau">cau</placeTerm>
-          </place>
-        </originInfo>
-      XML
-    end
+    it_behaves_like 'MODS cocina mapping' do
+      let(:mods) do
+        <<~XML
+          <originInfo eventType="publication">
+            <place>
+              <placeTerm type="code" authority="marccountry" authorityURI="http://id.loc.gov/vocabulary/countries/" valueURI="http://id.loc.gov/vocabulary/countries/cau">cau</placeTerm>
+            </place>
+          </originInfo>
+        XML
+      end
 
-    let(:cocina) do
-      {
-        "event": [
-          {
-            "type": 'publication',
-            "location": [
-              {
-                "code": 'cau',
-                "uri": 'http://id.loc.gov/vocabulary/countries/cau',
-                "source": {
-                  "code": 'marccountry',
-                  "uri": 'http://id.loc.gov/vocabulary/countries/'
+      let(:cocina) do
+        {
+          "event": [
+            {
+              "type": 'publication',
+              "location": [
+                {
+                  "code": 'cau',
+                  "uri": 'http://id.loc.gov/vocabulary/countries/cau',
+                  "source": {
+                    "code": 'marccountry',
+                    "uri": 'http://id.loc.gov/vocabulary/countries/'
+                  }
                 }
-              }
-            ]
-          }
-        ]
-      }
+              ]
+            }
+          ]
+        }
+      end
     end
-
-    xit 'broken'
   end
 
   describe 'Place - text and code for same place' do
-    let(:mods) do
-      <<~XML
-        <originInfo eventType="publication">
-          <place>
-            <placeTerm type="text" authority="marccountry" authorityURI="http://id.loc.gov/vocabulary/countries/" valueURI="http://id.loc.gov/vocabulary/countries/cau">California</placeTerm>
-            <placeTerm type="code" authority="marccountry" authorityURI="http://id.loc.gov/vocabulary/countries/" valueURI="http://id.loc.gov/vocabulary/countries/cau">cau</placeTerm>
-          </place>
-        </originInfo>
-      XML
-    end
+    it_behaves_like 'MODS cocina mapping' do
+      let(:mods) do
+        <<~XML
+          <originInfo eventType="publication">
+            <place>
+              <placeTerm type="text" authority="marccountry" authorityURI="http://id.loc.gov/vocabulary/countries/" valueURI="http://id.loc.gov/vocabulary/countries/cau">California</placeTerm>
+              <placeTerm type="code" authority="marccountry" authorityURI="http://id.loc.gov/vocabulary/countries/" valueURI="http://id.loc.gov/vocabulary/countries/cau">cau</placeTerm>
+            </place>
+          </originInfo>
+        XML
+      end
 
-    let(:cocina) do
-      {
-        "event": [
-          {
-            "type": 'publication',
-            "location": [
-              {
-                "value": 'California',
-                "code": 'cau',
-                "uri": 'http://id.loc.gov/vocabulary/countries/cau',
-                "source": {
-                  "code": 'marccountry',
-                  "uri": 'http://id.loc.gov/vocabulary/countries/'
+      let(:cocina) do
+        {
+          "event": [
+            {
+              "type": 'publication',
+              "location": [
+                {
+                  "value": 'California',
+                  "code": 'cau',
+                  "uri": 'http://id.loc.gov/vocabulary/countries/cau',
+                  "source": {
+                    "code": 'marccountry',
+                    "uri": 'http://id.loc.gov/vocabulary/countries/'
+                  }
                 }
-              }
-            ]
-          }
-        ]
-      }
+              ]
+            }
+          ]
+        }
+      end
     end
-
-    xit 'broken'
   end
 
   describe 'Place - text and code for different places - Version A (converted from MARC, standard MODS)' do
-    let(:mods) do
-      <<~XML
-        <originInfo eventType="publication">
-          <place>
-            <placeTerm type="code" authority="marccountry">enk</placeTerm>
-          </place>
-          <place>
-            <placeTerm type="text">London</placeTerm>
-          </place>
-        </originInfo>
-      XML
-    end
+    it_behaves_like 'MODS cocina mapping' do
+      let(:mods) do
+        <<~XML
+          <originInfo eventType="publication">
+            <place>
+              <placeTerm type="code" authority="marccountry">enk</placeTerm>
+            </place>
+            <place>
+              <placeTerm type="text">London</placeTerm>
+            </place>
+          </originInfo>
+        XML
+      end
 
-    let(:cocina) do
-      {
-        "event": [
-          {
-            "type": 'publication',
-            "location": [
-              {
-                "code": 'enk',
-                "source": {
-                  "code": 'marccountry'
+      let(:cocina) do
+        {
+          "event": [
+            {
+              "type": 'publication',
+              "location": [
+                {
+                  "code": 'enk',
+                  "source": {
+                    "code": 'marccountry'
+                  }
+                },
+                {
+                  "value": 'London'
                 }
-              },
-              {
-                "value": 'London'
-              }
-            ]
-          }
-        ]
-      }
+              ]
+            }
+          ]
+        }
+      end
     end
-
-    xit 'broken'
   end
 
   describe 'Place - text and code for different places - Version B (from replayable spreadsheet, incorrect MODS)' do
