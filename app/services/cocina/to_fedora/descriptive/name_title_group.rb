e046a1967f757331384736e3511bc8d5f65aef6b
Justin Littman <justinlittman@stanford.edu>
2021-01-29 08:55:30 -0500

    Fix

--- app/services/cocina/to_fedora/descriptive/name_title_group.rb
+++ app/services/cocina/to_fedora/descriptive/name_title_group.rb
@@ -37,15 +37,17 @@ module Cocina
         end
 
         def self.title_name_parts_for(title)
-          if title.structuredValue
+          if title.parallelValue
+            title.parallelValue.map { |value| title_name_parts_for_structured_value(value) }.flatten.compact.presence
+          else
             title_name_parts_for_structured_value(title)
-          elsif title.parallelValue
-            title.parallelValue.map { |structured_value| title_name_parts_for_structured_value(structured_value) }.flatten.compact.presence
           end
         end
         private_class_method :title_name_parts_for
 
         def self.title_name_parts_for_structured_value(title)
+          return nil unless title.structuredValue
+
           structured_title = title.structuredValue.find { |check_structured_title| check_structured_title.type == 'name' }
           if structured_title.nil?
             nil
