95d8365cdc0fea0d8348667fcdb3382f77481426
Naomi Dushay <ndushay@stanford.edu>
2021-01-28 17:49:15 -0800

    record_info mapping spec - formatting hash

--- spec/services/cocina/mapping/descriptive/mods/record_info_spec.rb
+++ spec/services/cocina/mapping/descriptive/mods/record_info_spec.rb
@@ -9,13 +9,17 @@ RSpec.describe 'MODS recordInfo <--> cocina mappings' do
         <<~XML
           <recordInfo>
             <languageOfCataloging usage="primary">
-              <languageTerm type="text" authority="iso639-2b" authorityURI="http://id.loc.gov/vocabulary/iso639-2/" valueURI="http://id.loc.gov/vocabulary/iso639-2/eng">English</languageTerm>
-              <languageTerm type="code" authority="iso639-2b" authorityURI="http://id.loc.gov/vocabulary/iso639-2/" valueURI="http://id.loc.gov/vocabulary/iso639-2/eng">eng</languageTerm>
+              <languageTerm type="text" authority="iso639-2b" authorityURI="http://id.loc.gov/vocabulary/iso639-2/"
+                valueURI="http://id.loc.gov/vocabulary/iso639-2/eng">English</languageTerm>
+              <languageTerm type="code" authority="iso639-2b" authorityURI="http://id.loc.gov/vocabulary/iso639-2/"
+                valueURI="http://id.loc.gov/vocabulary/iso639-2/eng">eng</languageTerm>
               <scriptTerm type="text" authority="iso15924">Latin</scriptTerm>
               <scriptTerm type="code" authority="iso15924">Latn</scriptTerm>
             </languageOfCataloging>
-            <recordContentSource authority="marcorg" authorityURI="http://id.loc.gov/vocabulary/organizations/" valueURI="http://id.loc.gov/vocabulary/organizations/cst">CSt</recordContentSource>
-            <descriptionStandard authority="dacs" authorityURI="http://id.loc.gov/vocabulary/descriptionConventions/" valueURI="http://id.loc.gov/vocabulary/descriptionConventions/dacs"></descriptionStandard>
+            <recordContentSource authority="marcorg" authorityURI="http://id.loc.gov/vocabulary/organizations/"
+              valueURI="http://id.loc.gov/vocabulary/organizations/cst">CSt</recordContentSource>
+            <descriptionStandard authority="dacs" authorityURI="http://id.loc.gov/vocabulary/descriptionConventions/"
+              valueURI="http://id.loc.gov/vocabulary/descriptionConventions/dacs"></descriptionStandard>
             <descriptionStandard>aacr</descriptionStandard>
             <recordOrigin>human prepared</recordOrigin>
           </recordInfo>
@@ -24,63 +28,62 @@ RSpec.describe 'MODS recordInfo <--> cocina mappings' do
 
       let(:cocina) do
         {
-          "adminMetadata": {
-            "language": [
+          adminMetadata: {
+            language: [
               {
-                "status": 'primary',
-                "value": 'English',
-                "code": 'eng',
-                "uri": 'http://id.loc.gov/vocabulary/iso639-2/eng',
-                "source": {
-                  "code": 'iso639-2b',
-                  "uri": 'http://id.loc.gov/vocabulary/iso639-2/'
+                status: 'primary',
+                value: 'English',
+                code: 'eng',
+                uri: 'http://id.loc.gov/vocabulary/iso639-2/eng',
+                source: {
+                  code: 'iso639-2b',
+                  uri: 'http://id.loc.gov/vocabulary/iso639-2/'
                 },
-                "script": {
-                  "value": 'Latin',
-                  "code": 'Latn',
-                  "source": {
-                    "code": 'iso15924'
+                script: {
+                  value: 'Latin',
+                  code: 'Latn',
+                  source: {
+                    code: 'iso15924'
                   }
                 }
               }
             ],
-            "contributor": [
+            contributor: [
               {
-                "name": [
+                name: [
                   {
-                    "code": 'CSt',
-                    "uri": 'http://id.loc.gov/vocabulary/organizations/cst',
-                    "source": {
-                      "code": 'marcorg',
-                      "uri": 'http://id.loc.gov/vocabulary/organizations/'
+                    code: 'CSt',
+                    uri: 'http://id.loc.gov/vocabulary/organizations/cst',
+                    source: {
+                      code: 'marcorg',
+                      uri: 'http://id.loc.gov/vocabulary/organizations/'
                     }
                   }
                 ],
-                "type": 'organization',
-                "role": [
+                type: 'organization',
+                role: [
                   {
-                    "value": 'original cataloging agency'
+                    value: 'original cataloging agency'
                   }
                 ]
               }
             ],
-            "metadataStandard":
-                  [
-                    {
-                      "code": 'dacs',
-                      "uri": 'http://id.loc.gov/vocabulary/descriptionConventions/dacs',
-                      "source": {
-                        "uri": 'http://id.loc.gov/vocabulary/descriptionConventions/'
-                      }
-                    },
-                    {
-                      "code": 'aacr'
-                    }
-                  ],
-            "note": [
+            metadataStandard: [
               {
-                "type": 'record origin',
-                "value": 'human prepared'
+                code: 'dacs',
+                uri: 'http://id.loc.gov/vocabulary/descriptionConventions/dacs',
+                source: {
+                  uri: 'http://id.loc.gov/vocabulary/descriptionConventions/'
+                }
+              },
+              {
+                code: 'aacr'
+              }
+            ],
+            note: [
+              {
+                type: 'record origin',
+                value: 'human prepared'
               }
             ]
           }
@@ -112,58 +115,58 @@ RSpec.describe 'MODS recordInfo <--> cocina mappings' do
 
     let(:cocina) do
       {
-        "adminMetadata": {
-          "language": [
+        adminMetadata: {
+          language: [
             {
-              "value": 'English',
-              "code": 'eng',
-              "uri": 'http://id.loc.gov/vocabulary/iso639-2/eng',
-              "source": {
-                "code": 'iso639-2b',
-                "uri": 'http://id.loc.gov/vocabulary/iso639-2/'
+              value: 'English',
+              code: 'eng',
+              uri: 'http://id.loc.gov/vocabulary/iso639-2/eng',
+              source: {
+                code: 'iso639-2b',
+                uri: 'http://id.loc.gov/vocabulary/iso639-2/'
               },
-              "script": {
-                "value": 'Latin',
-                "code": 'Latn',
-                "source": {
-                  "code": 'iso15924'
+              script: {
+                value: 'Latin',
+                code: 'Latn',
+                source: {
+                  code: 'iso15924'
                 }
               },
-              "status": 'primary'
+              status: 'primary'
             },
             {
-              "value": 'Chinese',
-              "code": 'chi',
-              "uri": 'http://id.loc.gov/vocabulary/iso639-2/chi',
-              "source": {
-                "code": 'iso639-2b',
-                "uri": 'http://id.loc.gov/vocabulary/iso639-2/'
+              value: 'Chinese',
+              code: 'chi',
+              uri: 'http://id.loc.gov/vocabulary/iso639-2/chi',
+              source: {
+                code: 'iso639-2b',
+                uri: 'http://id.loc.gov/vocabulary/iso639-2/'
               },
-              "script": {
-                "value": 'Han (Simplified variant)',
-                "code": 'Hans',
-                "source": {
-                  "code": 'iso15924'
+              script: {
+                value: 'Han (Simplified variant)',
+                code: 'Hans',
+                source: {
+                  code: 'iso15924'
                 }
               }
             }
           ],
-          "contributor": [
+          contributor: [
             {
-              "name": [
+              name: [
                 {
-                  "code": 'CSt',
-                  "uri": 'http://id.loc.gov/vocabulary/organizations/cst',
-                  "source": {
-                    "code": 'marcorg',
-                    "uri": 'http://id.loc.gov/vocabulary/organizations/'
+                  code: 'CSt',
+                  uri: 'http://id.loc.gov/vocabulary/organizations/cst',
+                  source: {
+                    code: 'marcorg',
+                    uri: 'http://id.loc.gov/vocabulary/organizations/'
                   }
                 }
               ],
-              "type": 'organization',
-              "role": [
+              type: 'organization',
+              role: [
                 {
-                  "value": 'original cataloging agency'
+                  value: 'original cataloging agency'
                 }
               ]
             }
@@ -196,72 +199,72 @@ RSpec.describe 'MODS recordInfo <--> cocina mappings' do
 
     let(:cocina) do
       {
-        "adminMetadata": {
-          "contributor": [
+        adminMetadata: {
+          contributor: [
             {
-              "name": [
+              name: [
                 {
-                  "code": 'CSt',
+                  code: 'CSt',
 
-                  "source": {
-                    "code": 'marcorg'
+                  source: {
+                    code: 'marcorg'
                   }
                 }
               ],
-              "type": 'organization',
-              "role": [
+              type: 'organization',
+              role: [
                 {
-                  "value": 'original cataloging agency'
+                  value: 'original cataloging agency'
                 }
               ]
             }
           ],
-          "event": [
+          event: [
             {
-              "type": 'creation',
-              "date": [
+              type: 'creation',
+              date: [
                 {
-                  "value": '180305',
-                  "encoding": {
-                    "code": 'marc'
+                  value: '180305',
+                  encoding: {
+                    code: 'marc'
                   }
                 }
               ]
             },
             {
-              "type": 'modification',
-              "date": [
+              type: 'modification',
+              date: [
                 {
-                  "value": '20200718050001.0',
-                  "encoding": {
-                    "code": 'iso8601'
+                  value: '20200718050001.0',
+                  encoding: {
+                    code: 'iso8601'
                   }
                 }
               ]
             }
           ],
-          "metadataStandard": [
+          metadataStandard: [
             {
-              "code": 'aacr'
+              code: 'aacr'
             }
           ],
-          "identifier": [
+          identifier: [
             {
-              "value": 'a12374669',
-              "type": 'SIRSI'
+              value: 'a12374669',
+              type: 'SIRSI'
             }
           ],
-          "note": [
+          note: [
             {
-              "type": 'record origin',
-              "value": 'Converted from MARCXML to MODS version 3.6 using MARC21slim2MODS3-6_SDR.xsl (SUL version 1 2018/06/13; LC Revision 1.118 2018/01/31)'
+              type: 'record origin',
+              value: 'Converted from MARCXML to MODS version 3.6 using MARC21slim2MODS3-6_SDR.xsl (SUL version 1 2018/06/13; LC Revision 1.118 2018/01/31)'
             }
           ],
-          "language": [
+          language: [
             {
-              "code": 'eng',
-              "source": {
-                "code": 'iso639-2b'
+              code: 'eng',
+              source: {
+                code: 'iso639-2b'
               }
             }
           ]
@@ -271,6 +274,8 @@ RSpec.describe 'MODS recordInfo <--> cocina mappings' do
   end
 
   describe 'Converted from ISO 19139' do
+    xit 'not implemented: recordContentSource is a value, not a code ...'
+
     let(:mods) do
       <<~XML
         <recordInfo>
@@ -286,39 +291,37 @@ RSpec.describe 'MODS recordInfo <--> cocina mappings' do
 
     let(:cocina) do
       {
-        "adminMetadata": {
-          "contributor": [
+        adminMetadata: {
+          contributor: [
             {
-              "name": [
+              name: [
                 {
-                  "value": 'Stanford'
+                  value: 'Stanford'
                 }
               ]
             }
           ],
-          "identifier": [
+          identifier: [
             {
-              "value": 'edu.stanford.purl:ft445st6184'
+              value: 'edu.stanford.purl:ft445st6184'
             }
           ],
-          "note": [
+          note: [
             {
-              "type": 'record origin',
-              "value": 'This record was translated from ISO 19139 to MODS v.3 using an xsl transformation.'
+              type: 'record origin',
+              value: 'This record was translated from ISO 19139 to MODS v.3 using an xsl transformation.'
             }
           ],
-          "language": [
+          language: [
             {
-              "code": 'eng',
-              "source": {
-                "code": 'iso639-2b'
+              code: 'eng',
+              source: {
+                code: 'iso639-2b'
               }
             }
           ]
         }
       }
     end
-
-    xit 'not implemented'
   end
 end
