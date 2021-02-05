83f7957a9bc79bf7b9985453ba15d418fc46fc04
Arcadia Falcone <arcadia@stanford.edu>
2021-02-05 15:45:42 -0800

    Add mapping for subject with language info at subject level

--- spec/services/cocina/mapping/descriptive/mods/physical_description_spec.rb
+++ spec/services/cocina/mapping/descriptive/mods/physical_description_spec.rb
@@ -4,156 +4,149 @@ require 'rails_helper'
 
 RSpec.describe 'MODS physicalDescription <--> cocina mappings' do
   describe 'Single physical description with all subelements' do
-    it_behaves_like 'MODS cocina mapping' do
-      let(:mods) do
-        <<~XML
-          <physicalDescription>
-            <form>ink on paper</form>
-            <reformattingQuality>access</reformattingQuality>
-            <internetMediaType>image/jpeg</internetMediaType>
-            <extent>1 sheet</extent>
-            <digitalOrigin>reformatted digital</digitalOrigin>
-            <note displayLabel="Condition">Small tear at top right corner.</note>
-            <note displayLabel="Material" type="material">Paper</note>
-            <note displayLabel="Layout" type="layout">34 and 24 lines to a page</note>
-            <note displayLabel="Height (mm)" type="dimensions">210</note>
-            <note displayLabel="Width (mm)" type="dimensions">146</note>
-            <note displayLabel="Collation" type="collation">1(8) 2(10) 3(8) 4(8) 5 (two) || a(16) (wants 16).</note>
-            <note displayLabel="Writing" type="handNote">change of hand</note>
-            <note displayLabel="Foliation" type="foliation">ff. i + 1-51 + ii-iii</note>
-          </physicalDescription>
-        XML
-      end
+    xit 'not implemented'
+    let(:mods) do
+      <<~XML
+        <physicalDescription>
+          <form>ink on paper</form>
+          <reformattingQuality>access</reformattingQuality>
+          <internetMediaType>image/jpeg</internetMediaType>
+          <extent>1 sheet</extent>
+          <digitalOrigin>reformatted digital</digitalOrigin>
+          <note displayLabel="Condition">Small tear at top right corner.</note>
+          <note displayLabel="Material" type="material">Paper</note>
+          <note displayLabel="Layout" type="layout">34 and 24 lines to a page</note>
+          <note displayLabel="Height (mm)" type="dimensions">210</note>
+          <note displayLabel="Width (mm)" type="dimensions">146</note>
+          <note displayLabel="Collation" type="collation">1(8) 2(10) 3(8) 4(8) 5 (two) || a(16) (wants 16).</note>
+          <note displayLabel="Writing" type="handNote">change of hand</note>
+          <note displayLabel="Foliation" type="foliation">ff. i + 1-51 + ii-iii</note>
+        </physicalDescription>
+      XML
+    end
 
-      let(:cocina) do
-        {
-          form: [
-            {
-              structuredValue: [
-                {
-                  value: 'ink on paper',
-                  type: 'form'
-                },
-                {
-                  value: 'access',
-                  type: 'reformatting quality',
-                  source: {
-                    value: 'MODS reformatting quality terms'
-                  }
-                },
-                {
-                  value: 'image/jpeg',
-                  type: 'media type',
-                  source: {
-                    value: 'IANA media types'
-                  }
-                },
-                {
-                  value: '1 sheet',
-                  type: 'extent'
-                },
-                {
-                  value: 'reformatted digital',
-                  type: 'digital origin',
-                  source: {
-                    value: 'MODS digital origin terms'
-                  }
-                }
-              ],
-              note: [
-                {
-                  value: 'Small tear at top right corner.',
-                  displayLabel: 'Condition'
-                },
-                {
-                  value: 'Paper',
-                  displayLabel: 'Material',
-                  type: 'material'
-                },
-                {
-                  value: '34 and 24 lines to a page',
-                  displayLabel: 'Layout',
-                  type: 'layout'
-                },
-                {
-                  value: '210',
-                  displayLabel: 'Height (mm)',
-                  type: 'dimensions'
-                },
-                {
-                  value: '146',
-                  displayLabel: 'Width (mm)',
-                  type: 'dimensions'
-                },
-                {
-                  value: '1(8) 2(10) 3(8) 4(8) 5 (two) || a(16) (wants 16).',
-                  displayLabel: 'Collation',
-                  type: 'collation'
-                },
-                {
-                  value: 'change of hand',
-                  displayLabel: 'Writing',
-                  type: 'handNote'
-                },
-                {
-                  value: 'ff. i + 1-51 + ii-iii',
-                  displayLabel: 'Foliation',
-                  type: 'foliation'
-                }
-              ]
-
+    let(:cocina) do
+      {
+        form: [
+          {
+            value: 'ink on paper',
+            type: 'form'
+          },
+          {
+            value: 'access',
+            type: 'reformatting quality',
+            source: {
+              value: 'MODS reformatting quality terms'
             }
-          ]
-        }
-      end
+          },
+          {
+            value: 'image/jpeg',
+            type: 'media type',
+            source: {
+              value: 'IANA media types'
+            }
+          },
+          {
+            value: '1 sheet',
+            type: 'extent'
+          },
+          {
+            value: 'reformatted digital',
+            type: 'digital origin',
+            source: {
+              value: 'MODS digital origin terms'
+            }
+          }
+        ],
+        note: [
+          {
+            value: 'Small tear at top right corner.',
+            displayLabel: 'Condition'
+          },
+          {
+            value: 'Paper',
+            displayLabel: 'Material',
+            type: 'material'
+          },
+          {
+            value: '34 and 24 lines to a page',
+            displayLabel: 'Layout',
+            type: 'layout'
+          },
+          {
+            value: '210',
+            displayLabel: 'Height (mm)',
+            type: 'dimensions'
+          },
+          {
+            value: '146',
+            displayLabel: 'Width (mm)',
+            type: 'dimensions'
+          },
+          {
+            value: '1(8) 2(10) 3(8) 4(8) 5 (two) || a(16) (wants 16).',
+            displayLabel: 'Collation',
+            type: 'collation'
+          },
+          {
+            value: 'change of hand',
+            displayLabel: 'Writing',
+            type: 'handNote'
+          },
+          {
+            value: 'ff. i + 1-51 + ii-iii',
+            displayLabel: 'Foliation',
+            type: 'foliation'
+          }
+        ]
+      }
     end
   end
 
   describe 'Multiple physical descriptions' do
-    it_behaves_like 'MODS cocina mapping' do
-      let(:mods) do
-        <<~XML
-          <physicalDescription>
-            <form>audio recording</form>
-            <extent>1 audiocassette</extent>
-          </physicalDescription>
-          <physicalDescription>
-            <form>transcript</form>
-            <extent>5 pages</extent>
-          </physicalDescription>
-        XML
-      end
+    xit 'not implemented'
+    let(:mods) do
+      <<~XML
+        <physicalDescription>
+          <form>audio recording</form>
+          <extent>1 audiocassette</extent>
+        </physicalDescription>
+        <physicalDescription>
+          <form>transcript</form>
+          <extent>5 pages</extent>
+        </physicalDescription>
+      XML
+    end
 
-      let(:cocina) do
-        {
-          form: [
-            {
-              structuredValue: [
-                {
-                  value: 'audio recording',
-                  type: 'form'
-                },
-                {
-                  value: '1 audiocassette',
-                  type: 'extent'
-                }
-              ]
-            },
-            {
-              structuredValue: [
-                {
-                  value: 'transcript',
-                  type: 'form'
-                },
-                {
-                  value: '5 pages',
-                  type: 'extent'
-                }
-              ]
-            }
-          ]
-        }
-      end
+    let(:cocina) do
+      {
+        form: [
+          {
+            groupedValue: [
+              {
+                value: 'audio recording',
+                type: 'form'
+              },
+              {
+                value: '1 audiocassette',
+                type: 'extent'
+              }
+            ]
+          },
+          {
+            groupedValue: [
+              {
+                value: 'transcript',
+                type: 'form'
+              },
+              {
+                value: '5 pages',
+                type: 'extent'
+              }
+            ]
+          }
+        ]
+      }
     end
   end
 
@@ -211,7 +204,7 @@ RSpec.describe 'MODS physicalDescription <--> cocina mappings' do
   end
 
   describe 'Display label with multiple form' do
-    it_behaves_like 'MODS cocina mapping' do
+    xit 'not implemented - apply displayLabel to form' do
       let(:mods) do
         <<~XML
           <physicalDescription displayLabel="Medium">
@@ -222,22 +215,94 @@ RSpec.describe 'MODS physicalDescription <--> cocina mappings' do
       end
 
       let(:cocina) do
+        # MODS form doesn't have a displayLabel attribute, so it goes on the parent
+        # element physicalDescription. Subelements with the same displayLabel go
+        # in the same parent physicalDescription element.
         {
           form: [
             {
-              displayLabel: 'Medium',
-              structuredValue: [
+              value: 'metal embossed on wood',
+              type: 'form',
+              displayLabel: 'Medium'
+            },
+            {
+              value: 'mezzotints (prints)',
+              type: 'form',
+              displayLabel: 'Medium'
+            }
+          ]
+        }
+      end
+    end
+  end
+
+  describe 'Multiple physicalDescription with different display labels' do
+    xit 'not implemented - multiple display labels' do
+      let(:mods) do
+        <<~XML
+          <physicalDescription displayLabel="Medium">
+            <form>ink</form>
+          </physicalDescription>
+          <physicalDescription displayLabel="Mount">
+            <form>silk</form>
+          </physicalDescription>
+        XML
+      end
+
+      let(:cocina) do
+        # Forms must go in separate physicalDescription elements to allow
+        # preserving both displayLabels.
+        {
+          form: [
+            {
+              value: 'ink',
+              type: 'form',
+              displayLabel: 'Medium'
+            },
+            {
+              value: 'silk',
+              type: 'form',
+              displayLabel: 'Mount'
+            }
+          ]
+        }
+      end
+    end
+  end
+
+  describe 'Multiple form, some with displayLabel and some without' do
+    xit 'not implemented - mixed form display labels' do
+      let(:mods) do
+        <<~XML
+          <physicalDescription displayLabel="Medium">
+            <form>metal embossed on wood</form>
+          </physicalDescription>
+          <physicalDescription>
+            <form>mezzotints (prints)</form>
+            <note>color</note>
+          </physicalDescription>
+        XML
+      end
+
+      let(:cocina) do
+        # Subelements with no displayLabel go in same physicalDescription
+        {
+          form: [
+            {
+              value: 'metal embossed on wood',
+              type: 'form',
+              displayLabel: 'Medium'
+            },
+            {
+              value: 'mezzotints (prints)',
+              type: 'form'
+            },
+            {
+              note: [
                 {
-                  value: 'metal embossed on wood',
-                  type: 'form'
-                },
-                {
-                  value: 'mezzotints (prints)',
-                  type: 'form'
+                  value: 'color'
                 }
-
               ]
-
             }
           ]
         }
