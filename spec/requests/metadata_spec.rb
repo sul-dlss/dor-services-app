f30758fa869059088723b765703542e7e1eda7e0
Naomi Dushay <ndushay@stanford.edu>
2021-03-11 10:45:38 -0800

    add metadata/mods endpoint to retrieve source mods

--- spec/requests/metadata_spec.rb
+++ spec/requests/metadata_spec.rb
@@ -19,8 +19,23 @@ RSpec.describe 'Display metadata' do
     end
   end
 
+  describe 'mods' do
+    it 'returns the source MODS xml' do
+      get '/v1/objects/druid:mk420bs7601/metadata/mods',
+          headers: { 'Authorization' => "Bearer #{jwt}" }
+      expect(response).to be_successful
+      expect(response.body).to be_equivalent_to <<~XML
+        <mods xmlns="http://www.loc.gov/mods/v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="3.6" xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <titleInfo>
+            <title>Hello</title>
+          </titleInfo>
+        </mods>
+      XML
+    end
+  end
+
   describe 'descriptive' do
-    it 'returns the DC xml' do
+    it 'returns the public xml' do
       get '/v1/objects/druid:mk420bs7601/metadata/descriptive',
           headers: { 'Authorization' => "Bearer #{jwt}" }
       expect(response).to be_successful
