fa0b3abbeb5c51d8c15bf118c90676283878ce82
Justin Coyne <jcoyne@justincoyne.com>
2021-03-31 21:24:37 -0500

    Refactor to make the parameters reveal the intention
    There's no need for the FromFedora::FileSet class to know about tags, all it needs to know is whether to notify Honeybadger or not

--- spec/services/cocina/from_fedora/file_sets_spec.rb
+++ spec/services/cocina/from_fedora/file_sets_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Cocina::FromFedora::FileSets do
   describe '.resource_type' do
-    subject { described_class.resource_type(node) }
+    subject { described_class.resource_type(node, ignore_resource_type_errors: false) }
 
     let(:node) { Nokogiri::XML::DocumentFragment.parse("<resource type=\"#{type}\" />").at_css('resource') }
 
@@ -20,21 +20,21 @@ RSpec.describe Cocina::FromFedora::FileSets do
       it { is_expected.to eq 'http://cocina.sul.stanford.edu/models/resources/3d.jsonld' }
     end
 
-    context 'when an invalid resource type' do
+    context 'when the resource type is invalid' do
       let(:type) { 'bogus' }
 
       before { allow(Honeybadger).to receive(:notify) }
 
-      context 'with non project phoenix tag' do
+      context 'when ignore_resource_type_errors is not set' do
         it 'notifies Honeybadger' do
-          described_class.resource_type(node, tags: ['Project : FunStuff'])
+          described_class.resource_type(node, ignore_resource_type_errors: false)
           expect(Honeybadger).to have_received(:notify)
         end
       end
 
-      context 'with project phoenix tag' do
+      context 'when ignore_resource_type_errors is set' do
         it 'does not notify Honeybadger' do
-          described_class.resource_type(node, tags: ['Google Book : GBS VIEW_FULL', 'Project : FunStuff'])
+          described_class.resource_type(node, ignore_resource_type_errors: true)
           expect(Honeybadger).not_to have_received(:notify)
         end
       end
