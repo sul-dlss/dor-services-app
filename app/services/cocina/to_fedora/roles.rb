dadf47580f3d30f9e6604de8fb2d1853d649faf5
Naomi Dushay <ndushay@stanford.edu>
2021-04-12 21:08:47 -0700

    disambiguate names of some vars in cocina mappings

--- app/services/cocina/to_fedora/roles.rb
+++ app/services/cocina/to_fedora/roles.rb
@@ -5,11 +5,11 @@ module Cocina
     # This transforms the AdminPolicyAdministrative schema to the
     # Fedora 3 roles
     class Roles
-      def self.write(apo, roles)
-        apo.purge_roles
+      def self.write(fedora_apo, roles)
+        fedora_apo.purge_roles
         roles.each do |role|
           role.members.each do |member|
-            apo.add_roleplayer(role.name, member.identifier, member.type.to_sym)
+            fedora_apo.add_roleplayer(role.name, member.identifier, member.type.to_sym)
           end
         end
       end
