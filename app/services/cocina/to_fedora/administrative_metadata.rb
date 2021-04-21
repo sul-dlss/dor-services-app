614a380bd32b17ef47c38d420a9fb2183172ee98
Justin Coyne <jcoyne@justincoyne.com>
2021-04-21 17:00:27 -0500

    Don't leave unused nodes in administrativeMetadata
    Fixes #2644

--- app/services/cocina/to_fedora/administrative_metadata.rb
+++ app/services/cocina/to_fedora/administrative_metadata.rb
@@ -6,18 +6,46 @@ module Cocina
     # Fedora 3 data model rights
     class AdministrativeMetadata
       def self.write(administrative_metadata, administrative)
-        ng_xml = administrative_metadata.ng_xml
-        admin_node = ng_xml.xpath('//administrativeMetadata').first
-        # Clear out the nodes we are updating
-        admin_node.xpath('registration/workflow|registration/collection|dissemination/workflow').each(&:remove)
+        new(administrative_metadata, administrative).write
+      end
 
-        admin_node.add_child "<dissemination><workflow id=\"#{administrative.disseminationWorkflow}\" /></dissemination>" if administrative.disseminationWorkflow
+      def initialize(administrative_metadata, administrative)
+        @administrative_metadata = administrative_metadata
+        @administrative = administrative
+      end
 
-        registration_workflows = Array(administrative.registrationWorkflow).map { |wf_id| "<workflow id=\"#{wf_id}\" />" }.join
-        registration_collections = Array(administrative.collectionsForRegistration).map { |wf_id| "<collection id=\"#{wf_id}\" />" }.join
-        admin_node.add_child "<registration>#{registration_workflows}#{registration_collections}</registration>"
+      attr_reader :administrative_metadata, :administrative
 
+      def write
+        clear_nodes
+        add_dissemination_nodes
+        add_registration_nodes
         administrative_metadata.ng_xml_will_change!
+      end
+
+      def clear_nodes
+        admin_node.xpath('registration/workflow|registration/collection|dissemination/workflow').each(&:remove)
+      end
+
+      def add_dissemination_nodes
+        return unless administrative.disseminationWorkflow
+
+        dissemination_node = admin_node.xpath('dissemination').first || admin_node.add_child('<dissemination/>').first
+        dissemination_node.add_child "<workflow id=\"#{administrative.disseminationWorkflow}\" />"
+      end
+
+      def add_registration_nodes
+        registration_node = admin_node.xpath('registration').first || admin_node.add_child('<registration/>').first
+        Array(administrative.registrationWorkflow).each do |wf_id|
+          registration_node.add_child "<workflow id=\"#{wf_id}\" />"
+        end
+        Array(administrative.collectionsForRegistration).each do |collection|
+          registration_node.add_child "<collection id=\"#{collection}\" />"
+        end
+      end
+
+      def admin_node
+        @admin_node ||= administrative_metadata.ng_xml.xpath('//administrativeMetadata').first
       end
     end
   end
---
+++ spec/services/cocina/to_fedora/administrative_metadata_spec.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Cocina::ToFedora::AdministrativeMetadata do
+  subject(:write) do
+    described_class.write(datastream, administrative)
+  end
+
+  let(:datastream) do
+    Dor::AdministrativeMetadataDS.from_xml(existing)
+  end
+
+  let(:administrative) do
+    Cocina::Models::AdminPolicyAdministrative.new(
+      hasAdminPolicy: 'druid:bc123df4567',
+      registrationWorkflow: ['registrationWF', 'assemblyWF'],
+      disseminationWorkflow: 'wasWF',
+      collectionsForRegistration: ['druid:pp562fx0548', 'druid:qy781dy0220']
+    )
+  end
+
+  context 'when the existing xml is mostly empty' do
+    let(:existing) do
+      <<~XML
+        <administrativeMetadata>
+          <registration/>
+        </administrativeMetadata>
+      XML
+    end
+
+    let(:expected) do
+      <<~XML
+        <administrativeMetadata>\n
+          <registration>
+            <workflow id=\"registrationWF\"/>
+            <workflow id=\"assemblyWF\"/>
+            <collection id=\"druid:pp562fx0548\"/>
+            <collection id=\"druid:qy781dy0220\"/>
+          </registration>\n
+          <dissemination>
+            <workflow id=\"wasWF\"/>
+          </dissemination>
+        </administrativeMetadata>
+      XML
+    end
+
+    it 'writes the converted structure' do
+      write
+      expect(datastream.content).to be_equivalent_to expected
+    end
+  end
+
+  context 'when the xml has some existing values' do
+    let(:existing) do
+      <<~XML
+        <administrativeMetadata>\n
+          <registration>
+            <workflow id=\"foo\"/>
+            <workflow id=\"bar\"/>
+            <collection id=\"druid:123\"/>
+            <collection id=\"druid:456\"/>
+          </registration>\n
+          <dissemination>
+            <workflow id=\"other stuff\"/>
+          </dissemination>
+        </administrativeMetadata>
+      XML
+    end
+
+    let(:expected) do
+      <<~XML
+        <administrativeMetadata>
+          <registration>
+            <workflow id="registrationWF"/>
+            <workflow id="assemblyWF"/>
+            <collection id="druid:pp562fx0548"/>
+            <collection id="druid:qy781dy0220"/>
+          </registration>
+          <dissemination>
+            <workflow id="wasWF"/>
+          </dissemination>
+        </administrativeMetadata>
+      XML
+    end
+
+    it 'writes without leaving unused nodes' do
+      write
+      expect(datastream.content).to be_equivalent_to expected
+    end
+  end
+end
