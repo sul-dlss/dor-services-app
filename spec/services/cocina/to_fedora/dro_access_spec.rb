42047d485dd652b1b956967fe0d49277a6cb3b57
Naomi Dushay <ndushay@stanford.edu>
2021-04-21 16:37:36 -0700

    mapping/access/dro_access_spec: shore up and remove duplication elsewhere

--- spec/services/cocina/to_fedora/dro_access_spec.rb
+++ spec/services/cocina/to_fedora/dro_access_spec.rb
@@ -23,7 +23,6 @@ RSpec.describe Cocina::ToFedora::DROAccess do
 
     before do
       item.rightsMetadata.content = <<~XML
-        <?xml version="1.0"?>
         <rightsMetadata>
           <access type="discover">
             <machine>
@@ -42,7 +41,6 @@ RSpec.describe Cocina::ToFedora::DROAccess do
     it 'builds the xml' do
       apply
       expect(item.rightsMetadata.ng_xml).to be_equivalent_to <<-XML
-        <?xml version="1.0"?>
         <rightsMetadata>
           <access type="discover">
             <machine>
@@ -60,140 +58,6 @@ RSpec.describe Cocina::ToFedora::DROAccess do
           </use>
           <copyright>
             <human>New Copyright Statement</human>
-          </copyright>
-        </rightsMetadata>
-      XML
-    end
-  end
-
-  context 'with cdl access' do
-    let(:access) do
-      Cocina::Models::DROAccess.new(access: 'citation-only', controlledDigitalLending: true, download: 'none')
-    end
-
-    it 'builds the xml' do
-      apply
-      expect(item.rightsMetadata.ng_xml).to be_equivalent_to <<-XML
-        <?xml version="1.0"?>
-          <rightsMetadata>
-            <access type="discover">
-              <machine>
-                <world/>
-              </machine>
-            </access>
-            <access type="read">
-              <machine>
-                <cdl>
-                  <group rule="no-download">stanford</group>
-                </cdl>
-              </machine>
-            </access>
-            <use>
-              <human type="useAndReproduction"/>
-              <human type="creativeCommons"/>
-              <machine type="creativeCommons" uri=""/>
-              <human type="openDataCommons"/>
-              <machine type="openDataCommons" uri=""/>
-            </use>
-            <copyright>
-              <human/>
-            </copyright>
-          </rightsMetadata>
-        </xml>
-      XML
-    end
-  end
-
-  context 'with a CC license' do
-    let(:access) do
-      Cocina::Models::DROAccess.new(license: 'https://creativecommons.org/licenses/by-nc-nd/3.0/')
-    end
-
-    it 'builds the xml' do
-      apply
-      expect(item.rightsMetadata.ng_xml).to be_equivalent_to <<-XML
-        <?xml version="1.0"?>
-        <rightsMetadata>
-          <access type="discover">
-            <machine>
-              <none/>
-            </machine>
-          </access>
-          <access type="read">
-            <machine>
-              <none/>
-            </machine>
-          </access>
-          <use>
-            <human type="useAndReproduction"/>
-            <license>https://creativecommons.org/licenses/by-nc-nd/3.0/</license>
-          </use>
-          <copyright>
-            <human/>
-          </copyright>
-        </rightsMetadata>
-      XML
-    end
-  end
-
-  context 'with an ODC license' do
-    let(:access) do
-      Cocina::Models::DROAccess.new(license: 'http://opendatacommons.org/licenses/by/1.0/')
-    end
-
-    it 'builds the xml' do
-      apply
-      expect(item.rightsMetadata.ng_xml).to be_equivalent_to <<-XML
-        <?xml version="1.0"?>
-        <rightsMetadata>
-          <access type="discover">
-            <machine>
-              <none/>
-            </machine>
-          </access>
-          <access type="read">
-            <machine>
-              <none/>
-            </machine>
-          </access>
-          <use>
-            <human type="useAndReproduction"/>
-            <license>http://opendatacommons.org/licenses/by/1.0/</license>
-          </use>
-          <copyright>
-            <human/>
-          </copyright>
-        </rightsMetadata>
-      XML
-    end
-  end
-
-  context 'with a "none" license' do
-    let(:access) do
-      Cocina::Models::DROAccess.new(license: 'http://cocina.sul.stanford.edu/licenses/none')
-    end
-
-    it 'builds the xml' do
-      apply
-      expect(item.rightsMetadata.ng_xml).to be_equivalent_to <<-XML
-        <?xml version="1.0"?>
-        <rightsMetadata>
-          <access type="discover">
-            <machine>
-              <none/>
-            </machine>
-          </access>
-          <access type="read">
-            <machine>
-              <none/>
-            </machine>
-          </access>
-          <use>
-            <human type="useAndReproduction"/>
-            <license>http://cocina.sul.stanford.edu/licenses/none</license>
-          </use>
-          <copyright>
-            <human/>
           </copyright>
         </rightsMetadata>
       XML
