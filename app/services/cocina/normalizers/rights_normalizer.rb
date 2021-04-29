429096cf3180cabc3ca8ba6459bbfbb85a2600a8
Peter Mangiafico <peter@mangiafico.org>
2021-04-29 10:18:25 -0700

    move roundtrip normalizer into class

--- app/services/cocina/normalizers/rights_normalizer.rb
+++ app/services/cocina/normalizers/rights_normalizer.rb
@@ -13,6 +13,12 @@ module Cocina
         RightsNormalizer.new(rights_ng_xml: rights_ng_xml).normalize
       end
 
+      # @param [Nokogiri::Document] roundtripped rights_ng_xml rights XML to be normalized
+      # @return [Nokogiri::Document] normalized roundtripped rights xml
+      def self.normalize_roundtrip(rights_ng_xml:, original_ng_xml:)
+        RightsNormalizer.new(rights_ng_xml: rights_ng_xml).normalize_roundtrip(original_ng_xml)
+      end
+
       def initialize(rights_ng_xml:)
         @ng_xml = rights_ng_xml.dup
         @ng_xml.encoding = 'UTF-8' if @ng_xml.respond_to?(:encoding=) # sometimes it's a String (?)
@@ -24,9 +30,31 @@ module Cocina
         ng_xml
       end
 
+      def normalize_roundtrip(original_ng_xml)
+        normalize_roundtrip_ng_xml(original_ng_xml)
+        remove_empty_elements(ng_xml.root) # this must be last
+        ng_xml
+      end
+
       private
 
       attr_reader :ng_xml
+
+      def normalize_roundtrip_ng_xml(original_ng_xml)
+        new_ng_xml = ng_xml.dup
+        if license_nodes(original_ng_xml).blank?
+          license_nodes(new_ng_xml).each do |license_node|
+            use_node = license_node.parent
+            license_node.remove
+            use_node.remove if use_node.children.empty?
+          end
+        end
+        new_ng_xml
+      end
+
+      def license_nodes(xml)
+        xml.root.xpath('//license')
+      end
 
       def normalize_original_xml
         new_ng_xml = ng_xml.dup
