b42f6114e60fd43ce2c72c64ad546fca72d1e56a
Peter Mangiafico <peter@mangiafico.org>
2021-04-30 14:39:14 -0700

    adjust rights normalization

--- spec/services/cocina/normalizers/rights_normalizer_spec.rb
+++ spec/services/cocina/normalizers/rights_normalizer_spec.rb
@@ -3,8 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Cocina::Normalizers::RightsNormalizer do
-  let(:normalized_original_ng_xml) { described_class.normalize(rights_ng_xml: original_ng_xml) }
-  let(:normalized_roundtrip_ng_xml) { described_class.normalize_roundtrip(rights_ng_xml: roundtrip_ng_xml, original_ng_xml: original_ng_xml) }
+  let(:normalized_original_ng_xml) { described_class.normalize(datastream: object_rights_ds) }
+  let(:object_rights_ds) { Dor::DefaultObjectRightsDS.new }
+
+  before { allow(object_rights_ds).to receive(:ng_xml).and_return(original_ng_xml) }
 
   context 'when normalizing rights' do
     context 'when dealing with licenses that share a use with use and reproduction' do
@@ -30,36 +32,19 @@ RSpec.describe Cocina::Normalizers::RightsNormalizer do
         XML
       end
 
-      let(:roundtrip_ng_xml) do
-        Nokogiri::XML <<~XML
-          <rightsMetadata>
-            <access type="discover">
-              <machine>
-                <world/>
-              </machine>
-            </access>
-            <access type="read">
-              <machine>
-                <world rule="no-download"/>
-              </machine>
-            </access>
-            <use>
-              <human type="useAndReproduction">Use at will.</human>
-            </use>
-          </rightsMetadata>
-        XML
-      end
-
-      it 'removes createCommons licenses from original rights xml' do
+      it 'removes createCommons licenses from original rights xml and adds correct license URI' do
         expect(original_ng_xml.root.xpath("//use/machine[@type='creativeCommons' and text()]").size).to eq(1)
         expect(original_ng_xml.root.xpath("//use/human[@type='creativeCommons' and text()]").size).to eq(1)
+        expect(original_ng_xml.root.xpath('//use/license').size).to eq(0)
         expect(normalized_original_ng_xml.root.xpath("//use/machine[@type='creativeCommons' and text()]").size).to eq(0)
         expect(normalized_original_ng_xml.root.xpath("//use/human[@type='creativeCommons' and text()]").size).to eq(0)
+        expect(normalized_original_ng_xml.root.xpath('//use/license').size).to eq(1)
+        expect(normalized_original_ng_xml.root.xpath('//use/license').first.text).to eq('https://creativecommons.org/licenses/by-nc/3.0/')
       end
 
       it 'leaves use and reproduction statement in all cases' do
+        expect(original_ng_xml.root.xpath("//use/human[@type='useAndReproduction']").first.text).to eq('Use at will.')
         expect(normalized_original_ng_xml.root.xpath("//use/human[@type='useAndReproduction']").first.text).to eq('Use at will.')
-        expect(normalized_roundtrip_ng_xml.root.xpath("//use/human[@type='useAndReproduction']").first.text).to eq('Use at will.')
       end
     end
 
@@ -85,25 +70,11 @@ RSpec.describe Cocina::Normalizers::RightsNormalizer do
         XML
       end
 
-      let(:roundtrip_ng_xml) do
-        Nokogiri::XML <<~XML
-          <rightsMetadata>
-            <access type="discover">
-              <machine>
-                <world/>
-              </machine>
-            </access>
-            <access type="read">
-              <machine>
-                <world rule="no-download"/>
-              </machine>
-            </access>
-          </rightsMetadata>
-        XML
-      end
-
-      it 'removes createCommons licenses from original rights xml' do
-        expect(normalized_original_ng_xml).to be_equivalent_to(roundtrip_ng_xml)
+      it 'removes createCommons licenses from original rights xml and adds license URI' do
+        expect(normalized_original_ng_xml.root.xpath("//use/machine[@type='creativeCommons' and text()]").size).to eq(0)
+        expect(normalized_original_ng_xml.root.xpath("//use/human[@type='creativeCommons' and text()]").size).to eq(0)
+        expect(normalized_original_ng_xml.root.xpath('//use/license').size).to eq(1)
+        expect(normalized_original_ng_xml.root.xpath('//use/license').first.text).to eq('https://creativecommons.org/licenses/by-nc/3.0/')
       end
     end
   end
