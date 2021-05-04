70330545f02ee40fd634f5afe792256c11f8fc29
Naomi Dushay <ndushay@stanford.edu>
2021-05-04 13:38:24 -0700

    identity_normalizer gets more goodies

--- app/services/cocina/normalizers/identity_normalizer.rb
+++ app/services/cocina/normalizers/identity_normalizer.rb
@@ -19,20 +19,97 @@ module Cocina
       end
 
       def normalize
-        normalize_source_id
+        normalize_dissertation_id_to_source_id
+        normalize_out_uuid
+        normalize_out_admin_tags
+        normalize_out_admin_policy
+        normalize_out_agreement_id
+        normalize_out_set_object_type
+        normalize_out_display_type
+        normalize_out_object_admin_class
+        normalize_out_citation_elements
+        normalize_out_call_sequence_ids
+        normalize_out_empty_other_ids
+        normalize_source_id_whitespace
 
-        regenerate_ng_xml(ng_xml.to_s)
+        regenerate_ng_xml(ng_xml.to_xml)
       end
 
       private
 
       attr_reader :ng_xml
 
-      def normalize_source_id
+      def normalize_source_id_whitespace
         ng_xml.root.xpath('//sourceId').each do |source_node|
           source_node['source'] = source_node['source']&.strip
           source_node.content = source_node.content&.strip
         end
+      end
+
+      # if there is no sourceId, but there is an otherId with name dissertationid, convert the dissertationid to a sourceId
+      #   (obviously only applies to (old) ETDs)
+      def normalize_dissertation_id_to_source_id
+        return if ng_xml.root.xpath('//sourceId').present?
+
+        diss_id_nodes = ng_xml.root.xpath('//otherId[@name="dissertationid"]')
+        return if diss_id_nodes.first&.content.blank?
+
+        new_source_id_node = Nokogiri::XML::Node.new('sourceId', ng_xml)
+        new_source_id_node.content = diss_id_nodes.first.content
+        new_source_id_node['source'] = 'dissertationid'
+        diss_id_nodes.first.parent.add_child(new_source_id_node)
+        diss_id_nodes.first.remove
+      end
+
+      # we don't care about uuids
+      def normalize_out_uuid
+        ng_xml.root.xpath('//otherId[@name="uuid"]').each(&:remove)
+      end
+
+      # administrative tags are stored by the administrative_tag service
+      def normalize_out_admin_tags
+        ng_xml.root.xpath('//tag').each(&:remove)
+      end
+
+      # adminPolicy id is retrieved from RELS-EXT
+      def normalize_out_admin_policy
+        ng_xml.root.xpath('//adminPolicy').each(&:remove)
+      end
+
+      # agreementId is retrieved from RELS-EXT
+      def normalize_out_agreement_id
+        ng_xml.root.xpath('//agreementId').each(&:remove)
+      end
+
+      #  keep objectType collection and drop set (set is vestigial early SDR junk)
+      def normalize_out_set_object_type
+        ng_xml.root.xpath('//objectType[text() = "set"]').each(&:remove) if ng_xml.root.xpath('//objectType[text() = "collection"]').present?
+      end
+
+      # displayType is vestigial early SDR junk
+      def normalize_out_display_type
+        ng_xml.root.xpath('//displayType').each(&:remove)
+      end
+
+      # objectAdminClass is vestigial early SDR junk
+      def normalize_out_object_admin_class
+        ng_xml.root.xpath('//objectAdminClass').each(&:remove)
+      end
+
+      # citationTitle, citationCreator are vestigial early SDR/ETD junk
+      def normalize_out_citation_elements
+        ng_xml.root.xpath('//citationTitle').each(&:remove)
+        ng_xml.root.xpath('//citationCreator').each(&:remove)
+      end
+
+      def normalize_out_empty_other_ids
+        ng_xml.root.xpath('//otherId').select { |el| el.content.blank? }.each(&:remove)
+      end
+
+      # these appear in project phoenix and they are vestigial early SDR junk
+      def normalize_out_call_sequence_ids
+        ng_xml.root.xpath('//otherId[@name="callseq"]').each(&:remove)
+        ng_xml.root.xpath('//otherId[@name="shelfseq"]').each(&:remove)
       end
     end
   end
