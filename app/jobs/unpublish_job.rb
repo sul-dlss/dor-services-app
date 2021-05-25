ac986219cf33728e21873dfbdace72d19242fac2
Justin Coyne <jcoyne@justincoyne.com>
2021-05-25 15:01:25 -0500

    Add missing kwarg when unpublishing

--- app/jobs/unpublish_job.rb
+++ app/jobs/unpublish_job.rb
@@ -6,7 +6,7 @@ class UnpublishJob < ApplicationJob
 
   def perform(druid:, background_job_result:)
     background_job_result.processing!
-    UnpublishService.unpublish(druid)
+    UnpublishService.unpublish(druid: druid)
     EventFactory.create(druid: druid, event_type: 'unpublish_complete', data: { background_job_result_id: background_job_result.id })
   end
 end
