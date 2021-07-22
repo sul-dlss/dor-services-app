90e845da5a2cd52dcd033e0009943cb4f2c8be20
Justin Coyne <jcoyne@justincoyne.com>
2021-07-22 11:27:06 -0500

    Serialize cocina-models to hash for activeJob
    ActiveJob can't support types it doesn't know how to serialize and this causes ActiveJob::SerializationError
    Fixes #2940

--- spec/jobs/update_doi_metadata_job_spec.rb
+++ spec/jobs/update_doi_metadata_job_spec.rb
@@ -4,19 +4,38 @@ require 'rails_helper'
 
 RSpec.describe UpdateDoiMetadataJob, type: :job do
   subject(:perform) do
-    described_class.perform_now(cocina_item)
+    described_class.perform_now(Cocina::Serializer.new.serialize(cocina_item))
   end
 
   let(:cocina_item) do
-    instance_double(Cocina::Models::DRO, externalIdentifier: 'druid:bc123df4567',
-                                         identification: instance_double(Cocina::Models::Identification, doi: '10.0001/bc123df4567'))
+    Cocina::Models.build(
+      'externalIdentifier' => 'druid:bc123df4567',
+      'type' => Cocina::Models::Vocab.image,
+      'version' => 1,
+      'label' => 'testing',
+      'access' => {},
+      'administrative' => {
+        'hasAdminPolicy' => 'druid:xx123xx4567'
+      },
+      'description' => {
+        'title' => [{ 'value' => 'Test obj' }],
+        'subject' => [{ 'type' => 'topic', 'value' => 'word' }]
+      },
+      'structural' => {
+        'contains' => []
+      },
+      'identification' => {
+        'doi' => '10.80343/bc123df4567'
+      }
+    )
   end
+
   let(:attributes) { { titles: [{ title: 'test deposit' }] } }
 
   context 'with no errors' do
     before do
       allow(Cocina::ToDatacite::Attributes).to receive(:mapped_from_cocina).and_return(attributes)
-      stub_request(:put, 'https://fake.datacite.example.com/dois/10.0001/bc123df4567')
+      stub_request(:put, 'https://fake.datacite.example.com/dois/10.80343/bc123df4567')
         .with(
           body: '{"data":{"attributes":{"titles":[{"title":"test deposit"}]}}}'
         )
@@ -31,7 +50,7 @@ RSpec.describe UpdateDoiMetadataJob, type: :job do
   context 'with a remote error' do
     before do
       allow(Cocina::ToDatacite::Attributes).to receive(:mapped_from_cocina).and_return(attributes)
-      stub_request(:put, 'https://fake.datacite.example.com/dois/10.0001/bc123df4567')
+      stub_request(:put, 'https://fake.datacite.example.com/dois/10.80343/bc123df4567')
         .with(
           body: '{"data":{"attributes":{"titles":[{"title":"test deposit"}]}}}'
         )
