fb8c038f8fbdc3766afa97258118971e3fd3f6e1
Naomi Dushay <ndushay@stanford.edu>
2021-07-21 18:31:18 -0700

    access_h2_datacite_spec closer but still xit

--- spec/services/cocina/mapping/descriptive/h2_datacite/access_h2_datacite_spec.rb
+++ spec/services/cocina/mapping/descriptive/h2_datacite/access_h2_datacite_spec.rb
@@ -2,27 +2,42 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Cocina --> DataCite mappings for access (H2 specific)' do
-  describe 'Contact email' do
-    xit 'not implemented' do
-      let(:cocina) do
-        {
-          access: {
-            accessContact: [
-              {
-                value: 'me@stanford.edu',
-                type: 'email',
-                displayLabel: 'Contact'
-              }
-            ]
-          }
-        }
-      end
+RSpec.describe 'Cocina --> DataCite mappings for description.access (H2 specific)' do
+  # NOTE: Because we haven't set a title in this Cocina::Models::Description, it will not validate against the openapi.
+  let(:cocina_description) { Cocina::Models::Description.new(cocina, false, false) }
+  let(:descriptions_attributes) { Cocina::ToDatacite::Note.descriptions_attributes(cocina_description) }
 
+  describe 'Contact email' do
+    let(:cocina) do
+      {
+        access: {
+          accessContact: [
+            {
+              value: 'me@stanford.edu',
+              type: 'email',
+              displayLabel: 'Contact'
+            }
+          ]
+        }
+      }
+    end
+
+    xit 'not implemented' do
       let(:datacite) do
         # no data generated
         {}
       end
     end
+  end
+
+  ### --------------- specs below added by developers ---------------
+
+  context 'when description.access is empty hash' do
+    let(:cocina) do
+      {
+      }
+    end
+
+    xit 'not implemented'
   end
 end
