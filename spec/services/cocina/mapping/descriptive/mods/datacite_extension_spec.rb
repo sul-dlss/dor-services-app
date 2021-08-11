035857de85d421c7d0a316fd3fcf34feffd1ce34
Arcadia Falcone <arcadia@stanford.edu>
2021-08-11 11:22:34 -0700

    Add MODS>Cocina mapping for roundtripping DataCite resourceTypeGeneral

---
+++ spec/services/cocina/mapping/descriptive/mods/datacite_extension_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'MODS physicalDescription <--> cocina mappings' do
+  describe 'DataCite resourceTypeGeneral' do
+    xit 'not implemented' do
+      let(:mods) do
+        <<~XML
+          <extension displayLabel="datacite">
+            <resourceType resourceTypeGeneral="Dataset"/>
+          </extension>
+        XML
+      end
+
+      let(:cocina) do
+        {
+          form: [
+            {
+              value: 'Dataset',
+              type: 'resource type',
+              source: {
+                value: 'DataCite resource types'
+              }
+            }
+          ]
+        }
+      end
+    end
+  end
+end
