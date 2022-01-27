0c2a4bb79588cfc2a998347c089704e512aba771
Michael J. Giarlo <mjgiarlo@stanford.edu>
2022-01-27 09:37:21 -0800

    Cocina-ize the AdminPolicyDefaultsController
    Fixes #3168
    
    This commit changes the AdminPolicyDefaultsController so that it saves and loads Cocina objects instead of Fedora ones. It tests both cases of default defaultAccess and custom defaultAccess. It also copies over an RSpec matcher from Argo allowing comparison of Cocina substructures, in this case the access substructure.

--- app/controllers/admin_policy_defaults_controller.rb
+++ app/controllers/admin_policy_defaults_controller.rb
@@ -2,11 +2,23 @@
 
 # Applies the AdminPolicy defaults to a repository object
 class AdminPolicyDefaultsController < ApplicationController
-  before_action :load_item, only: :apply
+  before_action :load_cocina_object, only: :apply
 
   def apply
-    @item.rightsMetadata.content = @item.admin_policy_object.defaultObjectRights.content
-    @item.save!
+    CocinaObjectStore.save(updated_cocina_object)
     head :no_content
+  end
+
+  private
+
+  def updated_cocina_object
+    @cocina_object.new(access: @cocina_object.access.new(**default_access_from_apo))
+  end
+
+  def default_access_from_apo
+    CocinaObjectStore
+      .find(@cocina_object.administrative.hasAdminPolicy)
+      .administrative
+      .defaultAccess
   end
 end
--- spec/requests/apply_admin_policy_defaults_spec.rb
+++ spec/requests/apply_admin_policy_defaults_spec.rb
@@ -2,24 +2,74 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Get the object' do
+RSpec.describe 'Apply APO access defaults to a member item' do
   before do
-    allow(Dor).to receive(:find).and_return(object)
-    allow(object).to receive(:save!)
+    allow(CocinaObjectStore).to receive(:find).with(object_druid).and_return(cocina_object)
+    allow(CocinaObjectStore).to receive(:find).with(apo_druid).and_return(cocina_admin_policy)
+    allow(CocinaObjectStore).to receive(:save)
   end
 
-  let(:object) { Dor::Item.new(admin_policy_object: admin_policy) }
-  let(:admin_policy) do
-    Dor::AdminPolicyObject.new.tap do |coll|
-      coll.defaultObjectRights.content = '<foo/>'
+  let(:apo_druid) { 'druid:df123cd4567' }
+  let(:object_druid) { 'druid:bc123df4567' }
+  let(:ur_apo_druid) { 'druid:hv992ry2431' }
+  let(:cocina_admin_policy) do
+    Cocina::Models::AdminPolicy.new(
+      externalIdentifier: apo_druid,
+      version: 1,
+      type: Cocina::Models::Vocab.admin_policy,
+      label: 'Dummy APO',
+      administrative: {
+        hasAdminPolicy: ur_apo_druid,
+        defaultAccess: default_access
+      }
+    )
+  end
+  let(:cocina_object) do
+    Cocina::Models::DRO.new(
+      externalIdentifier: object_druid,
+      version: 1,
+      type: Cocina::Models::Vocab.object,
+      label: 'Dummy Object',
+      access: {},
+      administrative: { hasAdminPolicy: apo_druid }
+    )
+  end
+
+  context 'when APO picks up default default object rights' do
+    let(:default_access) do
+      {
+        access: 'world',
+        download: 'world'
+      }
+    end
+
+    it 'copies APO defaultAccess to item access' do
+      post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
+           headers: { 'Authorization' => "Bearer #{jwt}" }
+      expect(response).to be_successful
+      expect(CocinaObjectStore).to have_received(:save)
+        .once
+        .with(cocina_object_with_access(default_access))
     end
   end
 
-  it 'copies the rights metadata from the AdminPolicy to the object' do
-    post '/v1/objects/druid:mk420bs7601/apply_admin_policy_defaults',
-         headers: { 'Authorization' => "Bearer #{jwt}" }
-    expect(response).to be_successful
-    expect(object.rightsMetadata.content).to eq '<foo/>'
-    expect(object).to have_received(:save!)
+  context 'when APO specifies custom default object rights' do
+    let(:default_access) do
+      {
+        access: 'world',
+        download: 'none',
+        useAndReproductionStatement: 'Use at will.',
+        license: 'https://creativecommons.org/licenses/by-nc/3.0/legalcode'
+      }
+    end
+
+    it 'copies APO defaultAccess to item access' do
+      post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
+           headers: { 'Authorization' => "Bearer #{jwt}" }
+      expect(response).to be_successful
+      expect(CocinaObjectStore).to have_received(:save)
+        .once
+        .with(cocina_object_with_access(default_access))
+    end
   end
 end
