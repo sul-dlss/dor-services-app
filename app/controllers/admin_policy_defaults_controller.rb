ce9b6d4d5b851b8b500c5d238e94ebb17ac0a16c
Michael J. Giarlo <mjgiarlo@stanford.edu>
2022-01-28 16:42:19 -0800

    Apply APO defaults only to items that are in the registered or opened states
    Fixes sul-dlss/argo#3019

--- app/controllers/admin_policy_defaults_controller.rb
+++ app/controllers/admin_policy_defaults_controller.rb
@@ -4,9 +4,30 @@
 class AdminPolicyDefaultsController < ApplicationController
   before_action :load_item, only: :apply
 
+  ALLOWED_WORKFLOW_STATES = %i[Registered Opened].freeze
+
   def apply
+    return error_response unless current_workflow_state.in?(ALLOWED_WORKFLOW_STATES)
+
     @item.rightsMetadata.content = @item.admin_policy_object.defaultObjectRights.content
     @item.save!
     head :no_content
+  end
+
+  private
+
+  def current_workflow_state
+    WorkflowClientFactory.build
+                         .status(druid: @item.pid, version: @item.current_version)
+                         .display_simplified
+  end
+
+  def error_response
+    json_api_error(
+      status: :unprocessable_entity,
+      message: "#{@item.pid} is in a state in which it cannot be modified (#{current_workflow_state}): " \
+               'APO defaults can only be applied when an object is either registered or opened for versioning',
+      title: 'Object cannot be modified in current state'
+    )
   end
 end
--- spec/requests/apply_admin_policy_defaults_spec.rb
+++ spec/requests/apply_admin_policy_defaults_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Get the object' do
   before do
     allow(Dor).to receive(:find).and_return(object)
     allow(object).to receive(:save!)
+    allow(WorkflowClientFactory).to receive(:build).and_return(workflow_client)
   end
 
   let(:object) { Dor::Item.new(admin_policy_object: admin_policy) }
@@ -14,12 +15,38 @@ RSpec.describe 'Get the object' do
       coll.defaultObjectRights.content = '<foo/>'
     end
   end
+  let(:workflow_client) { instance_double(Dor::Workflow::Client, status: status_client) }
+  let(:status_client) { instance_double(Dor::Workflow::Client::Status, display_simplified: workflow_state) }
 
-  it 'copies the rights metadata from the AdminPolicy to the object' do
-    post '/v1/objects/druid:mk420bs7601/apply_admin_policy_defaults',
-         headers: { 'Authorization' => "Bearer #{jwt}" }
-    expect(response).to be_successful
-    expect(object.rightsMetadata.content).to eq '<foo/>'
-    expect(object).to have_received(:save!)
+  AdminPolicyDefaultsController::ALLOWED_WORKFLOW_STATES.each do |workflow_state|
+    context "when item is in '#{workflow_state}' state" do
+      let(:workflow_state) { workflow_state }
+
+      it 'copies the rights metadata from the AdminPolicy to the object' do
+        post '/v1/objects/druid:mk420bs7601/apply_admin_policy_defaults',
+             headers: { 'Authorization' => "Bearer #{jwt}" }
+        expect(response).to be_successful
+        expect(object.rightsMetadata.content).to eq '<foo/>'
+        expect(object).to have_received(:save!)
+      end
+    end
+  end
+
+  ['Unknown Status', 'In accessioning', 'Accessioned'].each do |workflow_state|
+    context "when item is in '#{workflow_state}' state" do
+      let(:workflow_state) { workflow_state }
+
+      it 'returns an HTTP 422 response with an error message' do
+        post '/v1/objects/druid:mk420bs7601/apply_admin_policy_defaults',
+             headers: { 'Authorization' => "Bearer #{jwt}" }
+        expect(JSON.parse(response.body)['errors'].first['detail']).to include(
+          "is in a state in which it cannot be modified (#{workflow_state}): APO defaults " \
+          'can only be applied when an object is either registered or opened for versioning'
+        )
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response).not_to be_successful
+        expect(object).not_to have_received(:save!)
+      end
+    end
   end
 end
