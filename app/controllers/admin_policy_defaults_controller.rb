76454c5f3290ed7d48b60a871f33bdd37967cfaf
Michael J. Giarlo <mjgiarlo@stanford.edu>
2022-01-31 08:22:25 -0800

    Apply APO defaults only to items that are in the registered or opened states
    Fixes sul-dlss/argo#3019
    
    Picked from #3265 for easier testing, as both of this commit and the prior commit touch the same controller.

--- app/controllers/admin_policy_defaults_controller.rb
+++ app/controllers/admin_policy_defaults_controller.rb
@@ -2,14 +2,34 @@
 
 # Applies the AdminPolicy defaults to a repository object
 class AdminPolicyDefaultsController < ApplicationController
+  ALLOWED_WORKFLOW_STATES = %w[Registered Opened].freeze
+
   before_action :load_cocina_object, only: :apply
 
   def apply
+    return error_response unless current_workflow_state.in?(ALLOWED_WORKFLOW_STATES)
+
     CocinaObjectStore.save(updated_cocina_object)
     head :no_content
   end
 
   private
+
+  def current_workflow_state
+    WorkflowClientFactory
+      .build
+      .status(druid: @cocina_object.externalIdentifier, version: @cocina_object.version)
+      .display_simplified
+  end
+
+  def error_response
+    json_api_error(
+      status: :unprocessable_entity,
+      message: "#{@cocina_object.externalIdentifier} is in a state in which it cannot be modified (#{current_workflow_state}): " \
+               'APO defaults can only be applied when an object is either registered or opened for versioning',
+      title: 'Object cannot be modified in current state'
+    )
+  end
 
   def updated_cocina_object
     @cocina_object.new(access: @cocina_object.access.new(**default_access_from_apo))
--- spec/requests/apply_admin_policy_defaults_spec.rb
+++ spec/requests/apply_admin_policy_defaults_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe 'Apply APO access defaults to a member item' do
     allow(CocinaObjectStore).to receive(:find).with(object_druid).and_return(cocina_object)
     allow(CocinaObjectStore).to receive(:find).with(apo_druid).and_return(cocina_admin_policy)
     allow(CocinaObjectStore).to receive(:save)
+    allow(WorkflowClientFactory).to receive(:build).and_return(workflow_client)
   end
 
   let(:apo_druid) { 'druid:df123cd4567' }
   let(:object_druid) { 'druid:bc123df4567' }
-  let(:ur_apo_druid) { 'druid:hv992ry2431' }
   let(:cocina_admin_policy) do
     Cocina::Models::AdminPolicy.new(
       externalIdentifier: apo_druid,
@@ -19,7 +19,8 @@ RSpec.describe 'Apply APO access defaults to a member item' do
       type: Cocina::Models::Vocab.admin_policy,
       label: 'Dummy APO',
       administrative: {
-        hasAdminPolicy: ur_apo_druid,
+        hasAdminPolicy: 'druid:hv992ry2431',
+        hasAgreement: 'druid:bc753qt7345',
         defaultAccess: default_access
       }
     )
@@ -34,42 +35,67 @@ RSpec.describe 'Apply APO access defaults to a member item' do
       administrative: { hasAdminPolicy: apo_druid }
     )
   end
+  let(:default_access) do
+    {
+      access: 'world',
+      download: 'world'
+    }
+  end
+  let(:workflow_client) { instance_double(Dor::Workflow::Client, status: status_client) }
+  let(:status_client) { instance_double(Dor::Workflow::Client::Status, display_simplified: workflow_state) }
 
-  context 'when APO picks up default default object rights' do
-    let(:default_access) do
-      {
-        access: 'world',
-        download: 'world'
-      }
-    end
+  AdminPolicyDefaultsController::ALLOWED_WORKFLOW_STATES.each do |workflow_state|
+    context "when item is in '#{workflow_state}' state" do
+      let(:workflow_state) { workflow_state }
 
-    it 'copies APO defaultAccess to item access' do
-      post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
-           headers: { 'Authorization' => "Bearer #{jwt}" }
-      expect(response).to be_successful
-      expect(CocinaObjectStore).to have_received(:save)
-        .once
-        .with(cocina_object_with_access(default_access))
+      context 'when APO picks up default default object rights' do
+        it 'copies APO defaultAccess to item access' do
+          post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
+               headers: { 'Authorization' => "Bearer #{jwt}" }
+          expect(response).to be_successful
+          expect(CocinaObjectStore).to have_received(:save)
+            .once
+            .with(cocina_object_with_access(default_access))
+        end
+      end
+
+      context 'when APO specifies custom default object rights' do
+        let(:default_access) do
+          {
+            access: 'world',
+            download: 'none',
+            useAndReproductionStatement: 'Use at will.',
+            license: 'https://creativecommons.org/licenses/by-nc/3.0/legalcode'
+          }
+        end
+
+        it 'copies APO defaultAccess to item access' do
+          post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
+               headers: { 'Authorization' => "Bearer #{jwt}" }
+          expect(response).to be_successful
+          expect(CocinaObjectStore).to have_received(:save)
+            .once
+            .with(cocina_object_with_access(default_access))
+        end
+      end
     end
   end
 
-  context 'when APO specifies custom default object rights' do
-    let(:default_access) do
-      {
-        access: 'world',
-        download: 'none',
-        useAndReproductionStatement: 'Use at will.',
-        license: 'https://creativecommons.org/licenses/by-nc/3.0/legalcode'
-      }
-    end
+  ['Unknown Status', 'In accessioning', 'Accessioned'].each do |workflow_state|
+    context "when item is in '#{workflow_state}' state" do
+      let(:workflow_state) { workflow_state }
 
-    it 'copies APO defaultAccess to item access' do
-      post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
-           headers: { 'Authorization' => "Bearer #{jwt}" }
-      expect(response).to be_successful
-      expect(CocinaObjectStore).to have_received(:save)
-        .once
-        .with(cocina_object_with_access(default_access))
+      it 'returns an HTTP 422 response with an error message' do
+        post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
+             headers: { 'Authorization' => "Bearer #{jwt}" }
+        expect(JSON.parse(response.body)['errors'].first['detail']).to include(
+          "is in a state in which it cannot be modified (#{workflow_state}): APO defaults " \
+          'can only be applied when an object is either registered or opened for versioning'
+        )
+        expect(response).not_to be_successful
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(CocinaObjectStore).not_to have_received(:save)
+      end
     end
   end
 end
