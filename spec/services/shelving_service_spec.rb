bf68490e324523ae651ede0a1ca9429caa22baf2
Justin Littman <justinlittman@stanford.edu>
2022-01-31 10:54:05 -0500

    Cocina-izes shelving service. Stop getting web archiving stacks location from stacks attribute.
    closes #3082.

--- spec/services/shelving_service_spec.rb
+++ spec/services/shelving_service_spec.rb
@@ -3,18 +3,66 @@
 require 'rails_helper'
 
 RSpec.describe ShelvingService do
-  let(:service) { described_class.new(work) }
-  let(:work) { shelvable_item.new(pid: druid) }
+  let(:druid) { 'druid:ng782rw8378' }
 
-  let(:shelvable_item) do
-    Dor::Item
+  let(:cocina_object) do
+    instance_double(Cocina::Models::DRO, externalIdentifier: druid, structural: structural, type: Cocina::Models::Vocab.book)
+  end
+
+  let(:structural) do
+    Cocina::Models::DROStructural.new(
+      { contains: [{ externalIdentifier: 'http://cocina.sul.stanford.edu/fileSet/8d17c28b-5b3e-477e-912c-f168a1f4213f',
+                     type: 'http://cocina.sul.stanford.edu/models/resources/file.jsonld',
+                     version: 1,
+                     structural: { contains: [{ externalIdentifier: 'http://cocina.sul.stanford.edu/file/be451fd9-7908-4559-9e81-8d6f496a3181',
+                                                type: 'http://cocina.sul.stanford.edu/models/file.jsonld',
+                                                label: 'folder1PuSu/story1u.txt',
+                                                filename: 'folder1PuSu/story1u.txt',
+                                                size: 7888,
+                                                version: 1,
+                                                hasMessageDigests: [{ type: 'sha1',
+                                                                      digest: '61dfac472b7904e1413e0cbf4de432bda2a97627' },
+                                                                    { type: 'md5', digest: 'e2837b9f02e0b0b76f526eeb81c7aa7b' }],
+                                                access: { access: 'world', download: 'world' },
+                                                administrative: { publish: true, sdrPreserve: false, shelve: true },
+                                                hasMimeType: 'text/plain' }] },
+                     label: 'Folder 1' }],
+        isMemberOf: collections }
+    )
+  end
+
+  let(:collections) { ['druid:bb077hj4590'] }
+
+  let(:content_metadata) do
+    <<~XML
+      <contentMetadata type="book" objectId="#{druid}">
+        <resource sequence="1" type="file" id="folder1PuSu">
+          <label>Folder 1</label>
+          <file mimetype="text/plain" shelve="yes" publish="yes" size="7888" preserve="no" id="folder1PuSu/story1u.txt">
+            <checksum type="md5">e2837b9f02e0b0b76f526eeb81c7aa7b</checksum>
+            <checksum type="sha1">61dfac472b7904e1413e0cbf4de432bda2a97627</checksum>
+          </file>
+        </resource>
+      </contentMetadata>
+    XML
   end
 
   let(:stacks_root) { Dir.mktmpdir }
   let(:workspace_root) { Dir.mktmpdir }
+  let(:mock_diff) { instance_double(Moab::FileGroupDifference) }
+  let(:workflow_client) { instance_double(Dor::Workflow::Client, workflows: workflows) }
+  let(:workflows) { ['accessionWF', 'registrationWF'] }
+  let(:stacks_object_pathname) { Pathname(DruidTools::StacksDruid.new(druid, stacks_root).path) }
 
   before do
+    allow(WorkflowClientFactory).to receive(:build).and_return(workflow_client)
     allow(Dor::Config.stacks).to receive_messages(local_stacks_root: stacks_root, local_workspace_root: workspace_root)
+    allow(Cocina::ToFedora::ContentMetadataGenerator).to receive(:generate).and_return(content_metadata)
+    allow(Preservation::Client.objects).to receive(:shelve_content_diff).and_return(mock_diff)
+    allow(ShelvableFilesStager).to receive(:stage)
+    allow(DigitalStacksService).to receive(:remove_from_stacks)
+    allow(DigitalStacksService).to receive(:rename_in_stacks)
+    allow(DigitalStacksService).to receive(:shelve_to_stacks)
   end
 
   after do
@@ -22,70 +70,50 @@ RSpec.describe ShelvingService do
     FileUtils.remove_entry workspace_root
   end
 
-  describe '.shelve' do
-    let(:druid) { 'druid:ng782rw8378' }
-    let(:mock_diff) { instance_double(Moab::FileGroupDifference) }
-
-    before do
-      allow(described_class).to receive(:new).and_return(service)
-      # stub the shelve_diff method which is unit tested below
-      allow(Preservation::Client.objects).to receive(:shelve_content_diff).and_return(mock_diff)
-      allow(ShelvableFilesStager).to receive(:stage).with(druid, work.contentMetadata.content, mock_diff, Pathname)
-    end
-
+  context 'when structural present' do
     it 'pushes file changes for shelve-able files into the stacks' do
       stacks_object_pathname = Pathname(DruidTools::StacksDruid.new(druid, stacks_root).path)
       # make sure the DigitalStacksService is getting the correct delete, rename, and shelve requests
       # (These methods are unit tested in digital_stacks_service_spec.rb)
-      expect(DigitalStacksService).to receive(:remove_from_stacks).with(stacks_object_pathname, mock_diff)
-      expect(DigitalStacksService).to receive(:rename_in_stacks).with(stacks_object_pathname, mock_diff)
-      expect(DigitalStacksService).to receive(:shelve_to_stacks).with(Pathname, stacks_object_pathname, mock_diff)
-      described_class.shelve(work)
+      described_class.shelve(cocina_object)
+      expect(ShelvableFilesStager).to have_received(:stage).with(druid, content_metadata, mock_diff, Pathname)
+      expect(DigitalStacksService).to have_received(:remove_from_stacks).with(stacks_object_pathname, mock_diff)
+      expect(DigitalStacksService).to have_received(:rename_in_stacks).with(stacks_object_pathname, mock_diff)
+      expect(DigitalStacksService).to have_received(:shelve_to_stacks).with(Pathname, stacks_object_pathname, mock_diff)
+      expect(Cocina::ToFedora::ContentMetadataGenerator).to have_received(:generate).with(druid: druid, structural: structural, type: Cocina::Models::Vocab.book)
     end
   end
 
-  describe '#shelve_diff' do
-    subject(:result) { service.send(:shelve_diff) }
+  context 'when structural absent' do
+    let(:structural) { nil }
 
-    let(:druid) { 'druid:jq937jp0017' }
-
-    context 'when contentMetadata exists' do
-      before do
-        allow(Preservation::Client.objects).to receive(:shelve_content_diff)
-      end
-
-      it 'retrieves the differences between the current contentMetadata and preservation via preservation-client gem' do
-        service.send(:shelve_diff)
-        expect(Preservation::Client.objects).to have_received(:shelve_content_diff).with(druid: druid, content_metadata: work.contentMetadata.content)
-      end
-    end
-
-    context 'when contentMetadata does not exist' do
-      it 'raises an error' do
-        work.datastreams.delete 'contentMetadata'
-        expect { result }.to raise_error(Dor::Exception)
-      end
+    it 'raises' do
+      expect { described_class.shelve(cocina_object) }.to raise_error(Dor::Exception)
     end
   end
 
-  describe '#stacks_location' do
-    subject(:location) { service.send(:stacks_location) }
+  context 'when a web archive' do
+    let(:workflows) { %w[accessionWF registrationWF wasCrawlPreassemblyWF] }
+    let(:stacks_object_pathname) { Pathname('/web-archiving-stacks/data/collections/bb077hj4590/ng/782/rw/8378') }
 
-    let(:druid) { 'druid:xy123xy1234' }
-
-    it 'returns the default stack' do
-      work.contentMetadata.content = '<contentMetadata/>'
-      expect(location).to eq stacks_root
+    it 'pushes file changes for shelve-able files into the stacks' do
+      # make sure the DigitalStacksService is getting the correct delete, rename, and shelve requests
+      # (These methods are unit tested in digital_stacks_service_spec.rb)
+      described_class.shelve(cocina_object)
+      expect(ShelvableFilesStager).to have_received(:stage).with(druid, content_metadata, mock_diff, Pathname)
+      expect(DigitalStacksService).to have_received(:remove_from_stacks).with(stacks_object_pathname, mock_diff)
+      expect(DigitalStacksService).to have_received(:rename_in_stacks).with(stacks_object_pathname, mock_diff)
+      expect(DigitalStacksService).to have_received(:shelve_to_stacks).with(Pathname, stacks_object_pathname, mock_diff)
+      expect(Cocina::ToFedora::ContentMetadataGenerator).to have_received(:generate).with(druid: druid, structural: structural, type: Cocina::Models::Vocab.book)
     end
+  end
 
-    it 'returns the absolute stack' do
-      work.contentMetadata.content = '<contentMetadata stacks="/specialstacks"/>'
-      expect(location).to eq '/specialstacks'
-    end
+  context 'when a web archive without collection' do
+    let(:workflows) { %w[accessionWF registrationWF wasCrawlPreassemblyWF] }
+    let(:collections) { [] }
 
-    it 'returns a relative stack' do
-      work.contentMetadata.content = '<contentMetadata stacks="specialstacks"/>'
-      expect { location }.to raise_error(RuntimeError)
+    it 'raises' do
+      expect { described_class.shelve(cocina_object) }.to raise_error(Dor::Exception)
     end
   end
 end
