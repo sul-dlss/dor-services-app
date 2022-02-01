02496803678a92d0f95c3a997207294bff60081e
Michael J. Giarlo <mjgiarlo@stanford.edu>
2022-02-01 15:35:15 -0800

    Validate object type before applying APO access defaults
    Connects to #3168
    
    This operation can only currently operate against DROs (items) and collections.

--- app/controllers/admin_policy_defaults_controller.rb
+++ app/controllers/admin_policy_defaults_controller.rb
@@ -7,7 +7,8 @@ class AdminPolicyDefaultsController < ApplicationController
   before_action :load_cocina_object, only: :apply
 
   def apply
-    return error_response unless current_workflow_state.in?(ALLOWED_WORKFLOW_STATES)
+    return type_error_response unless @cocina_object.dro? || @cocina_object.collection?
+    return workflow_error_response unless current_workflow_state.in?(ALLOWED_WORKFLOW_STATES)
 
     CocinaObjectStore.save(updated_cocina_object)
     head :no_content
@@ -22,7 +23,15 @@ class AdminPolicyDefaultsController < ApplicationController
       .display_simplified
   end
 
-  def error_response
+  def type_error_response
+    json_api_error(
+      status: :bad_request,
+      message: "#{@cocina_object.externalIdentifier} is a #{@cocina_object.class} and this type cannot currently have APO access defaults applied",
+      title: 'Object cannot inherit APO access defaults'
+    )
+  end
+
+  def workflow_error_response
     json_api_error(
       status: :unprocessable_entity,
       message: "#{@cocina_object.externalIdentifier} is in a state in which it cannot be modified (#{current_workflow_state}): " \
@@ -32,17 +41,19 @@ class AdminPolicyDefaultsController < ApplicationController
   end
 
   def updated_cocina_object
-    @cocina_object.new(
-      access: @cocina_object.access.new(**default_access_from_apo),
+    access_updated = @cocina_object.new(
+      access: @cocina_object.access.new(default_access_from_apo)
+    )
+    return access_updated unless access_updated.dro? && access_updated.structural&.contains&.any?
+
+    access_updated.new(
       structural: @cocina_object.structural.new(
         contains: @cocina_object.structural.contains.map do |file_set|
           file_set.new(
             structural: file_set.structural.new(
               contains: file_set.structural.contains.map do |file|
                 file.new(
-                  access: file.access.new(
-                    default_access_from_apo.to_h.slice(*file_access_props)
-                  )
+                  access: file.access.new(default_access_from_apo(file_level: true))
                 )
               end
             )
@@ -56,10 +67,19 @@ class AdminPolicyDefaultsController < ApplicationController
     %i[access controlledDigitalLending download readLocation]
   end
 
-  def default_access_from_apo
-    CocinaObjectStore
-      .find(@cocina_object.administrative.hasAdminPolicy)
-      .administrative
-      .defaultAccess
+  def collection_access_props
+    %i[access copyright license useAndReproductionStatement]
+  end
+
+  def default_access_from_apo(file_level: false)
+    default_access = CocinaObjectStore.find(@cocina_object.administrative.hasAdminPolicy)
+                                      .administrative
+                                      .defaultAccess
+                                      .to_h
+
+    return default_access.slice(*file_access_props) if file_level
+    return default_access.slice(*collection_access_props) if @cocina_object.collection?
+
+    default_access
   end
 end
--- spec/requests/apply_admin_policy_defaults_spec.rb
+++ spec/requests/apply_admin_policy_defaults_spec.rb
@@ -76,114 +76,191 @@ RSpec.describe 'Apply APO access defaults to a member item' do
   end
   let(:workflow_client) { instance_double(Dor::Workflow::Client, status: status_client) }
   let(:status_client) { instance_double(Dor::Workflow::Client::Status, display_simplified: workflow_state) }
+  let(:workflow_state) { 'Registered' }
 
-  AdminPolicyDefaultsController::ALLOWED_WORKFLOW_STATES.each do |workflow_state|
-    context "when item is in '#{workflow_state}' state" do
-      let(:workflow_state) { workflow_state }
-
-      context 'when APO picks up default default object rights' do
-        let(:file_set_with_default_access) do
-          {
-            externalIdentifier: 'http://cocina.sul.stanford.edu/fileSet/234-567-890',
-            version: 1,
-            type: 'http://cocina.sul.stanford.edu/models/resources/file.jsonld',
-            label: 'Page 1',
-            structural: {
-              contains: [
-                {
-                  externalIdentifier: 'http://cocina.sul.stanford.edu/file/223-456-789',
-                  version: 1,
-                  type: 'http://cocina.sul.stanford.edu/models/file.jsonld',
-                  filename: '00001.jp2',
-                  label: '00001.jp2',
-                  hasMimeType: 'image/jp2',
-                  administrative: {
-                    publish: true,
-                    sdrPreserve: true,
-                    shelve: true
-                  },
-                  access: default_access,
-                  hasMessageDigests: []
-                }
-              ]
-            }
-          }
-        end
-
-        it 'copies APO defaultAccess to item access' do
-          post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
-               headers: { 'Authorization' => "Bearer #{jwt}" }
-          expect(response).to be_successful
-          expect(CocinaObjectStore).to have_received(:save)
-            .once
-            .with(cocina_object_with(access: default_access, structural: { contains: [file_set_with_default_access] }))
-        end
+  describe 'object types' do
+    # NOTE: We do not explicitly test DROs here as they are tested everywhere else in this spec.
+    context 'with a collection' do
+      let(:cocina_object) do
+        Cocina::Models::Collection.new(
+          externalIdentifier: object_druid,
+          version: 1,
+          type: Cocina::Models::Vocab.collection,
+          label: 'Dummy Collection',
+          access: {},
+          administrative: { hasAdminPolicy: apo_druid }
+        )
       end
 
-      context 'when APO specifies custom default object rights' do
-        let(:default_access) do
-          {
-            access: 'world',
-            download: 'none',
-            useAndReproductionStatement: 'Use at will.',
-            license: 'https://creativecommons.org/licenses/by-nc/3.0/legalcode'
-          }
-        end
+      it 'copies APO defaultAccess to collection access' do
+        post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
+             headers: { 'Authorization' => "Bearer #{jwt}" }
+        expect(response).to be_successful
+        expect(CocinaObjectStore).to have_received(:save)
+          .once
+          .with(cocina_object_with(access: default_access.slice(:access, :copyright, :license, :useAndReproductionStatement)))
+      end
+    end
 
-        let(:file_set_with_custom_access) do
-          {
-            externalIdentifier: 'http://cocina.sul.stanford.edu/fileSet/234-567-890',
-            version: 1,
-            type: 'http://cocina.sul.stanford.edu/models/resources/file.jsonld',
-            label: 'Page 1',
-            structural: {
-              contains: [
-                {
-                  externalIdentifier: 'http://cocina.sul.stanford.edu/file/223-456-789',
-                  version: 1,
-                  type: 'http://cocina.sul.stanford.edu/models/file.jsonld',
-                  filename: '00001.jp2',
-                  label: '00001.jp2',
-                  hasMimeType: 'image/jp2',
-                  administrative: {
-                    publish: true,
-                    sdrPreserve: true,
-                    shelve: true
-                  },
-                  access: default_access.slice(:access, :download),
-                  hasMessageDigests: []
-                }
-              ]
-            }
+    context 'with an APO' do
+      let(:cocina_object) do
+        Cocina::Models::AdminPolicy.new(
+          externalIdentifier: object_druid,
+          version: 1,
+          type: Cocina::Models::Vocab.admin_policy,
+          label: 'Dummy APO',
+          administrative: {
+            hasAdminPolicy: 'druid:hv992ry2431',
+            hasAgreement: 'druid:bc753qt7345',
+            defaultAccess: default_access
           }
-        end
+        )
+      end
 
-        it 'copies APO defaultAccess to item access' do
-          post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
-               headers: { 'Authorization' => "Bearer #{jwt}" }
-          expect(response).to be_successful
-          expect(CocinaObjectStore).to have_received(:save)
-            .once
-            .with(cocina_object_with(access: default_access, structural: { contains: [file_set_with_custom_access] }))
-        end
+      it 'returns an HTTP 400 response with an error message' do
+        post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
+             headers: { 'Authorization' => "Bearer #{jwt}" }
+        expect(JSON.parse(response.body)['errors'].first['detail']).to include(
+          "#{object_druid} is a Cocina::Models::AdminPolicy and this type cannot currently have APO access defaults applied"
+        )
+        expect(response).not_to be_successful
+        expect(response).to have_http_status(:bad_request)
+        expect(CocinaObjectStore).not_to have_received(:save)
       end
     end
   end
 
-  ['Unknown Status', 'In accessioning', 'Accessioned'].each do |workflow_state|
-    context "when item is in '#{workflow_state}' state" do
-      let(:workflow_state) { workflow_state }
+  context 'an object without structural' do
+    let(:cocina_object) do
+      Cocina::Models::DRO.new(
+        externalIdentifier: object_druid,
+        version: 1,
+        type: Cocina::Models::Vocab.object,
+        label: 'Dummy Object',
+        access: {},
+        administrative: { hasAdminPolicy: apo_druid }
+      )
+    end
 
-      it 'returns an HTTP 422 response with an error message' do
-        post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
-             headers: { 'Authorization' => "Bearer #{jwt}" }
-        expect(JSON.parse(response.body)['errors'].first['detail']).to include(
-          "is in a state in which it cannot be modified (#{workflow_state}): APO defaults " \
-          'can only be applied when an object is either registered or opened for versioning'
-        )
-        expect(response).not_to be_successful
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(CocinaObjectStore).not_to have_received(:save)
+    it 'copies APO defaultAccess to item access' do
+      post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
+           headers: { 'Authorization' => "Bearer #{jwt}" }
+      expect(response).to be_successful
+      expect(CocinaObjectStore).to have_received(:save)
+        .once
+        .with(cocina_object_with(access: default_access))
+    end
+  end
+
+  describe 'workflow states' do
+    AdminPolicyDefaultsController::ALLOWED_WORKFLOW_STATES.each do |workflow_state|
+      context "when item is in '#{workflow_state}' state" do
+        let(:workflow_state) { workflow_state }
+
+        context 'when APO picks up default default object rights' do
+          let(:file_set_with_default_access) do
+            {
+              externalIdentifier: 'http://cocina.sul.stanford.edu/fileSet/234-567-890',
+              version: 1,
+              type: 'http://cocina.sul.stanford.edu/models/resources/file.jsonld',
+              label: 'Page 1',
+              structural: {
+                contains: [
+                  {
+                    externalIdentifier: 'http://cocina.sul.stanford.edu/file/223-456-789',
+                    version: 1,
+                    type: 'http://cocina.sul.stanford.edu/models/file.jsonld',
+                    filename: '00001.jp2',
+                    label: '00001.jp2',
+                    hasMimeType: 'image/jp2',
+                    administrative: {
+                      publish: true,
+                      sdrPreserve: true,
+                      shelve: true
+                    },
+                    access: default_access,
+                    hasMessageDigests: []
+                  }
+                ]
+              }
+            }
+          end
+
+          it 'copies APO defaultAccess to item access' do
+            post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
+                 headers: { 'Authorization' => "Bearer #{jwt}" }
+            expect(response).to be_successful
+            expect(CocinaObjectStore).to have_received(:save)
+              .once
+              .with(cocina_object_with(access: default_access, structural: { contains: [file_set_with_default_access] }))
+          end
+        end
+
+        context 'when APO specifies custom default object rights' do
+          let(:default_access) do
+            {
+              access: 'world',
+              download: 'none',
+              useAndReproductionStatement: 'Use at will.',
+              license: 'https://creativecommons.org/licenses/by-nc/3.0/legalcode'
+            }
+          end
+
+          let(:file_set_with_custom_access) do
+            {
+              externalIdentifier: 'http://cocina.sul.stanford.edu/fileSet/234-567-890',
+              version: 1,
+              type: 'http://cocina.sul.stanford.edu/models/resources/file.jsonld',
+              label: 'Page 1',
+              structural: {
+                contains: [
+                  {
+                    externalIdentifier: 'http://cocina.sul.stanford.edu/file/223-456-789',
+                    version: 1,
+                    type: 'http://cocina.sul.stanford.edu/models/file.jsonld',
+                    filename: '00001.jp2',
+                    label: '00001.jp2',
+                    hasMimeType: 'image/jp2',
+                    administrative: {
+                      publish: true,
+                      sdrPreserve: true,
+                      shelve: true
+                    },
+                    access: default_access.slice(:access, :download),
+                    hasMessageDigests: []
+                  }
+                ]
+              }
+            }
+          end
+
+          it 'copies APO defaultAccess to item access' do
+            post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
+                 headers: { 'Authorization' => "Bearer #{jwt}" }
+            expect(response).to be_successful
+            expect(CocinaObjectStore).to have_received(:save)
+              .once
+              .with(cocina_object_with(access: default_access, structural: { contains: [file_set_with_custom_access] }))
+          end
+        end
+      end
+    end
+
+    ['Unknown Status', 'In accessioning', 'Accessioned'].each do |workflow_state|
+      context "when item is in '#{workflow_state}' state" do
+        let(:workflow_state) { workflow_state }
+
+        it 'returns an HTTP 422 response with an error message' do
+          post "/v1/objects/#{object_druid}/apply_admin_policy_defaults",
+               headers: { 'Authorization' => "Bearer #{jwt}" }
+          expect(JSON.parse(response.body)['errors'].first['detail']).to include(
+            "is in a state in which it cannot be modified (#{workflow_state}): APO defaults " \
+            'can only be applied when an object is either registered or opened for versioning'
+          )
+          expect(response).not_to be_successful
+          expect(response).to have_http_status(:unprocessable_entity)
+          expect(CocinaObjectStore).not_to have_received(:save)
+        end
       end
     end
   end
