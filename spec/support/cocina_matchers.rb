2c58c9112c7d97c196f77ae3c1c8c4c7532f33a9
Michael J. Giarlo <mjgiarlo@stanford.edu>
2022-01-31 16:28:14 -0800

    Expand cocina object matcher to support arbitrary cocina k/v pairs

--- spec/support/cocina_matchers.rb
+++ spec/support/cocina_matchers.rb
@@ -34,16 +34,18 @@ module CocinaMatchers
   end
 
   # NOTE: each k/v pair in the hash passed to this matcher will need to be present in actual
-  matcher :cocina_object_with_access do |expected|
-    match do |actual|
-      expected.all? do |expected_key, expected_value|
-        # NOTE: there's no better method on Hash that I could find for this.
-        #        #include? and #member? only check keys, not k/v pairs
-        actual.access.to_h.any? do |actual_key, actual_value|
-          if expected_value.is_a?(Hash) && actual_value.is_a?(Hash)
-            expected_value.all? { |pair| actual_value.to_a.include?(pair) }
-          else
-            actual_key == expected_key && actual_value == expected_value
+  matcher :cocina_object_with do |**kwargs|
+    kwargs.each do |cocina_section, expected|
+      match do |actual|
+        expected.all? do |expected_key, expected_value|
+          # NOTE: there's no better method on Hash that I could find for this.
+          #        #include? and #member? only check keys, not k/v pairs
+          actual.public_send(cocina_section).to_h.any? do |actual_key, actual_value|
+            if expected_value.is_a?(Hash) && actual_value.is_a?(Hash)
+              expected_value.all? { |pair| actual_value.to_a.include?(pair) }
+            else
+              actual_key == expected_key && actual_value == expected_value
+            end
           end
         end
       end
