cbefadef042e98a65089f9c9407d1e3ae3f3d5ff
Laura Wrubel <lwrubel@stanford.edu>
2022-02-02 09:42:56 -0500

    Mapping name subjects with parallelValue and subdivisions.

--- spec/services/cocina/to_fedora/descriptive/subject_name_spec.rb
+++ spec/services/cocina/to_fedora/descriptive/subject_name_spec.rb
@@ -275,4 +275,67 @@ RSpec.describe Cocina::ToFedora::Descriptive::Subject do
       XML
     end
   end
+
+  context 'with multiple person parallelValues' do
+    let(:subjects) do
+      [
+        Cocina::Models::DescriptiveValue.new(
+          {
+            structuredValue: [
+              {
+                parallelValue: [
+                  {
+                    structuredValue: [
+                      {
+                        value: 'Andrada',
+                        type: 'surname'
+                      },
+                      {
+                        value: 'Leitao, Francisco d\'',
+                        type: 'forename'
+                      },
+                      {
+                        value: '17th C.',
+                        type: 'life dates'
+                      }
+                    ]
+                  },
+                  {
+                    value: 'Andrada, L. F.',
+                    type: 'surname'
+                  },
+                  {
+                    value: 'Andrada, Leitao, Francisco d\', 17th C.',
+                    type: 'display'
+                  }
+                ],
+                type: 'person'
+              }
+            ]
+          }
+        )
+      ]
+    end
+
+    it 'builds the xml' do
+      expect(xml).to be_equivalent_to <<~XML
+        <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns="http://www.loc.gov/mods/v3" version="3.6"
+          xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-6.xsd">
+          <subject>
+            <name type="personal">
+              <namePart type="family">Andrada</namePart>
+              <namePart type="given">Leitao, Francisco d'</namePart>
+              <namePart type="date">17th C.</namePart>
+              <displayForm>Andrada, Leitao, Francisco d', 17th C.</displayForm>
+            </name>
+            <name type="personal">
+              <namePart type="family">Andrada, L. F.</namePart>
+              <displayForm>Andrada, Leitao, Francisco d', 17th C.</displayForm>
+            </name>
+          </subject>
+        </mods>
+      XML
+    end
+  end
 end
