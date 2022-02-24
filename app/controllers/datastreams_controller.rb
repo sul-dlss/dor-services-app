29c8450ce37a283d0c547d71ae5783d13619973f
Justin Coyne <jcoyne@justincoyne.com>
2022-02-24 14:27:45 -0600

    Stop using load_item

--- app/controllers/datastreams_controller.rb
+++ app/controllers/datastreams_controller.rb
@@ -4,18 +4,18 @@
 # This controller is intended to be temporary until we can decouple Argo's UX
 # from the datastream abstraction.
 class DatastreamsController < ApplicationController
-  before_action :load_item
-
   def index
-    result = @item.datastreams
-                  .reject { |name, instance| instance.new? || name == 'workflows' }
-                  .values
-                  .map { |ds| serialize_datastream(ds) }
+    item = Dor.find(params[:object_id])
+    result = item.datastreams
+                 .reject { |name, instance| instance.new? || name == 'workflows' }
+                 .values
+                 .map { |ds| serialize_datastream(ds) }
     render json: result
   end
 
   def show
-    render xml: @item.datastreams[params[:id]].content
+    item = Dor.find(params[:object_id])
+    render xml: item.datastreams[params[:id]].content
   end
 
   private
