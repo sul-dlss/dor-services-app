6e90bbba8ecd628a116859aef6950f18b27bfcbd
Justin Coyne <jcoyne@justincoyne.com>
2022-02-28 13:54:20 -0600

    Export only the required datastreams to preservation
    Fixes #3465

--- app/services/preservation_metadata_extractor.rb
+++ app/services/preservation_metadata_extractor.rb
@@ -12,67 +12,32 @@ class PreservationMetadataExtractor
 
   # @param [Cocina::Models::DRO, Cocina::Models::AdminPolicy, Cocina::Models::Collection] cocina_object The representation of the digital object
   # @param [DruidTools::Druid] workspace The representation of the item's work area
-  # @param [Hash<Symbol,Bool>] datastream_config the list of datastreams to export and whether they are required or not.
-  def initialize(cocina_object:, workspace:, datastream_config: DEFAULT_DATASTREAM_CONFIG)
+  def initialize(cocina_object:, workspace:)
     @cocina_object = cocina_object
-    @datastream_config = datastream_config
     @metadata_dir = Pathname.new(workspace.path('metadata', true))
   end
 
   # @return [Pathname] metadata directory
   def extract
-    extract_datastreams
+    generate_xml
     extract_cocina
     metadata_dir
   end
 
   private
 
-  attr_reader :metadata_dir, :datastream_config, :cocina_object
+  attr_reader :metadata_dir, :cocina_object
 
-  # Which datastreams to export and whether they are required or not
-  DEFAULT_DATASTREAM_CONFIG =
-    {
-      administrativeMetadata: false,
-      contentMetadata: false,
-      descMetadata: true,
-      defaultObjectRights: false,
-      events: false,
-      embargoMetadata: false,
-      identityMetadata: true,
-      provenanceMetadata: false,
-      relationshipMetadata: true,
-      rightsMetadata: false,
-      roleMetadata: false,
-      sourceMetadata: false,
-      versionMetadata: true,
-      workflows: false,
-      geoMetadata: false
-    }.freeze
+  # Generate all the required xml files
+  def generate_xml
+    workflow_file = metadata_dir.join('workflows.xml')
+    workflow_file.open('w') { |f| f << workflow_xml }
 
-  # Pull all the datastreams specified in the configuration file
-  #   into the workspace's metadata directory, overwriting existing file if present
-  def extract_datastreams
-    fedora_object = Dor.find(cocina_object.externalIdentifier)
-    datastream_config.each do |ds_name, required|
-      metadata_file = metadata_dir.join("#{ds_name}.xml")
-      metadata_string = datastream_content(fedora_object, ds_name, required)
-      metadata_file.open('w') { |f| f << metadata_string } if metadata_string
-    end
-  end
+    versions_file = metadata_dir.join('versionMetadata.xml')
+    versions_file.open('w') { |f| f << version_xml }
 
-  # @param [Symbol] ds_name The name of the desired Fedora datastream
-  # @param [Boolean] required is the datastream required
-  # @return [String] return the xml text of the specified datastream if it exists.
-  #   If not found, return nil unless it is a required datastream in which case raise exception
-  def datastream_content(fedora_object, ds_name, required)
-    ds = (ds_name == :relationshipMetadata ? 'RELS-EXT' : ds_name.to_s)
-    return workflow_xml if ds_name == :workflows
-    return version_xml(fedora_object) if ds_name == :versionMetadata
-    return content_xml if ds_name == :contentMetadata
-    return fedora_object.datastreams[ds].content if fedora_object.datastreams.key?(ds) && !fedora_object.datastreams[ds].new?
-
-    raise "required datastream #{ds_name} for #{fedora_object.pid} not found in DOR" if required
+    content_file = metadata_dir.join('contentMetadata.xml')
+    content_file.open('w') { |f| f << content_xml }
   end
 
   # Get the workflow xml representation from the workflow service
@@ -80,7 +45,8 @@ class PreservationMetadataExtractor
     WorkflowClientFactory.build.all_workflows_xml(cocina_object.externalIdentifier)
   end
 
-  def version_xml(fedora_object)
+  def version_xml
+    fedora_object = Dor.find(cocina_object.externalIdentifier)
     # This can be removed after migration.
     VersionMigrationService.migrate(fedora_object)
 
--- spec/services/preservation_metadata_extractor_spec.rb
+++ spec/services/preservation_metadata_extractor_spec.rb
@@ -5,7 +5,6 @@ require 'rails_helper'
 RSpec.describe PreservationMetadataExtractor do
   let(:workspace) { instance_double(DruidTools::Druid, path: 'foo') }
   let(:druid) { 'druid:nc893zj8956' }
-  let(:item) { instance_double(Dor::Item, pid: druid) }
   let(:instance) { described_class.new(workspace: workspace, cocina_object: cocina_object) }
   let(:cocina_object) do
     Cocina::Models::DRO.new({
@@ -24,196 +23,47 @@ RSpec.describe PreservationMetadataExtractor do
   end
 
   before do
-    allow(Dor).to receive(:find).and_return(item)
+    allow(Dor).to receive(:find).and_return(instance_double(Dor::Item, pid: druid))
   end
 
   describe '.extract' do
     subject(:extract) { instance.extract }
 
     let(:metadata_dir) { instance_double(Pathname) }
-    let(:metadata_file) { instance_double(Pathname, exist?: false) }
-    let(:metadata_string) { '<metadata/>' }
+    let(:workflow_path) { instance_double(Pathname, exist?: false, open: true) }
+    let(:version_path) { instance_double(Pathname, exist?: false, open: true) }
+    let(:content_path) { instance_double(Pathname, exist?: false, open: true) }
+    let(:workflow_file) { instance_double(File, :<< => nil) }
+    let(:version_file) { instance_double(File, :<< => nil) }
+    let(:content_file) { instance_double(File, :<< => nil) }
 
     before do
       allow(workspace).to receive(:path).with('metadata', true).and_return('metadata_dir')
       expect(Pathname).to receive(:new).with('metadata_dir').and_return(metadata_dir)
-      expect(metadata_dir).to receive(:join).at_least(6).times.and_return(metadata_file)
-      expect(metadata_file).to receive(:open).at_least(6).times
-      allow(instance).to receive(:datastream_content).and_return(metadata_string)
+      allow(Pathname).to receive(:new).and_call_original
+      allow(metadata_dir).to receive(:join).with('workflows.xml').and_return(workflow_path)
+      allow(metadata_dir).to receive(:join).with('versionMetadata.xml').and_return(version_path)
+      allow(metadata_dir).to receive(:join).with('contentMetadata.xml').and_return(content_path)
+
       allow(instance).to receive(:extract_cocina)
+      allow(workflow_path).to receive(:open).and_yield(workflow_file)
+      allow(version_path).to receive(:open).and_yield(version_file)
+      allow(content_path).to receive(:open).and_yield(content_file)
+
+      stub_request(:get, 'https://workflow.example.com/workflow/objects/druid:nc893zj8956/workflows')
+        .to_return(status: 200, body: '<workflow-stuff />', headers: {})
+      allow(VersionMigrationService).to receive(:migrate)
     end
 
     it 'extracts the metadata' do
       extract
-      expect(instance).to have_received(:datastream_content).with(item, :administrativeMetadata, false)
-      expect(instance).to have_received(:datastream_content).with(item, :contentMetadata, false)
-      expect(instance).to have_received(:datastream_content).with(item, :defaultObjectRights, false)
-      expect(instance).to have_received(:datastream_content).with(item, :descMetadata, true).once
-      expect(instance).to have_received(:datastream_content).with(item, :events, false)
-      expect(instance).to have_received(:datastream_content).with(item, :geoMetadata, false)
-      expect(instance).to have_received(:datastream_content).with(item, :embargoMetadata, false)
-      expect(instance).to have_received(:datastream_content).with(item, :identityMetadata, true)
-      expect(instance).to have_received(:datastream_content).with(item, :provenanceMetadata, false)
-      expect(instance).to have_received(:datastream_content).with(item, :relationshipMetadata, true)
-      expect(instance).to have_received(:datastream_content).with(item, :roleMetadata, false)
-      expect(instance).to have_received(:datastream_content).with(item, :sourceMetadata, false)
-      expect(instance).to have_received(:datastream_content).with(item, :rightsMetadata, false)
-      expect(instance).to have_received(:datastream_content).with(item, :versionMetadata, true)
-      expect(instance).to have_received(:datastream_content).with(item, :workflows, false)
+      expect(workflow_file).to have_received(:<<).with('<workflow-stuff />')
+      expect(version_file).to have_received(:<<)
+        .with("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<versionMetadata objectId=\"druid:nc893zj8956\"/>\n")
+      expect(content_file).to have_received(:<<)
+        .with("<?xml version=\"1.0\"?>\n<contentMetadata objectId=\"druid:nc893zj8956\" type=\"book\"/>\n")
+
       expect(instance).to have_received(:extract_cocina)
-    end
-  end
-
-  describe '#datastream_content' do
-    subject(:datastream_content) { instance.send(:datastream_content, item, ds_name, required) }
-
-    let(:ds_name) { :myMetadata }
-    let(:datastream) { instance_double(Dor::ContentMetadataDS) }
-    let(:required) { true }
-
-    before do
-      allow(item).to receive(:datastreams).and_return('myMetadata' => datastream)
-    end
-
-    it 'retrieves content of a required datastream' do
-      metadata_string = '<metadata/>'
-      expect(datastream).to receive(:new?).and_return(false)
-      expect(datastream).to receive(:content).and_return(metadata_string)
-      expect(datastream_content).to eq metadata_string
-    end
-
-    context 'when datastream is workflows' do
-      let(:ds_name) { :workflows }
-      let(:client) { instance_double(Dor::Workflow::Client, all_workflows_xml: '<workflows />') }
-
-      before do
-        allow(WorkflowClientFactory).to receive(:build).and_return(client)
-      end
-
-      it { is_expected.to eq '<workflows />' }
-    end
-
-    context 'when datastream is versionMetadata' do
-      let(:ds_name) { :versionMetadata }
-
-      let(:expected_xml) { '<versionMetadata />' }
-
-      before do
-        allow(VersionMigrationService).to receive(:migrate)
-        allow(ObjectVersion).to receive(:version_xml).and_return(expected_xml)
-      end
-
-      it { is_expected.to be_equivalent_to expected_xml }
-    end
-
-    context 'when datastream is contentMetadata' do
-      let(:ds_name) { :contentMetadata }
-      let(:structural) do
-        {
-          contains: [{
-            type: Cocina::Models::Vocab::Resources.image,
-            externalIdentifier: 'wt183gy6220',
-            label: 'Image 1',
-            version: 1,
-            structural: {
-              contains: [{
-                type: Cocina::Models::Vocab.file,
-                externalIdentifier: 'wt183gy6220_1',
-                label: 'Image 1',
-                filename: 'wt183gy6220_00_0001.jp2',
-                hasMimeType: 'image/jp2',
-                size: 3_182_927,
-                version: 1,
-                access: {},
-                administrative: {
-                  publish: false,
-                  sdrPreserve: false,
-                  shelve: false
-                },
-                hasMessageDigests: []
-              }]
-            }
-          }]
-        }
-      end
-      let(:cocina_object) do
-        Cocina::Models::DRO.new({
-                                  cocinaVersion: '0.0.1',
-                                  externalIdentifier: druid,
-                                  type: Cocina::Models::Vocab.book,
-                                  label: 'Test DRO',
-                                  version: 1,
-                                  description: {
-                                    title: [{ value: 'Test DRO' }],
-                                    purl: "https://purl.stanford.edu/#{druid.delete_prefix('druid:')}"
-                                  },
-                                  access: { access: 'world', download: 'world' },
-                                  administrative: { hasAdminPolicy: 'druid:hy787xj5878' },
-                                  structural: structural
-                                })
-      end
-
-      let(:fileset_id) { 'http://cocina.sul.stanford.edu/fileSet/3f231c45-5af9-4530-bea9-f99982e394fb' }
-      let(:expected_xml) do
-        <<~XML
-          <contentMetadata objectId="druid:nc893zj8956" type="book">\n
-            <resource id="http://cocina.sul.stanford.edu/fileSet/3f231c45-5af9-4530-bea9-f99982e394fb" sequence="1" type="image">\n
-              <label>Image 1</label>\n
-              <file id="wt183gy6220_00_0001.jp2" mimetype="image/jp2" size="3182927" publish="no" shelve="no" preserve="no"/>\n
-            </resource>\n
-          </contentMetadata>
-        XML
-      end
-
-      before do
-        allow(Cocina::IdGenerator).to receive(:generate_or_existing_fileset_id).and_return(fileset_id)
-      end
-
-      it 'returns the correct datastream xml' do
-        expect(datastream_content).to be_equivalent_to expected_xml
-      end
-
-      context 'when cocina structural is nil (APOs, collections)' do
-        let(:cocina_object) do
-          Cocina::Models::Collection.new({
-                                           cocinaVersion: '0.0.1',
-                                           externalIdentifier: druid,
-                                           type: Cocina::Models::Vocab.collection,
-                                           label: 'Test Collection',
-                                           version: 1,
-                                           description: {
-                                             title: [{ value: 'Test Collection' }],
-                                             purl: "https://purl.stanford.edu/#{druid.delete_prefix('druid:')}"
-                                           },
-                                           access: { access: 'world' },
-                                           administrative: { hasAdminPolicy: 'druid:hy787xj5878' }
-                                         })
-        end
-
-        it 'returns nil' do
-          expect(datastream_content).to be_nil
-        end
-      end
-    end
-
-    context 'when datastream is empty or missing' do
-      let(:ds_name) { :dummy }
-
-      before do
-        expect(datastream).not_to receive(:content)
-      end
-
-      context 'when the datastream is optional' do
-        let(:required) { false }
-
-        it { is_expected.to be_nil }
-      end
-
-      context 'when the datastream is required' do
-        it 'raises an error' do
-          expect { datastream_content }.to raise_exception(RuntimeError)
-        end
-      end
     end
   end
 
@@ -221,7 +71,6 @@ RSpec.describe PreservationMetadataExtractor do
     let(:metadata_dir) { instance_double(Pathname) }
     let(:metadata_file) { instance_double(Pathname, exist?: false) }
     let(:file) { instance_double(File, :<< => nil) }
-    # let(:metadata_string) { '<metadata/>' }
 
     before do
       allow(workspace).to receive(:path).with('metadata', true).and_return('metadata_dir')
