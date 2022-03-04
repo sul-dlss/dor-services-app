2ccf2b19ec01915e5bc439727022984930c87e3a
Naomi Dushay <ndushay@stanford.edu>
2022-03-04 11:01:37 -0800

    refactor migrate script into class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ SimpleCov.start :rails do
   add_filter '/lib/fedora_archiver.rb'
   add_filter '/lib/fedora_cache.rb'
   add_filter '/lib/fedora_loader.rb'
+  add_filter '/lib/fedora_migrator.rb'
   add_filter '/lib/report.rb'
   add_filter '/lib/unique_report.rb'
   add_filter '/lib/cache_cocina_object_store.rb'
