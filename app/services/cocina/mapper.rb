0a30df767dc236674ae3a64ea03dae93778e0f2d
Naomi Dushay <ndushay@stanford.edu>
2022-03-08 08:17:48 -0800

    add backtrace to improved error message

--- app/services/cocina/mapper.rb
+++ app/services/cocina/mapper.rb
@@ -50,8 +50,8 @@ module Cocina
       klass.new(props)
     rescue StandardError => e
       new_message = "Unable to build cocina props - #{e.message}"
-      Honeybadger.notify(new_message)
-      raise UnexpectedBuildError, new_message # wrap unexpected StandardError, caller will probably want to look at #cause
+      Honeybadger.notify(new_message) # is this redundant?
+      raise UnexpectedBuildError, new_message, e.backtrace # wrap StandardError, caller will probably want to look at #cause
     end
 
     private
