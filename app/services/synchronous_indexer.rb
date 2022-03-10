55d4ec770381985532d3da5566a05da56dcf94ae
Johnathan Martin <john.martin@stanford.edu>
2022-03-09 21:46:12 -0800

    ConstituentService#add provides cocina for reindexing
    SynchronousIndexer.reindex_remotely can be removed, as that was the only other caller

--- app/services/synchronous_indexer.rb
+++ app/services/synchronous_indexer.rb
@@ -3,15 +3,6 @@
 # Most of the indexing happens when Fedora sends an activeMQ message to dor_indexing_app
 # When we can't have latency in the indexing, we can use this class to directly call dor-indexing-app
 class SynchronousIndexer
-  def self.reindex_remotely(identifier)
-    result = connection.post("reindex/#{identifier}")
-    return if result.success?
-
-    error_message = "Response for reindexing was an error. #{result.status}: #{result.body}"
-    Honeybadger.notify(error_message, { druid: identifier })
-    Rails.logger.error(error_message)
-  end
-
   def self.reindex_remotely_from_cocina(identifier, cocina_object:, created_at:, updated_at:)
     body = { cocina_object: cocina_object, created_at: created_at, updated_at: updated_at }.to_json
     result = connection.put("reindex_from_cocina/#{identifier}", body, { 'Content-Type' => 'application/json' })
--- spec/services/synchronous_indexer_spec.rb
+++ spec/services/synchronous_indexer_spec.rb
@@ -3,35 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe SynchronousIndexer do
-  let(:pid) { 'druid:bc123dg4893' }
-
-  describe '.reindex_remotely' do
-    subject(:reindex) { described_class.reindex_remotely(pid) }
-
-    context 'with a successful request' do
-      before do
-        stub_request(:post, 'https://dor-indexing-app.example.edu/dor/reindex/druid:bc123dg4893')
-          .to_return(status: 200, body: '', headers: {})
-      end
-
-      it { is_expected.to be_nil }
-    end
-
-    context 'with an unsuccessful request' do
-      before do
-        allow(Honeybadger).to receive(:notify)
-        stub_request(:post, 'https://dor-indexing-app.example.edu/dor/reindex/druid:bc123dg4893')
-          .to_return(status: 500, body: 'broken', headers: {})
-      end
-
-      it 'logs an error' do
-        reindex
-        expect(Honeybadger).to have_received(:notify)
-          .with('Response for reindexing was an error. 500: broken', druid: 'druid:bc123dg4893')
-      end
-    end
-  end
-
   describe '.reindex_remotely_from_cocina' do
     subject(:reindex) { described_class.reindex_remotely_from_cocina(dro.external_identifier, cocina_object: dro, created_at: created_at, updated_at: created_at) }
 
