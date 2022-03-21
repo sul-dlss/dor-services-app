6810f283db4fac747fd449206dda4dd762c791d7
Justin Littman <justinlittman@stanford.edu>
2022-03-21 14:55:12 -0400

    Only deploy to worker.

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-server 'dor-services-app-prod-a.stanford.edu', user: 'dor_services', roles: %w[web app]
-server 'dor-services-app-prod-b.stanford.edu', user: 'dor_services', roles: %w[web app]
-server 'dor-services-worker-prod-a.stanford.edu', user: 'dor_services', roles: %w[app worker scheduler]
+# server 'dor-services-app-prod-a.stanford.edu', user: 'dor_services', roles: %w[web app]
+# server 'dor-services-app-prod-b.stanford.edu', user: 'dor_services', roles: %w[web app]
+# server 'dor-services-worker-prod-a.stanford.edu', user: 'dor_services', roles: %w[app worker scheduler]
 server 'dor-services-worker-prod-b.stanford.edu', user: 'dor_services', roles: %w[app worker]
 
 Capistrano::OneTimeKey.generate_one_time_key!
--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-server 'dor-services-app-qa-a.stanford.edu', user: 'dor_services', roles: %w(web app)
-server 'dor-services-app-qa-b.stanford.edu', user: 'dor_services', roles: %w(web app)
+# server 'dor-services-app-qa-a.stanford.edu', user: 'dor_services', roles: %w(web app)
+# server 'dor-services-app-qa-b.stanford.edu', user: 'dor_services', roles: %w(web app)
 server 'dor-services-worker-qa-a.stanford.edu', user: 'dor_services', roles: %w(app worker scheduler)
 
 Capistrano::OneTimeKey.generate_one_time_key!
