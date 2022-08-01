7d7131af421166eab47f72fe67781696bf6df3a2
Justin Coyne <jcoyne@justincoyne.com>
2022-08-01 14:49:48 -0500

    Start simplecov before loading any code

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start :rails do
+  add_filter '/spec/'
+  add_filter '/lib/fedora_archiver.rb'
+  add_filter '/bin/'
+  add_filter '/app/reports/'
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
