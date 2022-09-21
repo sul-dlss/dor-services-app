be023f439fbd4d15e41baf642ed6f6494ef21cf1
Naomi Dushay <ndushay@stanford.edu>
2022-09-21 11:44:30 -0700

    report subjects_with_double_dash narrows to 2 source codes

--- app/reports/subjects_with_double_dash.rb
+++ app/reports/subjects_with_double_dash.rb
@@ -12,32 +12,40 @@ class SubjectsWithDoubleDash
   # > and each of its elements, while the .HR accessor automatically unwraps
   # > arrays when using the lax mode. To avoid surprising results, we recommend
   # > using the .** accessor only in the strict mode.
-  JSON_PATH = 'strict $.subject.**'
+  JSONB_PATH = 'strict $.subject.**'
   SQL = <<~SQL.squish.freeze
     SELECT dros.external_identifier,
-           events->'value' as value,
+           subjects->'value' as value,
+           subjects->'source'->'code' as code,
            jsonb_path_query(dros.identification, '$.catalogLinks[*] ? (@.catalog == "symphony").catalogRecordId') ->> 0 as catkey,
            jsonb_path_query(dros.structural, '$.isMemberOf') ->> 0 as collection_id
            FROM "dros",
-           jsonb_path_query(dros.description, '#{JSON_PATH} ? (@.value like_regex "--")') events
+           jsonb_path_query(dros.description, '#{JSONB_PATH} ? (@.value like_regex "--")') subjects
            WHERE
-           jsonb_path_exists(description, '#{JSON_PATH}.value ? (@ like_regex "--")')
-           AND (events->>'type' is null OR events->>'type' != 'map coordinates');
-
+           jsonb_path_exists(description, '#{JSONB_PATH}.value ? (@ like_regex "--")')
+           AND (subjects->>'type' is null OR subjects->>'type' != 'map coordinates')
+           AND subjects->'source'->>'code' IN ('lcsh', 'naf')
   SQL
 
   def self.report
-    puts "item_druid,catkey,collection_druid,uri,code,value\n"
-    rows(SQL).each do |row|
-      puts row
-    end
+    puts "item_druid,catkey,collection_druid,collection_name,value\n"
+    rows(SQL).compact.each { |row| puts row }
   end
 
-  def self.rows(sql)
-    result = ActiveRecord::Base.connection.execute(sql)
+  def self.rows(sql_query)
+    sql_result_rows = ActiveRecord::Base.connection.execute(sql_query).to_a
 
-    result.to_a.map do |row|
-      [row['external_identifier'], row['catkey'], row['collection_id'], row['uri'], row['code'], "\"#{row['value']}\""].join(',')
+    sql_result_rows.map do |row|
+      collection_druid = row['collection_id']
+      collection_name = Collection.find_by(external_identifier: collection_druid)&.label
+
+      [
+        row['external_identifier'],
+        row['catkey'],
+        collection_druid,
+        "\"#{collection_name}\"",
+        "\"#{row['value']}\""
+      ].join(',')
     end
   end
 end
