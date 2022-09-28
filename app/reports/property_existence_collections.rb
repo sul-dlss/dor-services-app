12386ba8a36e85660db41887298058e680f8b08d
Naomi Dushay <ndushay@stanford.edu>
2022-09-28 16:12:21 -0700

    tweak generic property reports to make them easier to adapt

--- app/reports/property_existence_collections.rb
+++ app/reports/property_existence_collections.rb
@@ -1,30 +1,31 @@
 # frozen_string_literal: true
 
 # Report collection objects with occurences of a property.
+#  it is expected the property is an array, and we are selecting non-empty arrays with '? (@.size() > 0))'
+#  To check for property of type string, or to include empty arrays, remove '? (@.size() > 0))' from JSON_PATH
 
 # Invoke via:
 # bin/rails r -e production "PropertyExistenceCollections.report"
 class PropertyExistenceCollections
+  # NOTE: JSON_PATH may need to be changed, in addition to PROPERTY
+
+  # this can be any JSON PATH desired, not just single property, e.g. 'contributor.type'
+  PROPERTY = 'groupedValue' # could also be, e.g. 'contributor.type'
+
+  # NOTE: checking the size allows checking for only non-empty arrays;  we may have empty arrays, too.
   # NOTE: Prefer strict JSON querying over lax when using the `.**` operator, per
   #       https://www.postgresql.org/docs/14/functions-json.html#STRICT-AND-LAX-MODES
-  #
-  # > The .** accessor can lead to surprising results when using the lax mode.
-  # > ... This happens because the .** accessor selects both the segments array
-  # > and each of its elements, while the .HR accessor automatically unwraps
-  # > arrays when using the lax mode. To avoid surprising results, we recommend
-  # > using the .** accessor only in the strict mode.
-  # NOTE:  checking the size allows checking for only non-empty arrays;  we may have empty arrays, too.
-  JSONB_PATH = 'strict $.**.groupedValue ? (@.size() > 0)' # when property is array
-  # JSONB_PATH = 'strict $.**.contributor.type' # when property is a string
+  JSON_PATH = "strict $.**.#{PROPERTY} ? (@.size() > 0)".freeze # when property is array
+
   SQL = <<~SQL.squish.freeze
     SELECT external_identifier as collection_druid,
            jsonb_path_query(identification, '$.catalogLinks[*] ? (@.catalog == "symphony").catalogRecordId') ->> 0 as catkey
            FROM "collections" WHERE
-           jsonb_path_exists(collections.description, '#{JSONB_PATH}')
+           jsonb_path_exists(collections.description, '#{JSON_PATH}')
   SQL
 
   def self.report
-    puts "collection_druid,catkey,collection_name\n"
+    puts "collection_druid,catkey,collection_name,collections with #{PROPERTY}\n"
     rows(SQL).compact.each { |row| puts row }
   end
 
--- app/reports/property_existence_dros.rb
+++ app/reports/property_existence_dros.rb
@@ -1,31 +1,32 @@
 # frozen_string_literal: true
 
 # Report dro objects with occurences of a property.
+#  it is expected the property is an array, and we are selecting non-empty arrays with '? (@.size() > 0))'
+#  To check for property of type string, or to include empty arrays, remove '? (@.size() > 0))' from JSON_PATH
 
 # Invoke via:
 # bin/rails r -e production "PropertyExistenceDros.report"
 class PropertyExistenceDros
+  # NOTE: JSON_PATH may need to be changed, in addition to PROPERTY
+
+  # this can be any JSON PATH desired, not just single property, e.g. 'contributor.type'
+  PROPERTY = 'groupedValue' # could also be, e.g. 'contributor.type'
+
+  # NOTE: checking the size allows checking for only non-empty arrays;  we may have empty arrays, too.
   # NOTE: Prefer strict JSON querying over lax when using the `.**` operator, per
   #       https://www.postgresql.org/docs/14/functions-json.html#STRICT-AND-LAX-MODES
-  #
-  # > The .** accessor can lead to surprising results when using the lax mode.
-  # > ... This happens because the .** accessor selects both the segments array
-  # > and each of its elements, while the .HR accessor automatically unwraps
-  # > arrays when using the lax mode. To avoid surprising results, we recommend
-  # > using the .** accessor only in the strict mode.
-  # NOTE:  checking the size allows checking for only non-empty arrays;  we may have empty arrays, too.
-  JSONB_PATH = 'strict $.**.contributor.parallelContributor ? (@.size() > 0)' # when property is array
-  # JSONB_PATH = 'strict $.**.contributor.type' # when property is a string
+  JSON_PATH = "strict $.**.#{PROPERTY} ? (@.size() > 0)".freeze # when property is array
+
   SQL = <<~SQL.squish.freeze
     SELECT external_identifier as item_druid,
            jsonb_path_query(identification, '$.catalogLinks[*] ? (@.catalog == "symphony").catalogRecordId') ->> 0 as catkey,
            jsonb_path_query(structural, '$.isMemberOf') ->> 0 as collection_id
            FROM "dros" WHERE
-           jsonb_path_exists(dros.description, '#{JSONB_PATH}')
+           jsonb_path_exists(dros.description, '#{JSON_PATH}')
   SQL
 
   def self.report
-    puts "item_druid,catkey,collection_druid,collection_name\n"
+    puts "item_druid,catkey,collection_druid,collection_name,dros with #{PROPERTY}\n"
     rows(SQL).compact.each { |row| puts row }
   end
 
