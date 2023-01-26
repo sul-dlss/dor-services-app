9ccdf3a65477531c6cb8f4d52073050e793918cc
Michael J. Giarlo <mjgiarlo@stanford.edu>
2023-01-26 09:47:40 -0800

    Add honeybadger check-ins to cron jobs
    Fixes #4293
    
    This commit uses honeybadger check-ins to ensure regularly scheduled cron jobs are running, like the approach already being used in prescat and H2.

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,14 +21,25 @@
 
 # Learn more: http://github.com/javan/whenever
 
+# Load rubyconfig gem so that we have access to env-specific settings
+require 'config'
+
+Config.load_and_set_settings(Config.setting_files('config', 'production'))
+
+# These define jobs that checkin with Honeybadger.
+# If changing the schedule of one of these jobs, also update at https://app.honeybadger.io/projects/50568/check_ins
+job_type :rake, "cd :path && :environment_variable=:environment bundle exec rake :task --silent :output && curl 'https://api.honeybadger.io/v1/check_in/:check_in"
+
 # Suppress warnings to avoid unnecessary cron emails.
 env 'RUBYOPT', '-W0'
 
 every :day, at: '2:16am' do
+  set :check_in, Settings.honeybadger_checkins.embargo_release
   rake 'dsa:embargo_release'
 end
 
 # Run this an on off minute to avoid Google Books every 15 minutes
 every :day, at: '8:35pm' do
+  set :check_in, Settings.honeybadger_checkins.missing_druids
   rake 'missing_druids:unindexed_objects'
 end
