0c83e7bfa3ca48ea8c28adf72a4a1cb0aad9e972
Laura Wrubel <lwrubel@stanford.edu>
2023-03-17 08:16:16 -0400

    Rebased

--- spec/services/catalog/folio_writer_spec.rb
+++ spec/services/catalog/folio_writer_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Catalog::FolioWriter do
   subject(:folio_writer) { described_class.new(cocina_object:, marc_856_data:) }
 
   let(:druid) { 'druid:bc123dg9393' }
+  let(:bare_druid) { druid.delete_prefix('druid:') }
+
   let(:marc_856_data) do
     {
       indicators: '41',
@@ -14,6 +16,8 @@ RSpec.describe Catalog::FolioWriter do
         { code: 'x', value: 'SDR-PURL' },
         { code: 'x', value: 'item' },
         { code: 'x', value: 'barcode:36105216275185' },
+        { code: 'x', value: 'file:cc111cc1111%2Fwt183gy6220_00_0001.jp2' },
+        { code: 'x', value: 'collection:cc111cc1111:8832162:Collection label & A Special character' },
         { code: 'x', value: 'rights:world' }
       ]
     }
