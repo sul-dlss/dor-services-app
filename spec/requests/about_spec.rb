b49549df49335ae1194974b9757130be56797ea5
Justin Littman <justinlittman@stanford.edu>
2023-08-18 15:45:03 -0400

    Test circleci orb change

--- spec/requests/about_spec.rb
+++ spec/requests/about_spec.rb
@@ -5,6 +5,6 @@ require 'rails_helper'
 RSpec.describe '/v1/about' do
   it 'handles simple ping requests to /about' do
     get '/v1/about'
-    expect(response.body).to match(/Version: \d\..*$/)
+    expect(response.body).not_to match(/Version: \d\..*$/)
   end
 end
