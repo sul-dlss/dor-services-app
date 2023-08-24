9116fcbdd317b9a03e7105e3dd7c53b974af8097
Peter Mangiafico <peter@mangiafico.org>
2023-08-24 10:46:05 -0700

    replace symphony check with folio check, if feature flag enabled

--- spec/requests/status_spec.rb
+++ spec/requests/status_spec.rb
@@ -10,15 +10,14 @@ RSpec.describe 'Status (okcomputer)' do
     end
   end
 
-  describe 'for Symphony connection' do
-    let(:symphony_client) { instance_double(Faraday::Connection, get: true) }
-
+  describe 'for Folio connection' do
     before do
-      allow(Catalog::SymphonyReader).to receive(:client).and_return(symphony_client)
+      allow(Catalog::FolioReader).to receive(:to_marc).and_return('some marc stuff')
+      allow(Settings.enabled_features).to receive(:read_folio).and_return(true)
     end
 
     it 'is successful' do
-      get '/status/external-symphony'
+      get '/status/external-folio'
       expect(response).to have_http_status(:ok)
     end
   end
