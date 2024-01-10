0e85a972950f59a339b10b2e9555afa4e6d2fe06
Justin Littman <justinlittman@stanford.edu>
2024-01-10 11:29:15 -0500

    Stop using daemon

--- app/indexing_services/workflow_client_factory.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# This initializes the workflow client with values from settings
-class WorkflowClientFactory
-  def self.build
-    Dor::Workflow::Client.new(url: Settings.workflow_url, logger: Rails.logger, timeout: Settings.workflow.timeout)
-  end
-end
