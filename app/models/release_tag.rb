eab8ab8f355bc38fc9947a78c9c359c8f5d386e2
Justin Littman <justinlittman@stanford.edu>
2024-03-26 07:45:32 -0400

    Adds default order for release tags.
    closes #4736

--- app/models/release_tag.rb
+++ app/models/release_tag.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
+# Model for release tags, which indicate that an item or collection should be released.
 class ReleaseTag < ApplicationRecord
+  default_scope { order(created_at: :asc) }
 end
--- spec/models/release_tag_spec.rb
+++ spec/models/release_tag_spec.rb
@@ -3,5 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe ReleaseTag do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'default_scope' do
+    let!(:release_tag1) { create(:release_tag, created_at: Time.zone.now) }
+    let!(:release_tag2) { create(:release_tag, created_at: 1.day.ago) }
+
+    it 'orders by created_at' do
+      expect(described_class.all).to eq([release_tag2, release_tag1])
+    end
+  end
 end
