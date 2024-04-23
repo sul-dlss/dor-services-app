426d01f85d3fc4617388231aa5031abef09202a6
Aaron Collier <aaron.collier@stanford.edu>
2024-04-23 09:47:54 -0700

    Switch ContributorIdentifiers.report to use RepositoryObject

--- app/reports/contributor_identifiers.rb
+++ app/reports/contributor_identifiers.rb
@@ -7,12 +7,13 @@ class ContributorIdentifiers
   def self.report
     puts "item_druid,collection_druid,catalogRecordId,identifier\n"
 
-    Dro.where("jsonb_path_exists(description, '$.contributor.identifier.value')").find_each do |dro|
+    RepositoryObject.dros.where(head_version: "jsonb_path_exists(description, '$.contributor.identifier.value')").find_each do |dro|
       druid = dro.external_identifier
-      collection = dro.structural['isMemberOf'].first
-      catalog_record_id = dro.identification['catalogLinks'].first&.fetch('catalogRecordId')
+      head_version = dro.head_version
+      collection = head_version.structural['isMemberOf'].first
+      catalog_record_id = head_version.identification['catalogLinks'].first&.fetch('catalogRecordId')
 
-      dro.description.fetch('contributor').map do |contributor|
+      head_version.description.fetch('contributor').map do |contributor|
         contributor['identifier'].map do |identifier|
           puts "#{druid},#{collection},#{catalog_record_id},#{identifier['value']}\n"
         end
