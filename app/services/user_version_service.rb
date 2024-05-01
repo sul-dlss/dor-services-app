6d170c0815fe1ae4f4e06614c2feee9ceef612e1
Aaron Collier <aaron.collier@stanford.edu>
2024-05-01 15:06:29 -0700

    Add UserVersionService spec

--- app/services/user_version_service.rb
+++ app/services/user_version_service.rb
@@ -10,16 +10,25 @@ class UserVersionService
     new(druid:, version:).create(event_factory:)
   end
 
+  # @param [UserVersion] user_version to withdraw
+  # @param [Class] event_factory (EventFactory) the factory for creating events
   def self.withdraw(user_version:, event_factory: EventFactory)
     user_version.withdrawn = true
     user_version.save!
+    druid = user_version.repository_object_version.repository_object.external_identifier
     event_factory.create(druid:, event_type: 'user_version_withdrawn', data: { version: user_version.version.to_s })
   end
 
+  # @param [String] druid of the item
+  # @param [Integer] version of the item
+  # @param [UserVersion] user_version to move
+  # @param [Class] event_factory (EventFactory) the factory for creating events
   def self.move(druid:, version:, user_version:, event_factory: EventFactory)
     new(druid:, version:).move(user_version, event_factory)
   end
 
+  # @param [String] druid of the item
+  # @param [UserVersion] user_version to check
   def self.exist?(druid:, user_version:)
     new(druid:, version: nil).exist?(user_version:)
   end
@@ -30,6 +39,8 @@ class UserVersionService
     @druid = druid
     @repository_object = RepositoryObject.find_by!(external_identifier: druid)
     @repository_object_version = version.nil? ? @repository_object.head_version : @repository_object.versions.find_by!(version:)
+  rescue ActiveRecord::RecordNotFound
+    raise(UserVersioningError, "RepositoryObject not found for #{druid}")
   end
 
   # @param [Class] event_factory (EventFactory) the factory for creating events
@@ -45,16 +56,21 @@ class UserVersionService
     version
   end
 
+  # @param [UserVersion] user_version to move
+  # @param [Class] event_factory (EventFactory) the factory for creating events
+  # @raise [VersionService::VersioningError] if the object hasn't been accessioned
   def move(user_version, event_factory)
-    raise(UserVersioningError, 'RepositoryObject not closed') unless repository_object.closed?
+    raise(UserVersioningError, 'RepositoryObject not closed') unless repository_object_version.closed?
 
     user_version.repository_object_version = repository_object_version
     user_version.save!
     event_factory.create(druid:, event_type: 'user_version_moved', data: { version: user_version.version.to_s })
   end
 
+  # @param [UserVersion] user_version to check
+  # @return [Boolean] true if the user version exists
   def exist?(user_version:)
-    RepositoryObjectVersion.where(repository_object:, user_version:).exists?
+    repository_object.versions.map { |version| version.user_versions.include? user_version }.any?
   end
 
   attr_reader :druid, :repository_object, :repository_object_version
--- spec/services/user_version_service_spec.rb
+++ spec/services/user_version_service_spec.rb
@@ -4,33 +4,94 @@ require 'rails_helper'
 
 RSpec.describe UserVersionService do
   let(:druid) { 'druid:xz456jk0987' }
-  let(:cocina_object) { create(:ar_dro, external_identifier: druid).to_cocina_with_metadata }
   let(:event_factory) { class_double(EventFactory, create: true) }
-  let(:repository_object_version) { build(:repository_object_version, repository_object:, **attrs) }
-  let(:repository_object) { build(:repository_object, object_type:, external_identifier: druid) }
-  let(:version) { 1 }
+  let(:repository_object_version) { repository_object.versions.first }
+  let(:repository_object) { create(:repository_object, object_type:, external_identifier: druid) }
   let(:object_type) { 'dro' }
-  let(:attrs) do
-    {
-      version: 1,
-      version_description: 'My new version',
-      closed_at: Time.current
-    }
-  end
 
   describe '.create' do
-    subject(:create) { described_class.create(druid:, version: 1, event_factory:) }
+    subject(:user_version_service_create) { described_class.create(druid:, version: 1, event_factory:) }
 
-    context 'when the repository object is closed' do
+    context 'when the repository object version is closed' do
       before do
-        allow(RepositoryObject).to receive(:find_by!).and_return(repository_object)
-        allow(repository_object.versions).to receive(:find_by!).and_return(repository_object_version)
+        repository_object_version.update(closed_at: Time.current)
       end
 
       it 'creates a user version' do
-        create
+        user_version_service_create
         expect(repository_object_version.user_versions.count).to eq 1
       end
+    end
+
+    context 'when the repository object version is open' do
+      before do
+        repository_object_version.update(closed_at: nil)
+      end
+
+      it 'does not create a user version' do
+        expect { user_version_service_create }.to raise_error UserVersionService::UserVersioningError
+        expect(repository_object_version.user_versions.count).to eq 0
+      end
+    end
+
+    context 'when the repository object is not found' do
+      it 'does not create a user version' do
+        expect { user_version_service_create }.to raise_error UserVersionService::UserVersioningError
+        expect(repository_object_version.user_versions.count).to eq 0
+      end
+    end
+  end
+
+  describe '.withdraw' do
+    subject(:user_version_service_withdraw) { described_class.withdraw(user_version:, event_factory:) }
+
+    let(:user_version) { UserVersion.create!(version: 1, repository_object_version:) }
+
+    before do
+      repository_object_version.update(closed_at: Time.current)
+    end
+
+    it 'withdraws the user version' do
+      expect(user_version.reload.withdrawn).to be false
+      user_version_service_withdraw
+      expect(user_version.reload.withdrawn).to be true
+    end
+  end
+
+  describe '.move' do
+    subject(:user_version_service_move) { described_class.move(druid:, version: 2, user_version:, event_factory:) }
+
+    let(:repository_object_version2) { RepositoryObjectVersion.create!(version: 2, version_description: 'My second version', repository_object:) }
+    let(:user_version) { UserVersion.create!(version: 1, repository_object_version:) }
+
+    before do
+      repository_object_version.update(closed_at: Time.current)
+      repository_object_version2.update(closed_at: Time.current)
+    end
+
+    it 'moves the user version' do
+      expect(user_version.repository_object_version).to eq repository_object_version
+      user_version_service_move
+      expect(user_version.repository_object_version).to eq repository_object_version2
+    end
+  end
+
+  describe '.exist?' do
+    subject(:user_version_service_exist?) { described_class.exist?(druid:, user_version:) }
+
+    let(:user_version) { UserVersion.create!(version: 1, repository_object_version:) }
+
+    before do
+      repository_object_version.update(closed_at: Time.current)
+    end
+
+    it 'returns true if the user version exists' do
+      expect(user_version_service_exist?).to be true
+    end
+
+    it 'returns false if the user version does not exist' do
+      user_version.destroy
+      expect(user_version_service_exist?).to be false
     end
   end
 end
