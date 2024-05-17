9b96f344cc2fed9a347ebdaf27e61ab337077489
Justin Littman <justinlittman@stanford.edu>
2024-05-17 13:36:41 -0400

    Changes MembersController to use RepositoryObject.
    closes #4995

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,6 +4,6 @@
 class MembersController < ApplicationController
   # Return the members of this collection
   def index
-    @members = Dro.members_of_collection(params[:object_id]).select(:external_identifier, :version)
+    @members = RepositoryObject.currently_members_of_collection(params[:object_id]).select(:external_identifier, :version)
   end
 end
