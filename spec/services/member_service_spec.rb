00512f78caba5bc080284fd6511eeec10409a94d
Justin Littman <justinlittman@stanford.edu>
2024-05-17 14:06:28 -0400

    Changes MemberService to use RepositoryObject.
    closes #4998

--- spec/services/member_service_spec.rb
+++ spec/services/member_service_spec.rb
@@ -9,21 +9,29 @@ RSpec.describe MemberService do
   let(:exclude_opened) { false }
   let(:only_published) { false }
 
-  let!(:dro1) { create(:ar_dro, isMemberOf: [collection_druid]) }
-  let!(:dro2) { create(:ar_dro, isMemberOf: [collection_druid]) }
+  let(:repository_object1) { create(:repository_object) }
+  let(:repository_object2) { create(:repository_object) }
 
   let(:workflow_client) { instance_double(Dor::Workflow::Client) }
 
   before do
+    # Opened
+    repository_object_version1 = create(:repository_object_version, version: 2, is_member_of: [collection_druid], repository_object: repository_object1)
+    repository_object1.update!(head_version: repository_object_version1, opened_version: repository_object_version1)
+
+    # Closed
+    repository_object_version2 = create(:repository_object_version, version: 2, is_member_of: [collection_druid], repository_object: repository_object2, closed_at: Time.current)
+    repository_object2.update!(head_version: repository_object_version2, last_closed_version: repository_object_version2, opened_version: nil)
+
     # Not a member
-    create(:ar_dro)
+    create(:repository_object)
     allow(WorkflowClientFactory).to receive(:build).and_return(workflow_client)
   end
 
   describe '.for' do
     context 'when collection has members' do
       it 'returns members' do
-        expect(members).to contain_exactly(dro1.external_identifier, dro2.external_identifier)
+        expect(members).to contain_exactly(repository_object1.external_identifier, repository_object2.external_identifier)
       end
     end
 
@@ -36,21 +44,8 @@ RSpec.describe MemberService do
     context 'when excluding open members' do
       let(:exclude_opened) { true }
 
-      before do
-        allow(workflow_client).to receive(:active_lifecycle).with(
-          druid: dro1.external_identifier,
-          milestone_name: 'opened',
-          version: 1
-        ).and_return(true)
-        allow(workflow_client).to receive(:active_lifecycle).with(
-          druid: dro2.external_identifier,
-          milestone_name: 'opened',
-          version: 1
-        ).and_return(false)
-      end
-
       it 'returns members' do
-        expect(members).to eq [dro2.external_identifier]
+        expect(members).to eq [repository_object2.external_identifier]
       end
     end
 
@@ -58,12 +53,12 @@ RSpec.describe MemberService do
       let(:only_published) { true }
 
       before do
-        allow(workflow_client).to receive(:lifecycle).with(druid: dro1.external_identifier, milestone_name: 'published', version: 1).and_return(true)
-        allow(workflow_client).to receive(:lifecycle).with(druid: dro2.external_identifier, milestone_name: 'published', version: 1).and_return(false)
+        allow(workflow_client).to receive(:lifecycle).with(druid: repository_object1.external_identifier, milestone_name: 'published', version: 2).and_return(true)
+        allow(workflow_client).to receive(:lifecycle).with(druid: repository_object2.external_identifier, milestone_name: 'published', version: 2).and_return(false)
       end
 
       it 'returns members' do
-        expect(members).to eq [dro1.external_identifier]
+        expect(members).to eq [repository_object1.external_identifier]
       end
     end
   end
