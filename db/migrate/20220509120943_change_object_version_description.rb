a22551a0ba4502ec932136e8ddb50c3003188fd2
Aaron Collier <aaron.collier@stanford.edu>
2024-05-30 19:57:47 -0700

    Update docker executor to support debian image

--- db/migrate/20220509120943_change_object_version_description.rb
+++
@@ -1,5 +0,0 @@
-class ChangeObjectVersionDescription < ActiveRecord::Migration[5.2]
-  def change
-    ObjectVersion.connection.execute("update object_versions set description = concat('Version ', version) where description = concat('Version ', tag);")
-  end
-end
