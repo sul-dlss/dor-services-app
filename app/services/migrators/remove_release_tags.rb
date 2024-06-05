3178dede1a0b2b22af625e2c4d4dc5b4e4ccfe01
Aaron Collier <aaron.collier@stanford.edu>
2024-06-05 13:42:40 -0700

    Guard against nil administrative

--- app/services/migrators/remove_release_tags.rb
+++ app/services/migrators/remove_release_tags.rb
@@ -4,11 +4,11 @@ module Migrators
   # Migrator that will be used to remove release tags.
   class RemoveReleaseTags < Base
     def migrate?
-      repository_object.versions.any? { |version| version.administrative.key?('releaseTags') }
+      repository_object.versions.any? { |version| version.administrative&.key?('releaseTags') }
     end
 
     def migrate
-      repository_object.versions.select { |version| version.administrative.key?('releaseTags') }.each do |version|
+      repository_object.versions.select { |version| version.administrative&.key?('releaseTags') }.each do |version|
         version.administrative.delete('releaseTags')
         version.save!
       end
