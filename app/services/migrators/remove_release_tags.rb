63ebd464651ccb1d877573270681e0f17819f4a6
Aaron Collier <aaron.collier@stanford.edu>
2024-06-10 11:46:25 -0700

    Guard against not accessioned

--- app/services/migrators/remove_release_tags.rb
+++ app/services/migrators/remove_release_tags.rb
@@ -9,6 +9,8 @@ module Migrators
 
     def migrate
       repository_object.versions.select { |version| version.administrative&.key?('releaseTags') }.each do |version|
+        next unless WorkflowStateService.new(druid: repository_object.external_identifier, version: version.version).accessioned?
+
         puts "#{repository_object.external_identifier} - Removing release tags from version #{version.version}" # rubocop:disable Rails/Output
         version.administrative.delete('releaseTags')
       end
