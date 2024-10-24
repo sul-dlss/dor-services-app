d2734f8a0872253d8792f772f6c7093d7e6fbf1d
Justin Littman <justinlittman@stanford.edu>
2024-10-24 10:29:54 -0400

    testing

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: Settings.redis_url }
+#   # For Sidekiq Pro
+#   config.super_fetch!
+# end
+
 Sidekiq.configure_server do |config|
+  Rails.logger.info("Sidekiq labels: #{config[:labels].to_a}")
   config.redis = { url: Settings.redis_url }
 end
 
