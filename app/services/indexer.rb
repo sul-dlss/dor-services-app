34012d4223e0da0f94fcf41aeb2e8b7180f15118
Justin Littman <justinlittman@stanford.edu>
2025-02-25 08:13:54 -0500

    Add additional logging to help troubleshoot indexing. (#5271)
    refs #5231

--- app/services/indexer.rb
+++ app/services/indexer.rb
@@ -6,11 +6,15 @@ class Indexer
   def self.reindex(cocina_object:, trace_id: nil)
     return unless Settings.solr.enabled
 
+    trace_id ||= trace_id_for(druid: cocina_object.externalIdentifier)
     solr_doc = Indexing::Builders::DocumentBuilder.for(
       model: cocina_object,
-      trace_id: trace_id || trace_id_for(druid: cocina_object.externalIdentifier)
+      trace_id:
     ).to_solr
     solr.add(solr_doc)
+    # This logging is to assist with https://github.com/sul-dlss/dor-services-app/issues/5231
+    # It is capturing that a Solr document is being committed and the order relative to other commits.
+    Rails.logger.info("[Indexing] Committing #{cocina_object.externalIdentifier} with trace_id=#{trace_id}")
     solr.commit
   end
 
@@ -39,7 +43,7 @@ class Indexer
   def self.trace_id_for(druid:)
     source = Kernel.caller_locations(2, 1).first.to_s.delete_prefix("#{Rails.root}/") # rubocop:disable Rails/FilePath
     SecureRandom.uuid.tap do |trace_id|
-      Rails.logger.info("Reindexing #{druid} from #{source} with trace_id=#{trace_id}")
+      Rails.logger.info("[Indexing] Reindexing #{druid} from #{source} with trace_id=#{trace_id}")
     end
   end
 end
