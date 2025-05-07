6ac28069bd246e21de686cb0187c3563e198c1f1
Laura Wrubel <lwrubel@stanford.edu>
2025-05-07 15:38:33 -0400

    Digital serials migrator (#5340)

---
+++ app/services/migrators/move_digital_serials.rb
@@ -0,0 +1,125 @@
+# frozen_string_literal: true
+
+module Migrators
+  # Used to move digital serials data from description to identification.catalogLinks
+  class MoveDigitalSerials < Base
+    # A migrator may provide a list of druids to be migrated (optional).
+    def self.druids
+      DRUIDS
+    end
+
+    # A migrator must implement a migrate? method that returns true if the SDR object should be migrated.
+    def migrate?
+      DRUIDS.include?(repository_object.external_identifier)
+    end
+
+    def migrate
+      versions = [repository_object.head_version]
+
+      if repository_object.open? && repository_object.last_closed_version.present?
+        versions << repository_object.last_closed_version
+      end
+
+      versions.each do |version|
+        migrate_version(version)
+      end
+    end
+
+    def migrate_version(version) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+      return if version.identification['catalogLinks'].blank?
+
+      catalog_link = version.identification['catalogLinks'].find do |link|
+        link['catalog'] == 'folio'
+      end
+      return unless catalog_link
+
+      # create the partLabel if not already populated and delete the title parts
+      if catalog_link['partLabel'].blank?
+        title = version.description['title'].first
+        part_label = part_label_from_title(title)
+        if part_label.present?
+          catalog_link['partLabel'] = part_label
+          delete_title_parts(title)
+        end
+      end
+
+      # create the sortKey if not already populated and delete the note
+      if catalog_link['sortKey'].blank? && version.description['note'].present?
+        sort_note = version.description['note'].find do |note|
+          note['type'] == 'date/sequential designation'
+        end
+        if sort_note.present?
+          catalog_link['sortKey'] = sort_note['value']
+          version.description['note'].delete(sort_note)
+        end
+      end
+
+      # only update refresh if partLabel or sortKey exists
+      return unless catalog_link['partLabel'] || catalog_link['sortKey']
+
+      catalog_link['refresh'] = true
+    end
+
+    def part_types
+      ['part name', 'part number']
+    end
+
+    def part_label_from_title(title) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+      # Need to check both structuredValue on title and in parallelValue
+      structured_values = []
+      structured_values << title['structuredValue'] if title['structuredValue'].present?
+      # should only check the first value in parallelValue, and for a structuredValue
+      if title['parallelValue'].present? && title['parallelValue'].first['structuredValue'].present?
+        structured_values << title['parallelValue'].first['structuredValue']
+      end
+
+      part_parts = []
+      structured_values.each do |structured_value|
+        structured_value.each do |part|
+          part_parts << part if part_types.include?(part['type'])
+        end
+      end
+
+      return if part_parts.blank?
+
+      part_parts.each_with_index.map do |part, index|
+        # if the part is not the first one, check the previous part type to determine the delimiter
+        delimiter = if index.positive? && part_parts[index - 1]['type'] == 'part number'
+                      ', '
+                    elsif index.positive? && part_parts[index - 1]['type'] == 'part name'
+                      '. '
+                    else
+                      ''
+                    end
+        "#{delimiter}#{part['value']}"
+      end.join
+    end
+
+    def delete_title_parts(title) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity
+      # delete structuredValue parts from title
+      if title['structuredValue'].present?
+        parts_to_delete = title['structuredValue'].select do |part|
+          part_types.include?(part['type'])
+        end
+        parts_to_delete.each do |part|
+          title['structuredValue'].delete(part)
+        end
+      end
+
+      return unless title['parallelValue'].present? && title['parallelValue'].first['structuredValue'].present?
+
+      # delete title.parallelValue.first.structuredValue parts from title
+      parallel_parts_to_delete = title['parallelValue'].first['structuredValue'].select do |part|
+        part_types.include?(part['type'])
+      end
+      parallel_parts_to_delete.each do |part|
+        title['parallelValue'].first['structuredValue'].delete(part)
+      end
+    end
+
+    # QA druid for testing
+    DRUIDS = [
+      'druid:bc177tq6734'
+    ].freeze
+  end
+end
---
+++ spec/services/migrators/move_digital_serials_spec.rb
@@ -0,0 +1,281 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Migrators::MoveDigitalSerials do
+  subject(:migrator) { described_class.new(repository_object) }
+
+  let(:repository_object_version) { build(:repository_object_version, description:, identification:) }
+  let(:repository_object) do
+    create(:repository_object, :with_repository_object_version, repository_object_version:,
+                                                                external_identifier: 'druid:bc177tq6734')
+  end
+  let(:identification) do
+    { catalogLinks: [{ catalog: 'folio', catalogRecordId: 'a1234', refresh: false }],
+      sourceId: 'sul:sourceId' }
+  end
+  let(:description) do
+    {
+      title: [
+        {
+          structuredValue: [
+            { type: 'main title', value: 'Main Title' },
+            { type: 'part number', value: 'Volume 1' },
+            { type: 'part name', value: 'Spring' }
+          ]
+        }
+      ],
+      note: [
+        { type: 'date/sequential designation', value: '2023.01' },
+        { type: 'abstract', value: 'An abstract' }
+      ]
+    }
+  end
+
+  describe '#migrate?' do
+    subject { migrator.migrate? }
+
+    it { is_expected.to be true }
+  end
+
+  describe 'migrate' do
+    it 'populates the catalogLink partLabel and sortKey and deletes from description' do
+      migrator.migrate
+      expect(repository_object.head_version.identification).to eq(
+        { 'catalogLinks' => [{ 'catalog' => 'folio',
+                               'catalogRecordId' => 'a1234',
+                               'refresh' => true,
+                               'partLabel' => 'Volume 1, Spring',
+                               'sortKey' => '2023.01' }],
+          'sourceId' => 'sul:sourceId' }
+      )
+      expect(repository_object.head_version.description).to eq(
+        { 'title' => [{ 'structuredValue' => [{ 'type' => 'main title',
+                                                'value' => 'Main Title' }] }],
+          'note' => [{
+            'type' => 'abstract', 'value' => 'An abstract'
+          }] }
+      )
+    end
+
+    context 'when a parallelValue is present' do
+      let(:description) do
+        {
+          title: [
+            { parallelValue: [
+              {
+                structuredValue: [
+                  { type: 'main title', value: 'Main Title' },
+                  { type: 'part number', value: 'Volume 1' },
+                  { type: 'part name', value: 'Summer' }
+                ]
+              }
+            ] }
+          ]
+        }
+      end
+
+      it 'populates the catalogLink partLabel and sortKey and deletes from description' do
+        migrator.migrate
+        expect(repository_object.head_version.identification).to eq(
+          { 'catalogLinks' => [{ 'catalog' => 'folio',
+                                 'catalogRecordId' => 'a1234',
+                                 'refresh' => true,
+                                 'partLabel' => 'Volume 1, Summer' }],
+            'sourceId' => 'sul:sourceId' }
+        )
+        expect(repository_object.head_version.description).to eq(
+          { 'title' => [{ 'parallelValue' => [{ 'structuredValue' => [{ 'type' => 'main title',
+                                                                        'value' => 'Main Title' }] }] }] }
+        )
+      end
+    end
+
+    context 'when there are three title parts' do
+      let(:description) do
+        {
+          title: [
+            {
+              structuredValue: [
+                { type: 'main title', value: 'Main Title' },
+                { type: 'part number', value: 'Volume 1' },
+                { type: 'part name', value: 'Spring' },
+                { type: 'part number', value: '2023 May' }
+              ]
+            }
+          ]
+        }
+      end
+
+      it 'populates the catalogLink partLabel with correct punctuation' do
+        migrator.migrate
+        expect(repository_object.head_version.identification).to eq(
+          { 'catalogLinks' => [{ 'catalog' => 'folio',
+                                 'catalogRecordId' => 'a1234',
+                                 'refresh' => true,
+                                 'partLabel' => 'Volume 1, Spring. 2023 May' }],
+            'sourceId' => 'sul:sourceId' }
+        )
+        expect(repository_object.head_version.description).to eq(
+          { 'title' => [{ 'structuredValue' => [{ 'type' => 'main title',
+                                                  'value' => 'Main Title' }] }] }
+        )
+      end
+    end
+
+    context 'when no parts are present' do
+      let(:description) do
+        {
+          title: [
+            {
+              structuredValue: [
+                { type: 'main title', value: 'Main Title' }
+              ]
+            }
+          ]
+        }
+      end
+
+      it 'does not change the catalogLink or title' do
+        migrator.migrate
+        expect(repository_object.head_version.identification).to eq({ 'catalogLinks' => [{ 'catalog' => 'folio',
+                                                                                           'catalogRecordId' => 'a1234',
+                                                                                           'refresh' => false }],
+                                                                      'sourceId' => 'sul:sourceId' })
+        expect(repository_object.head_version.description).to eq(
+          { 'title' => [{ 'structuredValue' => [{ 'type' => 'main title',
+                                                  'value' => 'Main Title' }] }] }
+        )
+      end
+    end
+
+    context 'when no folio catalogLink is present' do
+      let(:identification) do
+        {  catalogLinks: [{ catalog: 'symphony', catalogRecordId: '1234', refresh: false }],
+           sourceId: 'sul:sourceId' }
+      end
+
+      it 'does not populate the catalogLink partLabel and sortKey' do
+        migrator.migrate
+
+        expect(repository_object.head_version.identification).to eq({ 'catalogLinks' => [{ 'catalog' => 'symphony',
+                                                                                           'catalogRecordId' => '1234',
+                                                                                           'refresh' => false }],
+                                                                      'sourceId' => 'sul:sourceId' })
+      end
+
+      it 'does not delete the title parts' do
+        migrator.migrate
+
+        expect(repository_object.head_version.description).to eq(
+          { 'title' => [{ 'structuredValue' => [{ 'type' => 'main title',
+                                                  'value' => 'Main Title' },
+                                                { 'type' => 'part number',
+                                                  'value' => 'Volume 1' },
+                                                { 'type' => 'part name',
+                                                  'value' => 'Spring' }] }],
+            'note' => [{ 'type' => 'date/sequential designation', 'value' => '2023.01' },
+                       { 'type' => 'abstract',
+                         'value' => 'An abstract' }] }
+        )
+      end
+    end
+
+    context 'when no catalogLink is present' do
+      let(:identification) do
+        { sourceId: 'sul:sourceId' }
+      end
+
+      it 'does not populate the catalogLink partLabel and sortKey' do
+        migrator.migrate
+
+        expect(repository_object.head_version.identification).to eq({ 'sourceId' => 'sul:sourceId' })
+      end
+    end
+
+    context 'when head version is not last closed version' do
+      let(:repository_object) { create(:repository_object, :closed) }
+      let!(:repository_object_version2) do
+        create(:repository_object_version, repository_object:, closed_at: Time.zone.now, description: description1,
+                                           identification:, version: 2)
+      end
+      let!(:repository_object_version3) do
+        create(:repository_object_version, repository_object:, description:, identification:, version: 3)
+      end
+      let(:description1) do
+        {
+          title: [
+            {
+              structuredValue: [
+                { type: 'main title', value: 'Main Title' },
+                { type: 'part number', value: 'Volume 1' },
+                { type: 'part name', value: 'Winter' }
+              ]
+            }
+          ],
+          note: [
+            { type: 'date/sequential designation', value: '2020.01' },
+            { type: 'abstract', value: 'An abstract' }
+          ]
+        }
+      end
+
+      before do
+        repository_object.update(head_version: repository_object_version3, opened_version: repository_object_version3,
+                                 last_closed_version: repository_object_version2)
+      end
+
+      it 'migrates both the last closed version and head version' do
+        migrator.migrate
+        expect(repository_object.head_version.identification).to eq(
+          { 'catalogLinks' => [{ 'catalog' => 'folio',
+                                 'catalogRecordId' => 'a1234',
+                                 'refresh' => true,
+                                 'partLabel' => 'Volume 1, Spring',
+                                 'sortKey' => '2023.01' }],
+            'sourceId' => 'sul:sourceId' }
+        )
+        expect(repository_object.head_version.description).to eq(
+          { 'title' => [{ 'structuredValue' => [{ 'type' => 'main title',
+                                                  'value' => 'Main Title' }] }],
+            'note' => [{
+              'type' => 'abstract', 'value' => 'An abstract'
+            }] }
+        )
+        expect(repository_object.last_closed_version.identification).to eq(
+          { 'catalogLinks' => [{ 'catalog' => 'folio',
+                                 'catalogRecordId' => 'a1234',
+                                 'refresh' => true,
+                                 'partLabel' => 'Volume 1, Winter',
+                                 'sortKey' => '2020.01' }],
+            'sourceId' => 'sul:sourceId' }
+        )
+        expect(repository_object.last_closed_version.description).to eq(
+          { 'title' => [{ 'structuredValue' => [{ 'type' => 'main title',
+                                                  'value' => 'Main Title' }] }],
+            'note' => [{
+              'type' => 'abstract', 'value' => 'An abstract'
+            }] }
+        )
+      end
+    end
+  end
+
+  describe '#publish?' do
+    it 'returns false as migrated SDR objects should not be published' do
+      expect(migrator.publish?).to be false
+    end
+  end
+
+  describe '#version?' do
+    it 'returns false as migrated SDR objects should not be versioned' do
+      expect(migrator.version?).to be false
+    end
+  end
+
+  describe '#version_description' do
+    it 'raises an error as version? is never true' do
+      expect { migrator.version_description }.to raise_error(NotImplementedError)
+    end
+  end
+end
