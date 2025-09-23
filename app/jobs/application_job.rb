08afb41894045508f1067679943e640b79abe5eb
Justin Littman <justinlittman@stanford.edu>
2025-09-23 08:54:30 -0400

    Change sidekiq config for active jobs.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,9 +2,6 @@
 
 # Common superclass for all jobs
 class ApplicationJob < ActiveJob::Base
-  # Automatically retry jobs that encountered a deadlock
-  # retry_on ActiveRecord::Deadlocked
-
-  # Most jobs are safe to ignore if the underlying records are no longer available
-  # discard_on ActiveJob::DeserializationError
+  # See config/initializers/sidekiq.rb for explanation.
+  sidekiq_options pool: REDIS
 end
