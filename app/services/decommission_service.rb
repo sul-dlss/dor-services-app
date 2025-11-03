449d1b5cbbb3189121a581a767e3c21c1b11689e
Aaron Collier <aaron.collier@stanford.edu>
2025-11-03 11:50:50 -0800

    Update test for clarity and add rake task

--- app/services/decommission_service.rb
+++ app/services/decommission_service.rb
@@ -2,39 +2,37 @@
 
 # Handles the decommissioning of objects
 class DecommissionService
-  class DecommissionFailed < StandardError; end
-
-  DECOMMISSION_ACCESS = { view: 'dark', download: 'none' }.freeze
-  DECOMMISSION_APO = { hasAdminPolicy: Settings.graveyard_admin_policy.druid }.freeze
-
-  attr_reader :cocina_object, :reason, :sunetid
-
-  def self.decommission(cocina_object:, reason:, sunetid:)
-    new(cocina_object:, reason:, sunetid:).decommission
+  # Decommissions an object
+  # @param [String] druid
+  # @param [String] description description for decommissioning (usually a TICKET reference)
+  # @param [String] sunetid user performing the decommission
+  # @return [Cocina::Models::DROWithMetadata] updated cocina object
+  def self.decommission(...)
+    new(...).decommission
   end
 
-  def initialize(cocina_object:, reason:, sunetid:)
-    @cocina_object = cocina_object
-    @reason = reason
+  def initialize(druid:, description:, sunetid:)
+    @druid = druid
+    @description = description
     @sunetid = sunetid
   end
 
+  attr_reader :druid, :description, :sunetid
+
   def decommission
-    VersionService.open(cocina_object:,
-                        description: "Decommissioned: #{reason}",
-                        assume_accessioned: true,
-                        opening_user_name: sunetid)
+    cocina_object = VersionService.open(cocina_object: CocinaObjectStore.find(druid),
+                                        description: "Decommissioned: #{description}",
+                                        assume_accessioned: true,
+                                        opening_user_name: sunetid)
 
-    updated_cocina_object = UpdateObjectService.update(cocina_object: decommissioned_cocina_object,
-                                                       description: "Decommissioned: #{reason}",
-                                                       who: sunetid)
+    updated_cocina_object = decommission_cocina_object(cocina_object:)
 
-    set_decommissioned_tags
-    release_workflow
+    set_decommissioned_tags(cocina_object: updated_cocina_object)
+    start_release_workflow(cocina_object: updated_cocina_object)
 
     VersionService.close(druid:,
                          version: updated_cocina_object.version,
-                         description: "Decommissioned: #{reason}",
+                         description: "Decommissioned: #{description}",
                          user_name: sunetid)
 
     updated_cocina_object
@@ -42,11 +40,22 @@ class DecommissionService
 
   private
 
-  def druid
-    cocina_object.externalIdentifier
+  def decommission_cocina_object(cocina_object:)
+    structural = Cocina::Models::DROStructural.new(cocina_object.structural&.to_h&.except(:contains))
+
+    UpdateObjectService.update(cocina_object: cocina_object.new(access: {
+                                                                  view: 'dark',
+                                                                  download: 'none'
+                                                                },
+                                                                administrative: {
+                                                                  hasAdminPolicy: Settings.graveyard_admin_policy.druid
+                                                                },
+                                                                structural:),
+                               description: "Decommissioned: #{description}",
+                               who: sunetid)
   end
 
-  def set_decommissioned_tags
+  def set_decommissioned_tags(cocina_object:)
     ReleaseTagService.latest_release_tags(druid:).each do |release_tag|
       next unless release_tag.release
 
@@ -56,27 +65,19 @@ class DecommissionService
     end
 
     AdministrativeTags.create(identifier: cocina_object.externalIdentifier,
-                              tags: ["Decommissioned : #{reason}"])
+                              tags: ["Decommissioned : #{description}"])
   end
 
-  def release_workflow
+  def start_release_workflow(cocina_object:)
     Workflow::Service.create(workflow_name: 'releaseWF',
                              druid: cocina_object.externalIdentifier,
                              version: cocina_object.version)
   end
 
-  def decommissioned_cocina_object
-    structural = Cocina::Models::DROStructural.new(cocina_object.structural&.to_h&.except(:contains))
-
-    cocina_object.new(access: DECOMMISSION_ACCESS,
-                      administrative: DECOMMISSION_APO,
-                      structural:)
-  end
-
   def decommission_tag(release_tag)
     Dor::ReleaseTag.new(
       to: release_tag.to,
-      who: release_tag.who,
+      who: sunetid,
       what: release_tag.what,
       release: false,
       date: DateTime.now.utc.iso8601
--- spec/services/decommission_service_spec.rb
+++ spec/services/decommission_service_spec.rb
@@ -3,126 +3,64 @@
 require 'rails_helper'
 
 RSpec.describe DecommissionService do
+  let(:cocina_object) { repository_object.head_version.to_cocina_with_metadata }
   let(:druid) { 'druid:hj185xx2222' }
-  let(:cocina_object) do
-    Cocina::Models.with_metadata(
-      build(:dro, id: druid).new(access: { view: 'world' },
-                                 administrative: { hasAdminPolicy: 'druid:hy787xj5878' },
-                                 structural: {
-                                   contains: [
-                                     {
-                                       type: Cocina::Models::FileSetType.file.to_s,
-                                       externalIdentifier: 'hj185xx2222_1',
-                                       label: 'Image 1',
-                                       version: 3,
-                                       structural: {
-                                         contains: [
-                                           {
-                                             type: Cocina::Models::ObjectType.file.to_s,
-                                             externalIdentifier: 'druid:hj185xx2222/not_shelved.jpg',
-                                             label: 'not shelved',
-                                             filename: 'not_shelved.jpg',
-                                             size: 29_634,
-                                             version: 3,
-                                             hasMimeType: 'image/jpeg',
-                                             hasMessageDigests: [
-                                               {
-                                                 type: 'sha1',
-                                                 digest: '85a32f398e228e8228ad84422941110597e0d87a'
-                                               },
-                                               {
-                                                 type: 'md5',
-                                                 digest: '2b9498107f73ff827e718d5c743f8802'
-                                               }
-                                             ],
-                                             access: {
-                                               view: 'dark',
-                                               download: 'none'
-                                             },
-                                             administrative: {
-                                               sdrPreserve: true,
-                                               shelve: false,
-                                               publish: false
-                                             }
-                                           }
-                                         ]
-                                       }
-                                     }
-                                   ],
-                                   isMemberOf: [collection_druid]
-                                 }),
-      lock
-    )
-  end
-  let(:collection_druid) { 'druid:bc778pm9866' }
-  let(:reason) { 'No longer needed' }
+  let(:description) { 'No longer needed' }
   let(:sunetid) { 'awesome_po' }
   let(:apo_object) { build(:admin_policy, id: Settings.graveyard_admin_policy.druid) }
-  let(:collection_object) { build(:collection, id: collection_druid) }
-  let(:lock) { 'druid:hj185xx2222=4=2' }
 
   describe '.decommission' do
     subject(:decommission) do
-      described_class.decommission(cocina_object:, reason:, sunetid:)
+      described_class.decommission(druid:, description:, sunetid:)
     end
-
-    let!(:repository_object) do
-      create(:repository_object, :with_repository_object_version, external_identifier: druid, version: 1)
-    end
-    let(:decommissioned_cocina_object) do
-      Cocina::Models.with_metadata(
-        build(:dro, id: druid).new(access: { view: 'dark' },
-                                   structural: {
-                                     contains: [],
-                                     isMemberOf: [collection_druid]
-                                   },
-                                   administrative: { hasAdminPolicy: apo_object.externalIdentifier }),
-        updated_lock,
-        created:,
-        modified:
-      )
-    end
-    let(:updated_lock) { 'druid:hj185xx2222=5=3' }
-    let(:created) { repository_object.reload.created_at.utc }
-    let(:modified) { repository_object.reload.updated_at.utc }
 
     before do
-      create(:release_tag, druid:, released_to: 'Searchworks', what: 'self', who: 'bob', release: true,
-                           created_at: 1.day.ago.iso8601)
       allow(CocinaObjectStore).to receive(:find).with(druid).and_return(cocina_object)
-      allow(CocinaObjectStore).to receive(:find).with(Settings.graveyard_admin_policy.druid).and_return(apo_object)
-      allow(CocinaObjectStore).to receive(:find).with(collection_druid).and_return(collection_object)
-      allow(Indexer).to receive(:reindex_later)
-      allow(VersionService).to receive(:open)
-      allow(VersionService).to receive(:close)
-      allow(Workflow::Service).to receive(:create)
-      allow(ReleaseTagService).to receive(:create)
-      allow(AdministrativeTags).to receive(:create)
     end
 
-    it 'decommissions the object' do
-      expect(decommission).to eq(decommissioned_cocina_object)
+    context 'when the object can be opened for versioning' do
+      let(:repository_object) do
+        create(:repository_object, :with_repository_object_version, :closed, external_identifier: druid, version: 1)
+      end
+      let(:updated_cocina_object) { repository_object.head_version.to_cocina_with_metadata }
 
-      expect(VersionService).to have_received(:open).with(cocina_object:,
-                                                          description: "Decommissioned: #{reason}",
-                                                          assume_accessioned: true,
-                                                          opening_user_name: sunetid)
+      before do
+        create(:release_tag, druid:, released_to: 'Searchworks', what: 'self', who: 'bob', release: true,
+                             created_at: 1.day.ago.iso8601)
+        allow(CocinaObjectStore).to receive(:find).with(Settings.graveyard_admin_policy.druid).and_return(apo_object)
+        allow(Settings.version_service).to receive(:sync_with_preservation).and_return false
+        allow(Indexer).to receive(:reindex_later)
+        allow(Workflow::Service).to receive(:create)
+        allow(ReleaseTagService).to receive(:create)
+        allow(AdministrativeTags).to receive(:create)
+      end
 
-      expect(ReleaseTagService).to have_received(:create).with(tag: an_instance_of(Dor::ReleaseTag),
-                                                               cocina_object:,
-                                                               create_only: true)
+      it 'decommissions the object' do
+        expect(decommission).to be_an_instance_of(Cocina::Models::DROWithMetadata)
+        repository_object.reload
+        expect(ReleaseTagService).to have_received(:create).with(tag: an_instance_of(Dor::ReleaseTag),
+                                                                 cocina_object: an_instance_of(Cocina::Models::DROWithMetadata),
+                                                                 create_only: true)
+        expect(AdministrativeTags).to have_received(:create).with(identifier: druid,
+                                                                  tags: ["Decommissioned : #{description}"])
+        expect(Indexer).to have_received(:reindex_later).with(druid:).twice
+        expect(Workflow::Service).to have_received(:create).with(workflow_name: 'releaseWF', druid:,
+                                                                 version: repository_object.head_version_version)
+        expect(repository_object.head_version).to be_closed
+        expect(updated_cocina_object.structural.contains).to be_empty
+        expect(updated_cocina_object.administrative.hasAdminPolicy).to eq(Settings.graveyard_admin_policy.druid)
+      end
+    end
 
-      expect(AdministrativeTags).to have_received(:create).with(identifier: druid,
-                                                                tags: ["Decommissioned : #{reason}"])
+    context 'when the object cannot be opened for versioning' do
+      let(:repository_object) do
+        create(:repository_object, :with_repository_object_version, external_identifier: druid, version: 1)
+      end
 
-      expect(VersionService).to have_received(:close).with(druid:,
-                                                           description: "Decommissioned: #{reason}",
-                                                           user_name: sunetid,
-                                                           version: cocina_object.version)
-
-      expect(Indexer).to have_received(:reindex_later).with(druid:)
-      expect(Workflow::Service).to have_received(:create).with(workflow_name: 'releaseWF', druid:,
-                                                               version: cocina_object.version)
+      it 'raises an error' do
+        expect { decommission }.to raise_error(VersionService::VersioningError)
+          .with_message('Object already opened for versioning')
+      end
     end
   end
 end
