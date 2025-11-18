abf7c3dc60ec47085c0b5ecf2a4db56cef226f0d
Justin Coyne <jcoyne@justincoyne.com>
2025-11-18 12:02:01 -0600

    Use cocina display to get the title
    Instead of converting cocina to mods and then looking for it.

--- app/services/mods_utils.rb
+++ app/services/mods_utils.rb
@@ -2,19 +2,6 @@
 
 # Functions for querying MODS XML
 class ModsUtils
-  # @param [Nokogiri::Document] ng_xml
-  # @return [Nokogiri::Element]
-  def self.primary_title_info(ng_xml)
-    title_info = ng_xml.xpath('//mods:mods/mods:titleInfo[not(@type)]',
-                              mods: Cocina::Models::Mapping::FromMods::Description::DESC_METADATA_NS).first
-    title_info ||= ng_xml.xpath('//mods:mods/mods:titleInfo[@usage="primary"]',
-                                mods: Cocina::Models::Mapping::FromMods::Description::DESC_METADATA_NS).first
-    title_info ||= ng_xml.xpath('//mods:mods/mods:titleInfo',
-                                mods: Cocina::Models::Mapping::FromMods::Description::DESC_METADATA_NSS).first
-
-    title_info
-  end
-
   def self.label(ng_xml)
     ng_xml.root.add_namespace_definition('mods', 'http://www.loc.gov/mods/v3')
     ng_xml.xpath('/mods:mods/mods:titleInfo[1]')
