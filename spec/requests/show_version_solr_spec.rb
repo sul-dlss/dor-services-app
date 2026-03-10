095bd7479644b500f601a33210b2d33843b608f8
Michael J. Giarlo <mjgiarlo@stanford.edu>
2026-03-10 09:30:37 -0700

    Allow clients to request version Solr JSON without validating Cocina
    Connects to sul-dlss/argo#5022
    
    This will return incomplete Solr documents instead of raising Ruby exceptions, allowing consumers like Argo to display its show page for old versions with invalid Cocina metadata.

--- spec/requests/show_version_solr_spec.rb
+++ spec/requests/show_version_solr_spec.rb
@@ -3,26 +3,50 @@
 require 'rails_helper'
 
 RSpec.describe 'Show solr for an object version' do
-  let(:druid) { 'druid:mx123qw2323' }
+  let(:object) { create(:repository_object, :with_repository_object_version, :closed) }
+  let(:workflows) { instance_double(Indexing::Indexers::WorkflowsIndexer, to_solr: { 'wf_ssim' => ['accessionWF'] }) }
 
-  let(:workflows) do
-    instance_double(Indexing::Indexers::WorkflowsIndexer, to_solr: { 'wf_ssim' => ['accessionWF'] })
+  before do
+    allow(Indexing::Indexers::WorkflowsIndexer).to receive(:new).and_return(workflows)
+    allow(Indexing::WorkflowFields).to receive(:for).and_return({ milestones_ssim: %w[foo bar] })
   end
 
-  context 'when found' do
+  it 'returns a 200' do
+    get "/v1/objects/#{object.external_identifier}/versions/1/solr",
+        headers: { 'Authorization' => "Bearer #{jwt}" }
+
+    expect(response).to have_http_status(:ok)
+    expect(response.parsed_body).to include(id: object.external_identifier)
+    expect(response.parsed_body).to include(rights_descriptions_ssimdv: ['world', 'dark (file)'])
+  end
+
+  context 'with an object that has invalid cocina' do
     before do
-      create(:repository_object, :with_repository_object_version, :closed, external_identifier: druid)
-      allow(Indexing::Indexers::WorkflowsIndexer).to receive(:new).and_return(workflows)
-      allow(Indexing::WorkflowFields).to receive(:for).and_return({ milestones_ssim: %w[foo bar] })
+      object.head_version.tap do |version|
+        version.description['title'] =
+          [{ 'value' => 'Test DRO', 'parallelValue' => [{ 'value' => 'Аԥышәара DRO' }, { 'value' => 'परीक्षण DRO' }] }]
+        version.save!
+      end
     end
 
-    it 'returns a 200' do
-      get "/v1/objects/#{druid}/versions/1/solr",
-          headers: { 'Authorization' => "Bearer #{jwt}" }
+    context 'when validate param is set to true (default value)' do
+      it 'raises a Cocina validation error' do
+        expect do
+          get "/v1/objects/#{object.external_identifier}/versions/1/solr?validate=true",
+              headers: { 'Authorization' => "Bearer #{jwt}" }
+        end.to raise_error(Cocina::Models::ValidationError)
+      end
+    end
 
-      expect(response).to have_http_status(:ok)
-      expect(response.parsed_body).to include(id: druid)
-      expect(response.parsed_body).to include(rights_descriptions_ssimdv: ['world', 'dark (file)'])
+    context 'when validate param is set to false' do
+      it 'returns a 200' do
+        get "/v1/objects/#{object.external_identifier}/versions/1/solr?validate=false",
+            headers: { 'Authorization' => "Bearer #{jwt}" }
+
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body).to include(id: object.external_identifier)
+        expect(response.parsed_body).to include(display_title_ss: 'Test DRO')
+      end
     end
   end
 end
