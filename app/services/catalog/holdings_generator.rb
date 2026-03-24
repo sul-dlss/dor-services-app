41271e45051a6c22c8f3cc5c55f1b55e50897601
Laura Wrubel <lwrubel@stanford.edu>
2026-03-24 16:54:51 -0400

    Clean-up for clarity

--- app/services/catalog/holdings_generator.rb
+++ app/services/catalog/holdings_generator.rb
@@ -3,36 +3,32 @@
 module Catalog
   # Creates or updates FOLIO holdings record
   class HoldingsGenerator
-    DEFAULT_CATALOG = 'folio'
-
-    def self.manage_holdings(cocina_object, catalog: 'folio')
-      new(cocina_object, catalog:).manage_holdings
+    def self.manage_holdings(...)
+      new(...).manage_holdings
     end
 
     # @param [Cocina::Models::DRO,Cocina::Models::Collection,Cocina::Models::AdminPolicy] cocina_object
-    # @param [String] catalog used to determine the catalog record id to use for the collection
-    def initialize(cocina_object, catalog: DEFAULT_CATALOG)
+    def initialize(cocina_object)
       @cocina_object = cocina_object
-      @catalog = catalog || DEFAULT_CATALOG
     end
 
     def manage_holdings
       return if catalog_record_id.blank?
 
       # Update the existing SDR holding if there is one
-      if sdr_holding.presence
-        update_holding(holding: sdr_holding)
+      if sdr_holding.present?
+        update_holding
       # Only create a holdings record if this object is being released.
       elsif PublicMetadataReleaseTagService.released_to_searchworks?(cocina_object:)
-        create_from_holdings(holdings: holdings)
+        create_from_holdings
       end
     end
 
-    def create_from_holdings(holdings:)
+    def create_from_holdings
       instance_id = FolioClient.fetch_external_id(hrid: catalog_record_id)
       holdings_record = {
         'instance_id' => instance_id,
-        'permanent_location_id' => sdr_location(holdings: holdings),
+        'permanent_location_id' => sdr_location,
         'source_id' => 'f32d531e-df79-46b3-8932-cdd35f7a2264', # FOLIO
         'holdings_type_id' => '5684e4a3-9279-4463-b6ee-20ae21bbec07', # Electronic
         'discoverySuppress' => false
@@ -42,7 +38,7 @@ module Catalog
 
     private
 
-    attr_reader :cocina_object, :catalog
+    attr_reader :cocina_object
 
     def holdings
       @holdings ||= FolioClient.fetch_holdings(hrid: catalog_record_id)
@@ -51,17 +47,17 @@ module Catalog
     def sdr_holding
       return nil if holdings.empty?
 
-      fetch_sdr_holding(holdings: holdings)
+      @sdr_holding ||= fetch_sdr_holding(holdings: holdings)
     end
 
-    def update_holding(holding:)
+    def update_holding
       # Changes the holdings record discoverySuppress value and PUTs to FOLIO
       discovery_suppress = !PublicMetadataReleaseTagService.released_to_searchworks?(cocina_object:)
       # No need to update if the discoverySuppress value is already correct
-      return if holding&.dig('discoverySuppress') == discovery_suppress
+      return if sdr_holding&.dig('discoverySuppress') == discovery_suppress
 
-      holding['discoverySuppress'] = discovery_suppress
-      FolioClient.update_holdings(holdings_id: holding['id'], holdings_record: holding)
+      sdr_holding['discoverySuppress'] = discovery_suppress
+      FolioClient.update_holdings(holdings_id: sdr_holding['id'], holdings_record: sdr_holding)
     end
 
     # @param holdings [Array<Hash>] holdings records retrieved for the object's HRID
@@ -73,17 +69,19 @@ module Catalog
           location[:id] == holding['permanentLocationId']
         end
       end
-      if sdr_holdings.size > 1
+
+      case sdr_holdings.size
+      when 1
+        sdr_holdings.first
+      when 0
+        nil
+      else
         raise StandardError,
               "Multiple SDR holdings records found for #{cocina_object.externalIdentifier}: #{sdr_holdings}"
       end
-
-      return nil if sdr_holdings.empty?
-
-      sdr_holdings.first
     end
 
-    def sdr_location(holdings:)
+    def sdr_location
       # Find the first holding that has a location with a campus_id that matches one of the relevant campus ids.
       # If none of the holdings have a location with a matching campus_id, default to sul_sdr location id.
       holdings.each do |holding|
@@ -98,9 +96,7 @@ module Catalog
 
     # @return [String] catalog_record_id for the object
     def catalog_record_id
-      @cocina_object.identification.catalogLinks.filter_map do |link|
-        link.catalogRecordId if link.catalog == 'folio'
-      end.first
+      cocina_object.identification.catalogLinks.find { |link| link.catalog == 'folio' }&.catalogRecordId
     end
   end
 end
