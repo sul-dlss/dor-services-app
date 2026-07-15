a9058c63265e56f99a2743e3dedc9fa650884309
Laura Wrubel <lwrubel@stanford.edu>
2026-07-15 11:46:33 -0400

    Revert #6249. Published cocina no longer needs label.

--- app/services/publish/public_cocina_service.rb
+++ app/services/publish/public_cocina_service.rb
@@ -14,12 +14,11 @@ module Publish
     # remove any file that is not published
     # remove any file_set that doesn't have at least one published file
     # remove partOfProject (similar to how we remove tags from identityMetadata)
-    def build # rubocop:disable Metrics/AbcSize
-      label = Cocina::Models::Builders::TitleBuilder.build(cocina.description.title)
+    def build
       if cocina.dro?
-        cocina.new(label:, structural: build_structural, administrative: build_administrative)
+        cocina.new(structural: build_structural, administrative: build_administrative)
       elsif cocina.collection?
-        cocina.new(label:, administrative: build_administrative)
+        cocina.new(administrative: build_administrative)
       else
         raise "unexpected call to PublicCocinaService.build for #{cocina.externalIdentifier}"
       end
