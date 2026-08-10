d74170a2eba0aa816c190f692334af3657f3503a
Justin Littman <justinlittman@stanford.edu>
2026-08-10 06:31:49 -0400

    Upgraded to Rails 8.1

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module DorServices
   # The main application class for the Dor Services application.
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.0
+    config.load_defaults 8.1
 
     config.after_initialize do
       Cocina::Models::Mapping::Purl.base_url = Settings.release.purl_base_url
